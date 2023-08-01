de16adba77303cc4d98a56102633abc63825aa96
cka-y <chaimaa@mobilitydata.org>
2023-07-31 20:33:44 -0400

    test: testing postgresql deploy

--- infra/postgresql/main.tf
+++ infra/postgresql/main.tf
@@ -1,7 +1,28 @@
+data "google_project" "project" {}
+
+locals {
+  services = [
+    "sqladmin.googleapis.com",
+    "cloudresourcemanager.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "services" {
+  for_each                   = toset(local.services)
+  service                    = each.value
+  project                    = var.project_id
+  disable_dependent_services = true
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.gcp_region
+}
+
 resource "google_sql_database_instance" "db" {
-  name             = var.instance_name
+  name             = var.postgresql_instance_name
   database_version = "POSTGRES_12"
-  region           = var.region
+  region           = var.gcp_region
 
   settings {
     tier = "db-f1-micro"
@@ -9,13 +30,13 @@ resource "google_sql_database_instance" "db" {
 }
 
 resource "google_sql_database" "default" {
-  name       = var.database_name
+  name       = var.postgresql_database_name
   instance   = google_sql_database_instance.db.name
   collation  = "en_US.UTF8"
 }
 
 resource "google_sql_user" "users" {
-  name     = var.user_name
+  name     = var.postgresql_user_name
   instance = google_sql_database_instance.db.name
-  password = var.user_password
+  password = var.postgresql_user_password
 }
