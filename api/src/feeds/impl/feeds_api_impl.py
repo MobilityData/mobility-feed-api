525e55b669a06f7b31a8d657a4e57834fe22aa2e
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-08-02 11:52:26 -0400

    add operationId to openAPI and refactor api impl function names

--- api/src/feeds/impl/feeds_api_impl.py
+++ api/src/feeds/impl/feeds_api_impl.py
@@ -4,6 +4,8 @@ from typing import List
 from feeds_gen.apis.feeds_api_base import BaseFeedsApi
 from feeds_gen.models.basic_feed import BasicFeed
 from feeds_gen.models.bounding_box import BoundingBox
+from feeds_gen.models.extra_models import TokenModel
+from feeds_gen.models.feed_log import FeedLog
 from feeds_gen.models.gtfs_dataset import GtfsDataset
 from feeds_gen.models.gtfs_feed import GtfsFeed
 from feeds_gen.models.gtfs_rt_feed import GtfsRTFeed
@@ -18,7 +20,26 @@ class FeedsApiImpl(BaseFeedsApi):
     If a method is left blank the associated endpoint will return a 500 HTTP response.
     """
 
-    def feeds_get(
+    def get_feed(
+            self,
+            id: str,
+    ) -> BasicFeed:
+        """Get the specified feed from the Mobility Database."""
+        return BasicFeed(id="gtfsFeedFoo", data_type=None, status=None, external_ids=[], provider="providerFoo",
+                         feed_name="feedFoo", note="note", source_info=SourceInfo())
+
+    async def get_feed_logs(
+            id: str,
+            limit: int,
+            offset: int,
+            filter: str,
+            sort: str,
+            token_ApiKeyAuth: TokenModel,
+    ) -> List[FeedLog]:
+        """Get a list of logs related to a feed."""
+        return []
+
+    def get_feeds(
             self,
             limit: int,
             offset: int,
@@ -26,32 +47,17 @@ class FeedsApiImpl(BaseFeedsApi):
             sort: str,
     ) -> List[BasicFeed]:
         """Get some (or all) feeds from the Mobility Database."""
-        return [self.feeds_id_get("gtfsFeedFoo")]
+        return [self.get_feed("gtfsFeedFoo")]
 
-    def feeds_gtfs_get(
+    def get_gtfs_feed(
             self,
-            limit: int,
-            offset: int,
-            filter: str,
-            sort: str,
-            bounding_latitudes: str,
-            bounding_longitudes: str,
-            bounding_filter_method: str,
-    ) -> List[GtfsFeed]:
-        """Get some (or all) GTFS feeds from the Mobility Database."""
-        return [self.feeds_id_get("foo")]
+            id: str,
+    ) -> GtfsFeed:
+        """Get the specified feed from the Mobility Database."""
+        return GtfsFeed(id="gtfsFeedFoo", data_type=None, status=None, external_ids=[], provider="providerFoo",
+                        feed_name="feedFoo", note="note", source_info=SourceInfo(), latest_dataset=LatestDataset())
 
-    def feeds_gtfs_rt_get(
-            self,
-            limit: int,
-            offset: int,
-            filter: str,
-            sort: str,
-    ) -> List[GtfsRTFeed]:
-        """Get some (or all) GTFS feeds from the Mobility Database."""
-        return [self.feeds_id_gtfs_rt_get("foo")]
-
-    def feeds_id_datasets_gtfs_get(
+    def get_gtfs_feed_datasets(
             self,
             id: str,
             latest: bool,
@@ -68,23 +74,20 @@ class FeedsApiImpl(BaseFeedsApi):
                             download_date=date.today(), creation_date=date.today(), last_update_date=date.today(),
                             hash="123", locations=[], bounding_box=BoundingBox(), features=[])]
 
-    def feeds_id_get(
+    def get_gtfs_feeds(
             self,
-            id: str,
-    ) -> BasicFeed:
-        """Get the specified feed from the Mobility Database."""
-        return GtfsFeed(id="gtfsFeedFoo", data_type=None, status=None, external_ids=[], provider="providerFoo",
-                        feed_name="feedFoo", note="note", source_info=SourceInfo())
+            limit: int,
+            offset: int,
+            filter: str,
+            sort: str,
+            bounding_latitudes: str,
+            bounding_longitudes: str,
+            bounding_filter_method: str,
+    ) -> List[GtfsFeed]:
+        """Get some (or all) GTFS feeds from the Mobility Database."""
+        return [self.get_gtfs_feed("foo")]
 
-    def feeds_id_gtfs_get(
-            self,
-            id: str,
-    ) -> GtfsFeed:
-        """Get the specified feed from the Mobility Database."""
-        return GtfsFeed(id="gtfsFeedFoo", data_type=None, status=None, external_ids=[], provider="providerFoo",
-                        feed_name="feedFoo", note="note", source_info=SourceInfo(), latest_dataset=LatestDataset())
-
-    def feeds_id_gtfs_rt_get(
+    def get_gtfs_rt_feed(
             self,
             id: str,
     ) -> GtfsRTFeed:
@@ -92,3 +95,13 @@ class FeedsApiImpl(BaseFeedsApi):
         return GtfsRTFeed(id="gtfsrtFoo", data_type=None, status=None, externals_ids=[], provider="providerFoo",
                           feed_name="feedFoo", note="Note", source_info=SourceInfo(), entity_types=[],
                           feed_references=[])
+
+    def get_gtfs_rt_feeds(
+            self,
+            limit: int,
+            offset: int,
+            filter: str,
+            sort: str,
+    ) -> List[GtfsRTFeed]:
+        """Get some (or all) GTFS feeds from the Mobility Database."""
+        return [self.get_gtfs_rt_feed("foo")]
