0c27544e875e5ea8cd522abf99d720bd65b4bb96
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-08-12 17:46:41 -0400

    fix db gen

--- scripts/db-gen.sh
+++ scripts/db-gen.sh
@@ -19,22 +19,22 @@ mkdir "$SCRIPT_PATH/../api/src/database_gen/"
 pip3 install -r "${SCRIPT_PATH}/../api/requirements.txt"
 
 # removing sqlacodegen.log file
-if [ -s "${SCRIPT_PATH}/sqlacodegen.log" ]
+if [ -s ${SCRIPT_PATH}/sqlacodegen.log ]
 then
-  rm "${SCRIPT_PATH}/sqlacodegen.log"
+  rm ${SCRIPT_PATH}/sqlacodegen.log
 fi
 # Running sqlacodegen and capturing errors and warnings in the sqlacodegen.log file
-sqlacodegen "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" --outfile "${OUT_FILE}" &> "${SCRIPT_PATH}/sqlacodegen.log"
+sqlacodegen "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" --outfile "${OUT_FILE}" &> ${SCRIPT_PATH}/sqlacodegen.log
 
 # shellcheck disable=SC2181
-if [ $? -eq 0 ] && [ ! -s "${SCRIPT_PATH}/sqlacodegen.log" ]
+if [ $? -eq 0 ] && [ ! -s ${SCRIPT_PATH}/sqlacodegen.log ]
 then
   echo "Success: executing sqlacodegen."
   exit 0
 else
   echo "Failure executing sqlacodegen" >&2
   printf "\nsqlacodegen error:\n"
-  cat ./sqlacodegen.log
+  cat ${SCRIPT_PATH}/sqlacodegen.log
   printf "\n"
   exit 1
 fi
