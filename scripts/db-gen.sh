07cf1331c55291007b4f22f0753cd4ecd49b5ee9
David Gamez <1192523+davidgamez@users.noreply.github.com>
2023-08-14 19:14:58 -0400

    Fix/geometry fields generation (#80)

--- scripts/db-gen.sh
+++ scripts/db-gen.sh
@@ -16,8 +16,28 @@ ENV_PATH=$SCRIPT_PATH/../config/.env.local
 source "$ENV_PATH"
 rm -rf "$SCRIPT_PATH/../api/src/database_gen/"
 mkdir "$SCRIPT_PATH/../api/src/database_gen/"
-pip3 install -r "${SCRIPT_PATH}/../api/requirements.txt"
-sqlacodegen "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" --outfile "${OUT_FILE}"
+pip install -r "${SCRIPT_PATH}/../api/requirements.txt"
+
+# removing sqlacodegen.log file
+if [ -s ${SCRIPT_PATH}/sqlacodegen.log ]
+then
+  rm ${SCRIPT_PATH}/sqlacodegen.log
+fi
+# Running sqlacodegen and capturing errors and warnings in the sqlacodegen.log file
+sqlacodegen "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}" --outfile "${OUT_FILE}" &> ${SCRIPT_PATH}/sqlacodegen.log
+
+# shellcheck disable=SC2181
+if [ $? -eq 0 ] && [ ! -s ${SCRIPT_PATH}/sqlacodegen.log ]
+then
+  echo "Success: executing sqlacodegen."
+  exit 0
+else
+  echo "Failure executing sqlacodegen" >&2
+  printf "\nsqlacodegen error:\n"
+  cat ${SCRIPT_PATH}/sqlacodegen.log
+  printf "\n"
+  exit 1
+fi
 
 rm -rf "$TEMP_FILE"
 
