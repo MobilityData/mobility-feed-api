11b485c2b3f8160da026a22c822a40c0b07cfe97
cka-y <chaimaa@mobilitydata.org>
2023-08-21 22:15:50 -0400

    test: testing gh action

--- infra/batch/datasets/main.py
+++ infra/batch/datasets/main.py
@@ -1,9 +1,10 @@
 import functions_framework
 
+
 # Register an HTTP function with the Functions Framework
 @functions_framework.http
 def batch_dataset(request):
-  # Your code here
-  print(request)
-  # Return an HTTP response
-  return 'Function has run successfully yay'
+    # Your code here
+    print(request)
+    # Return an HTTP response
+    return 'Function has run successfully yay'
