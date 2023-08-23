2a5c40a311e7a926c255cdfbe036ed0d7990125b
cka-y <chaimaa@mobilitydata.org>
2023-08-23 09:57:06 -0400

    test: testing gh action

--- infra/batch/datasets/main.py
+++ infra/batch/datasets/main.py
@@ -6,7 +6,8 @@ import functions_framework
 def batch_dataset(request):
     # Your code here
     print("Hello we are inside the code")
-    print("Redeployment test")
+    print("Redeployment test 2")
+    print("Redeployment test 3")
     print(request)
     # Return an HTTP response
     return 'Function has run successfully yay'
