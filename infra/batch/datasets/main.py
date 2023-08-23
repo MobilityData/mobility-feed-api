a5a77bb87836889cc913397a254d3b4e929a5833
cka-y <chaimaa@mobilitydata.org>
2023-08-23 10:03:39 -0400

    test: testing gh action

--- infra/batch/datasets/main.py
+++ infra/batch/datasets/main.py
@@ -1,5 +1,14 @@
+from google.cloud import storage
 import functions_framework
 
+def create_test_file(bucket_name, file_name):
+    # Create a storage client
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    # Write data to the blob
+    blob.upload_from_string('This is test data for redeployment test.')
 
 # Register an HTTP function with the Functions Framework
 @functions_framework.http
@@ -9,5 +18,9 @@ def batch_dataset(request):
     print("Redeployment test 2")
     print("Redeployment test 3")
     print(request)
+
+    # Create a test file in the specified bucket
+    create_test_file("datasets", "test-file.txt")
+
     # Return an HTTP response
     return 'Function has run successfully yay'
