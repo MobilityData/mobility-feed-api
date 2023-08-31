fbd35346140c7fe7b16496b5843b7a2dff249e26
cka-y <chaimaa@mobilitydata.org>
2023-08-31 00:00:41 -0400

    feat: testing process

--- infra/batch/main.tf
+++ infra/batch/main.tf
@@ -89,7 +89,6 @@ resource "google_cloudfunctions2_function" "function2" {
     timeout_seconds       = 540
     environment_variables = var.function_env_variables
     service_account_email = data.google_service_account.ci_impersonator_service_account.email
-    max_instance_request_concurrency = 10
   }
   event_trigger {
     trigger_region = "us-central1"
