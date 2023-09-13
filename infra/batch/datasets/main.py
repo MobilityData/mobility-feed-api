5c5742eef3d66c4e9320262e3f60ab286a1af9fc
cka-y <chaimaa@mobilitydata.org>
2023-09-13 17:23:15 -0400

    fix: increasing memory limit

--- infra/batch/datasets/main.py
+++ infra/batch/datasets/main.py
@@ -39,7 +39,7 @@ def upload_dataset(url, bucket_name, stable_id):
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
             'Chrome/39.0.2171.95 Safari/537.36'
     }
-    response = requests.get(url, headers=headers, verify=False)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
 
     content = response.content
