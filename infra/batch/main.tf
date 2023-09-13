a86b98412f58751c5b78c59164afc10de240b35b
cka-y <chaimaa@mobilitydata.org>
2023-09-13 17:26:40 -0400

    fix: increasing memory limit

--- infra/batch/main.tf
+++ infra/batch/main.tf
@@ -58,6 +58,7 @@ resource "google_cloudfunctions2_function" "http_function" {
   }
   service_config {
     available_memory      = var.available_memory
+    available_cpu         = "583m"
     timeout_seconds       = var.http_timeout_seconds
     environment_variables = var.function_env_variables
     service_account_email = data.google_service_account.ci_impersonator_service_account.email
