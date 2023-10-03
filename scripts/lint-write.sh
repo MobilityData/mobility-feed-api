8ad12aec704f98d5afd879707af9f22a4dfbcdc5
arda <arda.turkmen@twosigma.com>
2023-10-02 20:58:28 -0400

    fix unit tests in CI

--- scripts/lint-write.sh
+++ scripts/lint-write.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 #
-# This script executes all project tests.
-# By default all tests are executed, if you need to execute a single test file, pass the test file name as a parameter.
-# All test are expected to be inside the directory <project_folder>/tests.
-# Usage:
-#   api-test.sh <optional_test_file>
-#
-# Parameters:
-# <test_file>: optional
+# Convenience script to run linters in write mode
 
 # relative path
 SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 
-(cd $SCRIPT_PATH/../api/ && pip3 install -r requirements_dev.txt && python3 -m flake8 && python3 -m black .)
+cd $SCRIPT_PATH/../api/ || exit 1
+pip3 install virtualenv
+python -m virtualenv venv
+venv/bin/python -m pip install -r requirements_dev.txt
+venv/bin/python -m flake8 && venv/bin/python -m black .
