c16f81c3e62784e7b05c065895b87632fc343e8d
arda <arda.turkmen@twosigma.com>
2023-10-02 20:25:57 -0400

    fix unit tests in CI

--- scripts/lint-tests.sh
+++ scripts/lint-tests.sh
@@ -14,7 +14,7 @@
 SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 
 cd $SCRIPT_PATH/../api/ || exit 1
-pip3 install --user virtualenv
+pip3 install virtualenv
 python -m virtualenv venv
 venv/bin/python -m pip install -r requirements_dev.txt
 venv/bin/python -m flake8 && venv/bin/python -m black . --check
