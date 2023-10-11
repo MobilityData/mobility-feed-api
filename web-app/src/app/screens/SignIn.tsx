209754ec9cc0b1f66625649aad391e8e07635ec2
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-10-10 21:53:26 -0400

    add signin error messages

--- web-app/src/app/screens/SignIn.tsx
+++ web-app/src/app/screens/SignIn.tsx
@@ -12,25 +12,45 @@ import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../hooks';
-import { login, selectIsAuthenticated } from '../store/profile-reducer';
+import {
+  login,
+  selectEmailLoginError,
+  selectIsAuthenticated,
+} from '../store/profile-reducer';
 import { type EmailLogin } from '../types';
 import { useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Alert } from '@mui/material';
 
 export default function SignIn(): React.ReactElement {
   const dispatch = useAppDispatch();
   const navigateTo = useNavigate();
   const isAuthenticated = useSelector(selectIsAuthenticated);
+  const emailLoginError = useSelector(selectEmailLoginError);
 
-  const handleEmailLogin = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const emailLogin: EmailLogin = {
-      email: data.get('email') != null ? (data.get('email') as string) : '',
-      password:
-        data.get('password') != null ? (data.get('password') as string) : '',
-    };
-    dispatch(login(emailLogin));
-  };
+  const SignInSchema = Yup.object().shape({
+    email: Yup.string().email().required('Email is required'),
+
+    password: Yup.string()
+      .required('Password is required')
+      .min(4, 'Password is too short - should be 4 chars minimum'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: SignInSchema,
+    onSubmit: (values) => {
+      const emailLogin: EmailLogin = {
+        email: values.email,
+        password: values.password,
+      };
+      dispatch(login(emailLogin));
+    },
+  });
 
   React.useEffect(() => {
     if (isAuthenticated) {
@@ -62,7 +82,7 @@ export default function SignIn(): React.ReactElement {
         </Typography>
         <Box
           component='form'
-          onSubmit={handleEmailLogin}
+          onSubmit={formik.handleSubmit}
           noValidate
           sx={{
             mt: 1,
@@ -80,7 +100,13 @@ export default function SignIn(): React.ReactElement {
             name='email'
             autoComplete='email'
             autoFocus
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            error={formik.errors.email != null}
           />
+          {formik.errors.email != null ? (
+            <Alert severity='error'>{formik.errors.email}</Alert>
+          ) : null}
           <TextField
             margin='normal'
             required
@@ -90,7 +116,13 @@ export default function SignIn(): React.ReactElement {
             type='password'
             id='password'
             autoComplete='current-password'
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            error={formik.errors.password != null}
           />
+          {formik.errors.email != null ? (
+            <Alert severity='error'>{formik.errors.password}</Alert>
+          ) : null}
           <FormControlLabel
             control={<Checkbox value='remember' color='primary' />}
             label='Remember me'
@@ -100,11 +132,14 @@ export default function SignIn(): React.ReactElement {
             type='submit'
             variant='contained'
             sx={{ mt: 3, mb: 2 }}
-            onClick={() => handleEmailLogin}
+            onClick={() => formik.handleChange}
             data-testid='signin'
           >
             Sign In
           </Button>
+          {emailLoginError != null ? (
+            <Alert severity='error'>{emailLoginError.message}</Alert>
+          ) : null}
         </Box>
         <Box
           sx={{
--- web-app/src/app/store/profile-reducer.ts
+++ web-app/src/app/store/profile-reducer.ts
@@ -11,7 +11,7 @@ interface User {
 
 interface UserProfileState {
   status: 'unauthenticated' | 'login_in' | 'authenticated' | 'login_out';
-  error: AppError | undefined;
+  loginError: AppError | null;
   user: User | undefined;
 }
 
@@ -20,23 +20,8 @@ interface UserProfileState {
 const initialState: UserProfileState = {
   status: 'unauthenticated',
   user: undefined,
-  error: undefined,
+  loginError: null,
 };
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-
-// export const incrementAsync = createAsyncThunk<
-//   number,
-//   number,
-//   { state: { counter: CounterState } }
-// >('counter/fetchCount', async (amount: number, { getState }) => {
-//   const { value } = getState().counter;
-//   const response = await fetchCount(value, amount);
-//   return response.data;
-// });
 
 export const userProfileSlice = createSlice({
   name: 'userProfile',
@@ -44,12 +29,15 @@ export const userProfileSlice = createSlice({
   reducers: {
     login: (state, action: PayloadAction<EmailLogin>) => {
       state.status = 'login_in';
+      state.loginError = null;
     },
     loginSuccess: (state, action: PayloadAction<User>) => {
       state.status = 'authenticated';
+      state.loginError = null;
     },
     loginFail: (state, action: PayloadAction<AppError>) => {
-      state = { ...state, error: action.payload, status: 'unauthenticated' };
+      state.loginError = action.payload;
+      state.status = 'unauthenticated';
     },
     logout: (
       state,
@@ -59,9 +47,11 @@ export const userProfileSlice = createSlice({
       }>,
     ) => {
       state.status = 'login_out';
+      state.loginError = null;
     },
     logoutSucess: (state) => {
       state.status = 'unauthenticated';
+      state.loginError = null;
     },
   },
 });
@@ -69,13 +59,13 @@ export const userProfileSlice = createSlice({
 export const { login, loginSuccess, loginFail, logout } =
   userProfileSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectUserProfile = (state: RootState): User | undefined =>
   state.userProfile.user;
 
 export const selectIsAuthenticated = (state: RootState): boolean =>
   state.userProfile.status === 'authenticated';
+
+export const selectEmailLoginError = (state: RootState): AppError | null =>
+  state.userProfile.loginError;
 
 export default userProfileSlice.reducer;
