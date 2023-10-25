896df134aa69f7707986140bbe88c15db58b0c3f
cka-y <chaimaa@mobilitydata.org>
2023-10-25 15:41:32 -0400

    fix: removed unused bigquery functions

--- infra/batch/helpers/utils.py
+++ infra/batch/helpers/utils.py
@@ -1,9 +1,7 @@
+import os
+
 from google.cloud import storage
 from sqlalchemy import create_engine
-import os
-from google.cloud import bigquery
-from datetime import date
-from typing import List, Dict, Union, Any
 
 
 def create_bucket(bucket_name):
@@ -35,57 +33,3 @@ def get_db_engine():
     engine = create_engine(sqlalchemy_database_url, echo=True)
     return engine
 
-
-def create_bigquery_table(
-        project_id: str,
-        dataset_name: str,
-        table_name: str,
-        schema: List[bigquery.SchemaField]
-) -> bigquery.Table:
-    """Create a BigQuery dataset and table with the given schema.
-    :param project_id: The ID of the project where the dataset and table will reside.
-    :param dataset_name: The name of the dataset to create.
-    :param table_name: The name of the table to create.
-    :param schema: The schema for the table.
-    :return: The created table.
-    """
-    client = bigquery.Client()
-
-    # Create a Dataset
-    dataset_id = f"{project_id}.{dataset_name}"
-    dataset = bigquery.Dataset(dataset_id)
-    client.create_dataset(dataset, exists_ok=True)
-
-    # Create a Table
-    table_id = f"{dataset_id}.{table_name}"
-    table = bigquery.Table(table_id, schema=schema)
-    return client.create_table(table, exists_ok=True)
-
-
-def insert_rows_into_table(
-        table: bigquery.Table,
-        rows: List[Dict[str, Union[str, int, date]]]
-):
-    """Insert rows into a BigQuery table.
-    :param table: The table where rows will be inserted.
-    :param rows: The rows to insert.
-    :return: list of errors
-    """
-    client = bigquery.Client()
-    return client.insert_rows_json(table, rows)
-
-
-def query_table(
-        project_id: str,
-        query: str
-) -> List[Dict[str, Any]]:
-    """
-    Query a BigQuery table and return the results as a list of dictionaries.
-    :param project_id: The ID of the project where the table resides.
-    :param query: The SQL query string.
-    :return: A list of dictionaries containing the query results.
-    """
-    client = bigquery.Client(project=project_id)
-    query_job = client.query(query)
-    results = [dict(row) for row in query_job]
-    return results
