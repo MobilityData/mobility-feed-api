964777b0d74ee29915a62296491fd9db6ec27cf8
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-11-08 22:59:46 -0500

    complete unit tests and add lint to functions folder

--- scripts/lint-write.sh
+++ scripts/lint-write.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 #
-# Convenience script to run linters in write mode
+# This script write lint rules based on flake8.
+# Usage:
+#   lint-write.sh
 
-# relative path
-SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+# absolute path
+ABS_SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cd $SCRIPT_PATH/../api/ || exit 1
-pip3 install virtualenv
-python -m virtualenv venv
-venv/bin/python -m pip install -r requirements_dev.txt
-venv/bin/python -m flake8 && venv/bin/python -m black .
+# funtion to execute tests with parameter path
+execute_tests() {
+    printf "\nExecuting lint write in $1\n" 
+    cd $ABS_SCRIPTPATH/$1 || exit 1
+    pip3 install --disable-pip-version-check virtualenv > /dev/null
+    python -m virtualenv venv > /dev/null
+    venv/bin/python -m pip install -r requirements_dev.txt > /dev/null
+    venv/bin/python -m flake8 && venv/bin/python -m black .
+    printf "\n"
+}
+
+execute_tests "../api/"
+execute_tests "../functions-python/"
+
