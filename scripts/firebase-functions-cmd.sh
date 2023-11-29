5a2b27c0e5210982c3024facc3c18b3f32dcfa98
cka-y <chaimaa@mobilitydata.org>
2023-11-29 11:07:23 -0500

    feat: created yarn workspaces

--- scripts/firebase-functions-cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# First argument is the task (i.e. test, lint)
-TASK=$1
-
-find functions -name 'package.json' -not -path '*/node_modules/*' -exec dirname {} \; | while read dir; do
-    echo "Running $TASK in $dir"
-    (cd "$dir" && yarn "$TASK")
-done
