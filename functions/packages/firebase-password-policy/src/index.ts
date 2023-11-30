b630c2de4179601de129715f714855eac49ff534
cka-y <chaimaa@mobilitydata.org>
2023-11-30 16:01:45 -0500

    feat: firebase password policy

--- functions/packages/firebase-password-policy/src/index.ts
+++ functions/packages/firebase-password-policy/src/index.ts
@@ -1,18 +1,5 @@
 import {initializeApp} from "firebase-admin/app";
-import {CallableRequest, onCall} from "firebase-functions/v2/https";
-
+import * as impl from "./impl/firebase-password-policy-impl";
 
 initializeApp();
-
-export const testHelloWorld = onCall(
-  {minInstances: 0, maxInstances: 100, invoker: "public"},
-  /**
-     * Updates or creates instance of user's information in Datastore
-     * @param {CallableRequest} request
-     * @return {Promise<string>} Success message
-     * @throws {HttpsError} Throws an error if the user is not authenticated,
-     * full name is not provided or if there is an error updating the user
-     */
-  async (request: CallableRequest): Promise<string> => {
-    return "Hello World";
-  });
+impl.setPasswordPolicyConfig().then();
