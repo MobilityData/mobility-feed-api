bafc67546fc9167c69da453f3d4857e993c1f282
Jingsi Lu <jingsi@mobilitydata.org>
2023-12-06 15:05:57 -0500

    fixed cypress

--- web-app/cypress/e2e/changepassword.cy.ts
+++ web-app/cypress/e2e/changepassword.cy.ts
@@ -6,6 +6,6 @@ describe('Change Password Screen', () => {
   it('should render components', () => {
     cy.get('input[id="currentPassword"]').should('exist');
     cy.get('input[id="newPassword"]').should('exist');
-    cy.get('input[id="confirmPassword"]').should('exist');
+    cy.get('input[id="confirmNewPassword"]').should('exist');
   });
 });
