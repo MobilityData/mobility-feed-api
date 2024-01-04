94f18bf729424c7c604ebe50ba7694f8cdcdda80
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2024-01-04 11:48:57 -0500

    document dataset service

--- functions-python/dataset_service/main.py
+++ functions-python/dataset_service/main.py
@@ -22,13 +22,20 @@ from typing import Optional, Final
 from google.cloud import datastore
 from google.cloud.datastore import Client
 
+# This files contains the dataset trace and batch execution models and services.
+# The dataset trace is used to store the trace of a dataset and the batch execution
+# One batch execution can have multiple dataset traces.
+# Each dataset trace represents the resulting log of the dataset processing.
+# The persistent layer used is Google Cloud Datastore.
 
+# Status of the dataset trace
 class Status(Enum):
     FAILED = "FAILED"
     PUBLISHED = "PUBLISHED"
     NOT_PUBLISHED = "NOT_PUBLISHED"
 
 
+# Dataset trace class to store the trace of a dataset
 @dataclass
 class DatasetTrace:
     stable_id: str
@@ -41,6 +48,7 @@ class DatasetTrace:
     error_message: Optional[str] = None
 
 
+# Batch execution class to store the trace of a batch execution
 @dataclass
 class BatchExecution:
     execution_id: str
@@ -52,14 +60,17 @@ dataset_trace_collection: Final[str] = "dataset_trace"
 batch_execution_collection: Final[str] = "batch_execution"
 
 
+# Dataset trace service with CRUD operations for the dataset trace
 class DatasetTraceService:
     def __init__(self, client: Client = None):
         self.client = datastore.Client() if client is None else client
 
+    # Save the dataset trace
     def save(self, dataset_trace: DatasetTrace):
         entity = self._dataset_trace_to_entity(dataset_trace)
         self.client.put(entity)
 
+    # Get the dataset trace by trace id
     def get_by_id(self, trace_id: str) -> [DatasetTrace]:
         key = self.client.key(dataset_trace_collection, trace_id)
         entity = self.client.get(key)
@@ -69,6 +80,7 @@ class DatasetTraceService:
         else:
             return []
 
+    # Get the dataset trace by execution id and stable id
     def get_by_execution_and_stable_ids(
         self, execution_id: str, stable_id: str
     ) -> [DatasetTrace]:
@@ -83,6 +95,7 @@ class DatasetTraceService:
         else:
             return []
 
+    # Transform the dataset trace to entity
     def _dataset_trace_to_entity(self, dataset_trace: DatasetTrace) -> datastore.Entity:
         trace_id = str(uuid.uuid4())
         key = self.client.key(dataset_trace_collection, trace_id)
@@ -94,6 +107,7 @@ class DatasetTraceService:
 
         return entity
 
+    # Transform the entity to dataset trace
     @staticmethod
     def _entity_to_dataset_trace(entity: datastore.Entity) -> DatasetTrace:
         return DatasetTrace(
@@ -108,14 +122,17 @@ class DatasetTraceService:
         )
 
 
+# Batch execution service with CRUD operations for the batch execution
 class BatchExecutionService:
     def __init__(self):
         self.client = datastore.Client()
 
+    # Save the batch execution
     def save(self, execution: BatchExecution):
         entity = self._execution_to_entity(execution)
         self.client.put(entity)
 
+    # Get the batch execution by execution id
     def get_by_id(self, execution_id: str) -> [BatchExecution]:
         query = self.client.query(kind=batch_execution_collection)
         query.add_filter("execution_id", "=", execution_id)
@@ -125,8 +142,9 @@ class BatchExecutionService:
         if results:
             return self._entity_to_execution(results)
         else:
-            return []
+            return None
 
+    # Transform the entity to batch execution
     def _execution_to_entity(self, execution: BatchExecution) -> datastore.Entity:
         key = self.client.key(batch_execution_collection, execution.execution_id)
         entity = datastore.Entity(key=key)
