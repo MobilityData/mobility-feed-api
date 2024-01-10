f650561579be8f804ef0d494aa03e9790dc7414c
Jingsi Lu <jingsi@mobilitydata.org>
2024-01-10 14:34:18 -0500

    used global_session in database.py

--- api/src/database/database.py
+++ api/src/database/database.py
@@ -14,6 +14,7 @@ from utils.logger import Logger
 
 
 lock = threading.Lock()
+global_session = None
 
 
 def generate_unique_id() -> str:
@@ -47,7 +48,6 @@ class Database:
         host = os.getenv("POSTGRES_HOST")
         self.logger = Logger(Database.__module__).get_logger()
         self.engine = None
-        self.session = None
         self.connection_attempts = 0
         self.SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{schema}"
 
@@ -70,13 +70,14 @@ class Database:
         Checks the connection status
         :return: True if the database is accessible False otherwise
         """
-        return self.engine is not None and self.session is not None
+        return self.engine is not None and global_session is not None
 
     def start_session(self):
         """
-        Starts a session
+        Starts a singleton session
         :return: True if the session was started, False otherwise
         """
+        global global_session
         try:
             if self.engine is None:
                 self.connection_attempts += 1
@@ -86,21 +87,24 @@ class Database:
                 else:
                     self.engine = create_engine(self.SQLALCHEMY_DATABASE_URL, echo=True)
                 self.logger.debug("Database connected.")
-            if self.session is not None and self.session.is_active:
-                self.session.close()
-            self.session = Session(self.engine, autoflush=False)
-        except Exception as e:
-            self.logger.error(f"Database new session creation failed with exception: \n {e}")
+            if global_session is not None and global_session.is_active:
+                self.logger.info("Database session reused.")
+                return global_session
+            global_session = Session(self.engine, autoflush=False)
+            self.logger.info("Singleton Database session started.")
+        except Exception as error:
+            raise Exception(f"Error creating database session: {error}")
         return self.is_connected()
+
 
     def close_session(self):
         """
-        Starts a session
+        Closes a session
         :return: True if the session was started, False otherwise
         """
         try:
-            if self.session is not None and self.session.is_active:
-                self.session.close()
+            if global_session is not None and global_session.is_active:
+                global_session.close()
         except Exception as e:
             self.logger.error(f"Session closing failed with exception: \n {e}")
         return self.is_connected()
@@ -133,7 +137,7 @@ class Database:
             if update_session:
                 self.start_session()
             if query is None:
-                query = self.session.query(model)
+                query = global_session.query(model)
             if conditions:
                 for condition in conditions:
                     query = query.filter(condition)
@@ -143,13 +147,16 @@ class Database:
                 query = query.limit(limit)
             if offset is not None:
                 query = query.offset(offset)
-            results = self.session.execute(query).all()
+            results = global_session.execute(query).all()
             if group_by:
                 return [list(group) for _, group in itertools.groupby(results, group_by)]
             return results
         except Exception as e:
             self.logger.error(f"SELECT query failed with exception: \n{e}")
             return None
+        finally:
+            if not update_session:
+                self.close_session()
 
     def select_from_active_session(self, model: Base, conditions: list = None, attributes: list = None):
         """
@@ -160,9 +167,9 @@ class Database:
         :return: Empty list if database is inaccessible, the results of the query otherwise
         """
         try:
-            if not self.session or not self.session.is_active:
+            if not global_session or not global_session.is_active:
                 raise Exception("Inactive session")
-            results = [obj for obj in self.session.new if isinstance(obj, model)]
+            results = [obj for obj in global_session.new if isinstance(obj, model)]
             if conditions:
                 for condition in conditions:
                     attribute_name = condition.left.name
@@ -193,13 +200,16 @@ class Database:
         try:
             if update_session:
                 self.start_session()
-            self.session.merge(orm_object, load=load)
+            global_session.merge(orm_object, load=load)
             if auto_commit:
-                self.session.commit()
+                global_session.commit()
             return True
         except Exception as e:
             self.logger.error(f"Merge query failed with exception: \n{e}")
             return False
+        finally:
+            if not update_session:
+                self.close_session()
 
     def commit(self):
         """
@@ -208,16 +218,16 @@ class Database:
         :return: True if commit was successful, False otherwise
         """
         try:
-            if self.session is not None and self.session.is_active:
-                self.session.commit()
+            if global_session is not None and global_session.is_active:
+                global_session.commit()
                 return True
             return False
         except Exception as e:
             self.logger.error(f"Commit failed with exception: \n{e}")
             return False
         finally:
-            if self.session is not None:
-                self.session.close()
+            if global_session is not None:
+                global_session.close()
 
     def flush(self):
         """
@@ -226,8 +236,8 @@ class Database:
         :return: True if flush was successful, False otherwise
         """
         try:
-            if self.session is not None and self.session.is_active:
-                self.session.flush()
+            if global_session is not None and global_session.is_active:
+                global_session.flush()
                 return True
             return False
         except Exception as e:
