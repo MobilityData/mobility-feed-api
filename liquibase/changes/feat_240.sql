f8c14ff5cc055aa6cd654e7bd5011cdc92f1de20
cka-y <60586858+cka-y@users.noreply.github.com>
2024-01-18 17:23:36 -0500

    feat: validation schema updates (#241)
    * feat: validation schema updates
    
    * fix: circular dependency
    
    * fix: update liquibase/changes/feat_240.sql
    
    Co-authored-by: David Gamez <1192523+davidgamez@users.noreply.github.com>
    
    * fix: update liquibase/changes/feat_240.sql
    
    Co-authored-by: David Gamez <1192523+davidgamez@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: David Gamez <1192523+davidgamez@users.noreply.github.com>

---
+++ liquibase/changes/feat_240.sql
@@ -0,0 +1,29 @@
+-- 1. Add Table ValidationReport
+CREATE TABLE ValidationReport (
+    id VARCHAR(255) PRIMARY KEY,
+    validator_version VARCHAR(255)
+);
+
+-- 2. Adding a table linking GTFSDataset to the validation report to avoid circular dependency warning from sqlacodegen
+CREATE TABLE ValidationReportGTFSDataset (
+    dataset_id VARCHAR(255) REFERENCES GTFSDataset(id),
+    validation_report_id VARCHAR(255) REFERENCES ValidationReport UNIQUE,
+    PRIMARY KEY (validation_report_id, dataset_id)
+);
+
+-- 3. Create custom type for severity
+CREATE TYPE severity_type AS ENUM ('ERROR', 'WARNING', 'INFO');
+
+-- 4. Add Table Notice
+CREATE TABLE Notice (
+    dataset_id VARCHAR(255) REFERENCES GTFSDataset(id),
+    validation_report_id VARCHAR(255) REFERENCES ValidationReport(id),
+    notice_code VARCHAR(255),
+    severity severity_type,
+    total_notices INT NOT NULL CHECK (total_notices > 0),
+    PRIMARY KEY (dataset_id, validation_report_id, notice_code)
+);
+
+-- 5. Add indexes for the Notice table
+CREATE INDEX idx_notice_dataset_id ON Notice(dataset_id);
+CREATE INDEX idx_notice_dataset_id_validation_id ON Notice(dataset_id, validation_report_id);
