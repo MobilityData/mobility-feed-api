09791a0759093ec28c68b55cd0c9391ca834c19c
cka-y <chaimaa@mobilitydata.org>
2024-01-23 14:29:33 -0500

    feat: infra code update

---
+++ functions-python/extract_bb/src/main.py
@@ -0,0 +1,35 @@
+from cloudevents.http import CloudEvent
+
+import functions_framework
+
+
+# Triggered by a change in a storage bucket
+@functions_framework.cloud_event
+def hello_gcs(cloud_event: CloudEvent) -> tuple:
+    """This function is triggered by a change in a storage bucket.
+
+    Args:
+        cloud_event: The CloudEvent that triggered this function.
+    Returns:
+        The event ID, event type, bucket, name, metageneration, and timeCreated.
+    """
+    data = cloud_event.data
+
+    event_id = cloud_event["id"]
+    event_type = cloud_event["type"]
+
+    bucket = data["bucket"]
+    name = data["name"]
+    metageneration = data["metageneration"]
+    timeCreated = data["timeCreated"]
+    updated = data["updated"]
+
+    print(f"Event ID: {event_id}")
+    print(f"Event type: {event_type}")
+    print(f"Bucket: {bucket}")
+    print(f"File: {name}")
+    print(f"Metageneration: {metageneration}")
+    print(f"Created: {timeCreated}")
+    print(f"Updated: {updated}")
+
+    return event_id, event_type, bucket, name, metageneration, timeCreated, updated
--- infra/functions-python/main.tf
+++ infra/functions-python/main.tf
@@ -15,11 +15,11 @@
 #
 
 locals {
-  functions_dir          = "${path.module}../../../functions-python"
-  function_config_file   = "function_config.json"
-  function_tokens_path   = "${path.module}../../../functions-python/tokens"
   function_tokens_config = jsondecode(file("${path.module}../../../functions-python/tokens/function_config.json"))
   function_tokens_zip    = "${path.module}/../../functions-python/tokens/.dist/tokens.zip"
+
+  function_extract_bb_config = jsondecode(file("${path.module}../../../functions-python/extract_bb/function_config.json"))
+  function_extract_bb_zip    = "${path.module}/../../functions-python/extract_bb/.dist/extract_bb.zip"
 }
 
 # Service account to execute the cloud functions
@@ -33,12 +33,21 @@ resource "google_storage_bucket" "functions_bucket" {
   location = "us"
 }
 
+# Cloud function source code zip files:
+# 1. Tokens
 resource "google_storage_bucket_object" "function_token_zip" {
   name   = "tokens-${substr(filebase64sha256(local.function_tokens_zip),0,10)}.zip"
   bucket = google_storage_bucket.functions_bucket.name
   source = local.function_tokens_zip
 }
+# 2. Bucket extract bounding box
+resource "google_storage_bucket_object" "function_extract_bb_zip_object" {
+  name   = "bucket-extract-bb-${substr(filebase64sha256(local.function_extract_bb_zip),0,10)}.zip"
+  bucket = google_storage_bucket.functions_bucket.name
+  source = local.function_extract_bb_zip
+}
 
+# Secrets
 data "google_iam_policy" "secret_access" {
   binding {
     role = "roles/secretmanager.secretAccessor"
@@ -56,6 +65,8 @@ resource "google_secret_manager_secret_iam_policy" "policy" {
   policy_data = data.google_iam_policy.secret_access.policy_data
 }
 
+# Cloud function definitions
+# 1. functions-python/tokens cloud function
 resource "google_cloudfunctions2_function" "tokens" {
   name        = local.function_tokens_config.name
   description = local.function_tokens_config.description
@@ -91,10 +102,46 @@ resource "google_cloudfunctions2_function" "tokens" {
   }
 }
 
+# 2. functions/extract_bb cloud function
+resource "google_cloudfunctions2_function" "extract_bb" {
+  name        = local.function_extract_bb_config.name
+  description = local.function_extract_bb_config.description
+  location    = var.gcp_region
+
+  event_trigger {
+    event_type = "google.cloud.storage.object.v1.finalized"
+    event_filters {
+      attribute = "resourceName"
+      value     = "/projects/_/buckets/${var.datasets_bucket_name}-${var.environment}/**/{filename=mdb-*.zip}"
+    }
+  }
+
+  build_config {
+    runtime     = var.python_runtime
+    entry_point = local.function_extract_bb_config.entry_point
+    source {
+      storage_source {
+        bucket = google_storage_bucket.functions_bucket.name
+        object = google_storage_bucket_object.function_extract_bb_zip_object.name
+      }
+    }
+  }
+  service_config {
+    available_memory = local.function_extract_bb_config.memory
+    timeout_seconds = local.function_extract_bb_config.timeout
+    available_cpu = local.function_extract_bb_config.available_cpu
+    max_instance_request_concurrency = local.function_extract_bb_config.max_instance_request_concurrency
+    max_instance_count = local.function_extract_bb_config.max_instance_count
+    min_instance_count = local.function_extract_bb_config.min_instance_count
+    service_account_email = google_service_account.functions_service_account.email
+    ingress_settings = local.function_extract_bb_config.ingress_settings
+  }
+}
+
 # IAM entry for all users to invoke the function 
 resource "google_cloudfunctions2_function_iam_member" "tokens_invoker" {
   project        = var.project_id
-  location         = var.gcp_region
+  location       = var.gcp_region
   cloud_function = google_cloudfunctions2_function.tokens.name
   role           = "roles/cloudfunctions.invoker"
   member         = "allUsers"
@@ -102,10 +149,10 @@ resource "google_cloudfunctions2_function_iam_member" "tokens_invoker" {
 
 resource "google_cloud_run_service_iam_member" "tokens_cloud_run_invoker" {
   project        = var.project_id
-  location         = var.gcp_region
-  service = google_cloudfunctions2_function.tokens.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+  location       = var.gcp_region
+  service        = google_cloudfunctions2_function.tokens.name
+  role           = "roles/run.invoker"
+  member         = "allUsers"
 }
 
 output "function_tokens_name" {
