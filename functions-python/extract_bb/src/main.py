9464121aa97d07ef44542f1f1057bc3c725cc1ba
cka-y <chaimaa@mobilitydata.org>
2024-01-24 11:57:52 -0500

    feat: updating code

--- functions-python/extract_bb/src/main.py
+++ functions-python/extract_bb/src/main.py
@@ -5,7 +5,7 @@ import functions_framework
 
 # Triggered by a change in a storage bucket
 @functions_framework.cloud_event
-def hello_gcs(cloud_event: CloudEvent) -> tuple:
+def extract_bounding_box(cloud_event: CloudEvent) -> str:
     """This function is triggered by a change in a storage bucket.
 
     Args:
@@ -15,21 +15,6 @@ def hello_gcs(cloud_event: CloudEvent) -> tuple:
     """
     data = cloud_event.data
 
-    event_id = cloud_event["id"]
-    event_type = cloud_event["type"]
+    print(f"data: {data}")
 
-    bucket = data["bucket"]
-    name = data["name"]
-    metageneration = data["metageneration"]
-    timeCreated = data["timeCreated"]
-    updated = data["updated"]
-
-    print(f"Event ID: {event_id}")
-    print(f"Event type: {event_type}")
-    print(f"Bucket: {bucket}")
-    print(f"File: {name}")
-    print(f"Metageneration: {metageneration}")
-    print(f"Created: {timeCreated}")
-    print(f"Updated: {updated}")
-
-    return event_id, event_type, bucket, name, metageneration, timeCreated, updated
+    return 'Yup'
