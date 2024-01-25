58cefe7b9b23f5f7dfe537e6d9d664776c4bc159
cka-y <chaimaa@mobilitydata.org>
2024-01-25 18:34:03 -0500

    feat: restoring api-deployer

--- infra/main.tf
+++ infra/main.tf
@@ -104,18 +104,18 @@ module "functions-python" {
 
 }
 
-#module "feed-api-load-balancer" {
-#  depends_on  = [module.feed-api, module.functions-python]
-#  project_id  = var.project_id
-#  gcp_region  = var.gcp_region
-#  environment = var.environment
-#
-#  feed_api_name                    = module.feed-api.feed_api_name
-#  oauth2_client_id                 = var.oauth2_client_id
-#  oauth2_client_secret             = var.oauth2_client_secret
-#  global_rate_limit_req_per_minute = var.global_rate_limit_req_per_minute
-#
-#  function_tokens_name = module.functions-python.function_tokens_name
-#
-#  source = "./load-balancer"
-#}
+module "feed-api-load-balancer" {
+  depends_on  = [module.feed-api, module.functions-python]
+  project_id  = var.project_id
+  gcp_region  = var.gcp_region
+  environment = var.environment
+
+  feed_api_name                    = module.feed-api.feed_api_name
+  oauth2_client_id                 = var.oauth2_client_id
+  oauth2_client_secret             = var.oauth2_client_secret
+  global_rate_limit_req_per_minute = var.global_rate_limit_req_per_minute
+
+  function_tokens_name = module.functions-python.function_tokens_name
+
+  source = "./load-balancer"
+}
