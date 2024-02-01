f0604821f86face956f9cc4d0973733a359baee6
Jingsi Lu <jingsi@mobilitydata.org>
2024-02-01 10:20:48 -0500

    added comments in on_start() function

--- load-test/gtfs_user_test.py
+++ load-test/gtfs_user_test.py
@@ -1,20 +1,23 @@
 from locust import HttpUser, TaskSet, task, between
 import os
 
+
 class gtfs_user(HttpUser):
     wait_time = between(5, 15)
 
     def on_start(self):
-        self.client.headers = {'Authorization': os.getenv('FEEDS_AUTH_TOKEN')}
+        # put the refresh token as github action secret
+        # get the access token and pass it to the script
+        self.client.headers = {'Authorization': os.environ('FEEDS_AUTH_TOKEN')}
 
     @task
     def feeds(self):
         self.client.get("/v1/feeds")
-    
+
     @task
     def feed_byId(self):
         self.client.get("/v1/feeds/mdb-10")
-    
+
     @task
     def gtfs_feeds(self):
         self.client.get("/v1/gtfs_feeds")
@@ -30,12 +33,11 @@ class gtfs_user(HttpUser):
     @task
     def gtfs_realtime_feed_byId(self):
         self.client.get("/v1/gtfs_rt_feeds/mdb-1852")
-    
+
     @task
     def gtfs_feeds_datasets(self):
         self.client.get("/v1/gtfs_feeds/mdb-10/datasets")
+
     @task
     def gtfs_dataset(self):
         self.client.get("/v1/datasets/gtfs/mdb-10")
-
-   
