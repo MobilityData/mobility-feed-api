2108c0d8a0634d47c4faee535ef9919c483427dc
Jingsi Lu <jingsi@mobilitydata.org>
2024-02-06 11:02:50 -0500

    added TreeView dependency

---
+++ api/tests/unittest/test_version.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from fastapi import app
+
+client = TestClient(app)
+
+
+def test_version():
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert "version" in response.json()
+    assert "commit_hash" in response.json()
