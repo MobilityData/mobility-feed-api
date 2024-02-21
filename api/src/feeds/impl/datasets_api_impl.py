b6f7508ac5823ce95654e3ae47c3f5b8337d3184
jujiang526 <jiang.ju@gmail.com>
2024-02-21 08:58:35 -0500

    feat: validation report (#292)

--- api/src/feeds/impl/datasets_api_impl.py
+++ api/src/feeds/impl/datasets_api_impl.py
@@ -2,11 +2,11 @@ import json
 from typing import List
 
 from geoalchemy2 import WKTElement
-from sqlalchemy import or_
-from sqlalchemy.orm import Query
+from sqlalchemy import or_, select, func, and_
+from sqlalchemy.orm import Query, aliased
 
 from database.database import Database
-from database_gen.sqlacodegen_models import Gtfsdataset, t_componentgtfsdataset, Feed
+
 from feeds.impl.error_handling import (
     invalid_bounding_coordinates,
     invalid_bounding_method,
@@ -14,9 +14,21 @@ from feeds.impl.error_handling import (
     raise_http_error,
     dataset_not_found,
 )
+from database_gen.sqlacodegen_models import (
+    Gtfsdataset,
+    t_componentgtfsdataset,
+    Feed,
+    Validationreport,
+    t_validationreportgtfsdataset,
+    Notice,
+)
 from feeds_gen.apis.datasets_api_base import BaseDatasetsApi
 from feeds_gen.models.bounding_box import BoundingBox
 from feeds_gen.models.gtfs_dataset import GtfsDataset
+from feeds_gen.models.validation_report import ValidationReport
+
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 
 class DatasetsApiImpl(BaseDatasetsApi):
@@ -44,6 +56,38 @@ class DatasetsApiImpl(BaseDatasetsApi):
             )
             .join(Feed, Feed.id == Gtfsdataset.feed_id)
         )
+
+    @staticmethod
+    def _load_validation_report():
+        """
+        This method is for loading validation reports. This could be done as part of loading gtfs dataset
+        but the query for loading gtfs dataset is already complex, and therefore I decided to create a separate method
+        for this.
+        """
+        vrgd = t_validationreportgtfsdataset.alias()
+        max_vr = aliased(Validationreport)
+
+        max_validator_version_subquery = (
+            select(vrgd.c.dataset_id, func.max(max_vr.validator_version).label("max_validator_version"))
+            .join(max_vr, max_vr.id == vrgd.c.validation_report_id)
+            .group_by(vrgd.c.dataset_id)
+            .alias("max_versions")
+        )
+
+        notices_query = (
+            select(Notice)
+            .join(vrgd, Notice.validation_report_id == vrgd.c.validation_report_id)
+            .join(Validationreport, Validationreport.id == Notice.validation_report_id)
+            .join(
+                max_validator_version_subquery,
+                and_(
+                    vrgd.c.dataset_id == max_validator_version_subquery.c.dataset_id,
+                    Validationreport.validator_version == max_validator_version_subquery.c.max_validator_version,
+                ),
+            )
+        )
+
+        return Database().session.execute(notices_query).scalars().all()
 
     @staticmethod
     def apply_bounding_filtering(
@@ -108,10 +152,38 @@ class DatasetsApiImpl(BaseDatasetsApi):
             group_by=lambda x: x[0].stable_id,
         )
 
+        notices = DatasetsApiImpl._load_validation_report()
+
         gtfs_datasets = []
         for dataset_group in dataset_groups:
             dataset_objects, components, bound_box_strings, feed_ids = zip(*dataset_group)
             database_gtfs_dataset = dataset_objects[0]
+            notices_for_dataset = [notice for notice in notices if notice.dataset_id == database_gtfs_dataset.id]
+
+            validator_report = None
+            if notices_for_dataset:
+                validator_report = ValidationReport(
+                    components=sorted([component for component in components if component is not None])
+                )
+                database_validator_report = notices_for_dataset[0].validation_report
+                validator_report.total_info = sum(
+                    [notice.total_notices for notice in notices_for_dataset if notice.severity == "INFO"]
+                )
+                validator_report.total_warning = sum(
+                    [notice.total_notices for notice in notices_for_dataset if notice.severity == "WARNING"]
+                )
+                validator_report.total_error = sum(
+                    [notice.total_notices for notice in notices_for_dataset if notice.severity == "ERROR"]
+                )
+                validator_report.validated_at = (
+                    database_validator_report.validated_at.strftime(DATETIME_FORMAT)
+                    if database_validator_report.validated_at
+                    else None
+                )
+                validator_report.validator_version = database_validator_report.validator_version
+                validator_report.url_json = database_validator_report.json_report
+                validator_report.url_html = database_validator_report.html_report
+
             gtfs_dataset = GtfsDataset(
                 id=database_gtfs_dataset.stable_id,
                 feed_id=feed_ids[0],
@@ -121,7 +193,7 @@ class DatasetsApiImpl(BaseDatasetsApi):
                 if database_gtfs_dataset.downloaded_at
                 else None,
                 hash=database_gtfs_dataset.hash,
-                components=[component for component in components if component is not None],
+                validation_report=validator_report,
             )
 
             if bound_box_string := bound_box_strings[0]:
--- api/tests/test_utils/database.py
+++ api/tests/test_utils/database.py
@@ -5,12 +5,31 @@ from typing import Final
 from geoalchemy2 import WKTElement
 
 from database.database import Database, generate_unique_id
-from database_gen.sqlacodegen_models import Gtfsfeed, Gtfsrealtimefeed, Gtfsdataset, Externalid
+from database_gen.sqlacodegen_models import (
+    Gtfsfeed,
+    Gtfsrealtimefeed,
+    Gtfsdataset,
+    Externalid,
+    Validationreport,
+    Notice,
+    Component,
+)
 
 TEST_GTFS_FEED_STABLE_IDS = ["mdb-1", "mdb-10", "mdb-20", "mdb-30"]
 TEST_DATASET_STABLE_IDS = ["mdb-2", "mdb-3", "mdb-11", "mdb-12"]
 TEST_GTFS_RT_FEED_STABLE_ID = "mdb-1561"
 TEST_EXTERNAL_IDS = ["external_id_1", "external_id_2", "external_id_3", "external_id_4"]
+OLD_VALIDATION_VERSION = "1.0.0"
+OLD_VALIDATION_TIME = datetime.utcnow() - timedelta(hours=1)
+NEW_VALIDATION_VERSION = "2.0.0"
+NEW_VALIDATION_TIME = datetime.utcnow()
+VALIDATION_INFO_COUNT_PER_NOTICE = 5
+VALIDATION_INFO_NOTICES = 10
+VALIDATION_WARNING_COUNT_PER_NOTICE = 3
+VALIDATION_WARNING_NOTICES = 4
+VALIDATION_ERROR_COUNT_PER_NOTICE = 2
+VALIDATION_ERROR_NOTICES = 7
+COMPONENT_IDS = [generate_unique_id() for _ in range(3)]
 
 date_string: Final[str] = "2024-01-31 00:00:00"
 date_format: Final[str] = "%Y-%m-%d %H:%M:%S"
@@ -23,67 +42,136 @@ def populate_database(db: Database):
     gtfs_feed_ids = [generate_unique_id() for _ in range(len(TEST_GTFS_FEED_STABLE_IDS))]
     dataset_ids = [generate_unique_id() for _ in range(len(TEST_DATASET_STABLE_IDS))]
     gtfs_rt_feed_id = generate_unique_id()
-    for stable_id, gtfs_feed_id in zip(TEST_GTFS_FEED_STABLE_IDS, gtfs_feed_ids):
-        db.merge(Gtfsfeed(id=gtfs_feed_id, stable_id=stable_id, data_type="gtfs", status="active"), auto_commit=True)
-    db.merge(
-        Gtfsrealtimefeed(
-            id=gtfs_rt_feed_id, stable_id=TEST_GTFS_RT_FEED_STABLE_ID, data_type="gtfs_rt", status="active"
-        ),
-        auto_commit=True,
-    )
-    min_lat = 37.615264
-    max_lat = 38.2321
-    min_lon = -84.8984452721203
-    max_lon = -84.4789953029549
-    polygon = (
-        f"POLYGON(({min_lon} {min_lat}, {min_lon} {max_lat}, {max_lon} {max_lat}, {max_lon} {min_lat}, "
-        f"{min_lon} {min_lat}))"
-    )
 
-    for idx, dataset_id in enumerate(dataset_ids):
+    try:
+        for stable_id, gtfs_feed_id in zip(TEST_GTFS_FEED_STABLE_IDS, gtfs_feed_ids):
+            db.merge(
+                Gtfsfeed(id=gtfs_feed_id, stable_id=stable_id, data_type="gtfs", status="active"), auto_commit=True
+            )
+        for component_id in COMPONENT_IDS:
+            db.merge(Component(name=component_id), auto_commit=True)
         db.merge(
-            Gtfsdataset(
-                id=dataset_id,
-                stable_id=TEST_DATASET_STABLE_IDS[idx],
-                feed_id=gtfs_feed_ids[idx // 2],
-                latest=idx % 2 == 1,
-                bounding_box=WKTElement(polygon, srid=4326),
-                # This makes downloaded_at predictable and unique for each dataset
-                downloaded_at=(datasets_download_first_date + idx * one_day),
+            Gtfsrealtimefeed(
+                id=gtfs_rt_feed_id, stable_id=TEST_GTFS_RT_FEED_STABLE_ID, data_type="gtfs_rt", status="active"
             ),
             auto_commit=True,
         )
-    for idx, external_id in enumerate(TEST_EXTERNAL_IDS):
-        db.merge(
-            Externalid(
-                feed_id=gtfs_feed_ids[idx // 2],
-                associated_id=external_id,
-                source="source" + str(idx + 1),
-            )
+        min_lat = 37.615264
+        max_lat = 38.2321
+        min_lon = -84.8984452721203
+        max_lon = -84.4789953029549
+        polygon = (
+            f"POLYGON(({min_lon} {min_lat}, {min_lon} {max_lat}, {max_lon} {max_lat}, {max_lon} {min_lat}, "
+            f"{min_lon} {min_lat}))"
         )
-    db.session.execute(
-        f"INSERT INTO redirectingid (source_id, target_id) VALUES ('{gtfs_feed_ids[0]}', '{gtfs_feed_ids[1]}')"
-    )
-    db.session.execute(
-        f"INSERT INTO redirectingid (source_id, target_id) VALUES ('{gtfs_feed_ids[1]}', '{gtfs_feed_ids[2]}')"
-    )
-    db.session.execute(
-        f"INSERT INTO redirectingid (source_id, target_id) VALUES ('{gtfs_feed_ids[1]}', '{gtfs_feed_ids[3]}')"
-    )
-    db.commit()
-    db.flush()
-    yield db
-    for dataset_id in dataset_ids:
-        db.session.execute(f"DELETE FROM gtfsdataset where id ='{dataset_id}'")
-    for external_id in TEST_EXTERNAL_IDS:
-        db.session.execute(f"DELETE FROM externalid where associated_id = '{external_id}'")
-    for gtfs_feed_id in gtfs_feed_ids:
-        db.session.execute(
-            f"DELETE from redirectingid where " f"source_id = '{gtfs_feed_id}' OR target_id = '{gtfs_feed_id}'"
-        )
-        db.session.execute(f"DELETE FROM gtfsfeed where id = '{gtfs_feed_id}'")
 
-    db.session.execute(f"DELETE FROM gtfsrealtimefeed where id = '{gtfs_rt_feed_id}'")
-    for feed_id in [*gtfs_feed_ids, gtfs_rt_feed_id]:
-        db.session.execute(f"DELETE FROM feed where id = '{feed_id}'")
-    db.commit()
+        for idx, dataset_id in enumerate(dataset_ids):
+            # for each dataset, create two validation reports, one old and one new
+            old_validation_report = Validationreport(
+                id=generate_unique_id(),
+                validator_version=OLD_VALIDATION_VERSION,
+                validated_at=OLD_VALIDATION_TIME,
+            )
+            new_validation_report = Validationreport(
+                id=generate_unique_id(),
+                validator_version=NEW_VALIDATION_VERSION,
+                validated_at=NEW_VALIDATION_TIME,
+            )
+
+            db.merge(
+                Gtfsdataset(
+                    id=dataset_id,
+                    stable_id=TEST_DATASET_STABLE_IDS[idx],
+                    feed_id=gtfs_feed_ids[idx // 2],
+                    latest=idx % 2 == 1,
+                    bounding_box=WKTElement(polygon, srid=4326),
+                    validation_reports=[old_validation_report, new_validation_report],
+                    # This makes downloaded_at predictable and unique for each dataset
+                    downloaded_at=(datasets_download_first_date + idx * one_day),
+                ),
+                auto_commit=True,
+            )
+
+            # create multiple notices for each severity level
+            for _ in range(VALIDATION_INFO_NOTICES):
+                db.merge(
+                    Notice(
+                        dataset_id=dataset_id,
+                        validation_report_id=new_validation_report.id,
+                        notice_code=generate_unique_id(),
+                        severity="INFO",
+                        total_notices=VALIDATION_INFO_COUNT_PER_NOTICE,
+                    ),
+                )
+            for _ in range(VALIDATION_WARNING_NOTICES):
+                db.merge(
+                    Notice(
+                        dataset_id=dataset_id,
+                        validation_report_id=new_validation_report.id,
+                        notice_code=generate_unique_id(),
+                        severity="WARNING",
+                        total_notices=VALIDATION_WARNING_COUNT_PER_NOTICE,
+                    ),
+                )
+            for _ in range(VALIDATION_ERROR_NOTICES):
+                db.merge(
+                    Notice(
+                        dataset_id=dataset_id,
+                        validation_report_id=new_validation_report.id,
+                        notice_code=generate_unique_id(),
+                        severity="ERROR",
+                        total_notices=VALIDATION_ERROR_COUNT_PER_NOTICE,
+                    ),
+                )
+            for component_id in COMPONENT_IDS:
+                db.session.execute(
+                    f"INSERT INTO componentgtfsdataset (component, dataset_id) "
+                    f"VALUES ('{component_id}', '{dataset_id}')"
+                )
+
+        for idx, external_id in enumerate(TEST_EXTERNAL_IDS):
+            db.merge(
+                Externalid(
+                    feed_id=gtfs_feed_ids[idx // 2],
+                    associated_id=external_id,
+                    source="source" + str(idx + 1),
+                )
+            )
+        db.session.execute(
+            f"INSERT INTO redirectingid (source_id, target_id) VALUES ('{gtfs_feed_ids[0]}', '{gtfs_feed_ids[1]}')"
+        )
+        db.session.execute(
+            f"INSERT INTO redirectingid (source_id, target_id) VALUES ('{gtfs_feed_ids[1]}', '{gtfs_feed_ids[2]}')"
+        )
+        db.session.execute(
+            f"INSERT INTO redirectingid (source_id, target_id) VALUES ('{gtfs_feed_ids[1]}', '{gtfs_feed_ids[3]}')"
+        )
+        db.commit()
+        db.flush()
+        yield db
+    except Exception as e:
+        print(e)
+    finally:
+        # clean up the testing data regardless of the test result
+        for dataset_id in dataset_ids:
+            db.session.execute(f"DELETE FROM componentgtfsdataset where dataset_id = '{dataset_id}'")
+            db.session.execute(f"DELETE FROM notice where dataset_id ='{dataset_id}'")
+            db.session.execute(f"DELETE FROM validationreportgtfsdataset where dataset_id ='{dataset_id}'")
+            db.session.execute(f"DELETE FROM gtfsdataset where id ='{dataset_id}'")
+        for external_id in TEST_EXTERNAL_IDS:
+            db.session.execute(f"DELETE FROM externalid where associated_id = '{external_id}'")
+        for gtfs_feed_id in gtfs_feed_ids:
+            db.session.execute(
+                f"DELETE from redirectingid where " f"source_id = '{gtfs_feed_id}' OR target_id = '{gtfs_feed_id}'"
+            )
+            db.session.execute(f"DELETE FROM gtfsfeed where id = '{gtfs_feed_id}'")
+
+        db.session.execute(f"DELETE FROM gtfsrealtimefeed where id = '{gtfs_rt_feed_id}'")
+        for feed_id in [*gtfs_feed_ids, gtfs_rt_feed_id]:
+            db.session.execute(f"DELETE FROM feed where id = '{feed_id}'")
+        db.session.execute(
+            f"""DELETE FROM component where name in ({', '.join(["'" + component_id + "'"
+                                                                                for component_id
+                                                                                in COMPONENT_IDS])})"""
+        )
+        db.commit()
