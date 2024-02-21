6afb31fdfc5c481d81397801123a2e8c806773fb
jcpitre <jcpitre@mobilitydata.org>
2024-02-21 14:03:06 -0500

    Corrected extract version script.

--- scripts/extract-hash-and-version.sh
+++ scripts/extract-hash-and-version.sh
@@ -22,15 +22,16 @@ echo "[DEFAULT]" >> $versionFile
 echo "LONG_COMMIT_HASH=$LONG_COMMIT_HASH" >> $versionFile
 echo "SHORT_COMMIT_HASH=$SHORT_COMMIT_HASH" >> $versionFile
 
+# Typically actions/checkout@v4 in the workflows does not get all the tags. Get them here.
+git fetch --tags
+
 # First obtain the tags that are associated with the current commit, if any. Tags should look like this: v1.1.0
 # sort -V is great! It sorts with versions, so v10.1.1 will come after v2.1.1 for example.
 EXTRACTED_VERSION=`git tag --contains "$LONG_COMMIT_HASH" | egrep '^v[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -1`
 
 if [ -z "$EXTRACTED_VERSION" ]; then
   # If there are no tags on the current commit, get all the tags with the latest at the end of the list.
-  # Typically actions/checkout@v4 in the workflows does not get the all tags. Get them ince we have to dig into past tags,
-  # tet them here
-  git fetch --tags
+
   EXTRACTED_VERSION=`git tag --sort=-creatordate | egrep '^v[0-9]+\.[0-9]+\.[0-9]+' | head -1`
   if [ -z "$EXTRACTED_VERSION" ]; then
     EXTRACTED_VERSION="v0.0.0"
