85b53548eefba4d55691d30e771ef8f6d5a8d6fc
jcpitre <106176106+jcpitre@users.noreply.github.com>
2024-02-26 15:24:46 -0500

    feat: Add extraction of commit hash and version in workflow. (#303)
    Added extraction of commit hash and version in workflow. Added these to the Metadata endpoint.

--- api/src/feeds/impl/metadata_api_impl.py
+++ api/src/feeds/impl/metadata_api_impl.py
@@ -1,3 +1,7 @@
+import configparser
+import logging
+import os
+
 from feeds_gen.apis.metadata_api_base import BaseMetadataApi
 from feeds_gen.models.metadata import Metadata
 
@@ -13,4 +17,28 @@ class MetadataApiImpl(BaseMetadataApi):
         self,
     ) -> Metadata:
         """Get metadata about this API."""
-        return Metadata(version="1.0.0")
+        version = None
+        long_commit_hash = None
+        try:
+            # Create a configparser object
+            config = configparser.ConfigParser()
+
+            current_directory = os.getcwd()
+
+            file = "version_info"
+            # The config file is on api/src. Normally the cwd is api/src, but with unit tests it's api. In that case
+            # add /src
+            if current_directory.endswith("api"):
+                file = os.path.join("src", file)
+
+            # Read the properties file. This file should have been filled as part of the build.
+            config.read(file)
+
+            # Access the values using the get() method
+            long_commit_hash = config.get("DEFAULT", "LONG_COMMIT_HASH")
+            version = config.get("DEFAULT", "EXTRACTED_VERSION")
+
+        except Exception as e:
+            logging.error(f"Cannot read {file} file from directory {current_directory}: \n {e}")
+
+        return Metadata(version=version, commit_hash=long_commit_hash)
