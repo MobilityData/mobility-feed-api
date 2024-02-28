dacac09cab2c5003a1b3ab984dee829075776693
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2024-02-28 09:24:27 -0500

    add ga tag

--- web-app/src/app/utils/config.ts
+++ web-app/src/app/utils/config.ts
@@ -1,8 +1,10 @@
 /**
- * Get environment config
+ * Get environment config.
+ * If the value is a placeholder means the value was not set properly, return an empty string
  * @param key variable key
  * @returns variable value or empty string
  */
 export const getEnvConfig = (key: string): string => {
-  return process.env[key] ?? '';
+  const value = process.env[key] ?? '';
+  return value === '{{' + key + '}}' ? '' : value.trim();
 };
