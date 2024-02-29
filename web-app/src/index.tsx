d55fc655aa903142f6c3926c17833eeadf83d931
David Gamez <1192523+davidgamez@users.noreply.github.com>
2024-02-29 15:18:13 -0500

    feat: setup Google Analytics tags on UI (#320)

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -4,6 +4,14 @@ import './index.css';
 import App from './app/App';
 import { theme } from './app/Theme';
 import { ThemeProvider } from '@mui/material/styles';
+import ReactGA from 'react-ga4';
+import { getEnvConfig } from './app/utils/config';
+
+const gaId = getEnvConfig('REACT_APP_GOOGLE_ANALYTICS_ID');
+if (gaId.length > 0) {
+  ReactGA.initialize(gaId);
+  ReactGA.send('pageview');
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
