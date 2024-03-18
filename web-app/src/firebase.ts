a1541a8fba1d50680c6a730f6fafdb523bbb49f9
cka-y <chaimaa@mobilitydata.org>
2024-03-18 13:29:26 -0400

    fix: gh action error

--- web-app/src/firebase.ts
+++ web-app/src/firebase.ts
@@ -13,6 +13,5 @@ const firebaseConfig = {
 export const app = firebase.initializeApp(firebaseConfig);
 export const remoteConfig = firebase.remoteConfig();
 remoteConfig.settings.minimumFetchIntervalMillis = Number(
-  process.env.REMOTE_CONFIG_MINIMUM_FETCH_INTERVAL_MILLIS ?? 3600000, // default to 12 hours
+  process.env.REACT_APP_REMOTE_CONFIG_MINIMUM_FETCH_INTERVAL_MILLI ?? 3600000, // default to 12 hours
 );
-console.log(process.env.REMOTE_CONFIG_MINIMUM_FETCH_INTERVAL_MILLIS);
