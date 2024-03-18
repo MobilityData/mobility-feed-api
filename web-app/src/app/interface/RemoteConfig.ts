45b0510255134acbe27741b5d54a390d0e001e70
cka-y <chaimaa@mobilitydata.org>
2024-03-18 11:49:34 -0400

    fix: default value to true

--- web-app/src/app/interface/RemoteConfig.ts
+++ web-app/src/app/interface/RemoteConfig.ts
@@ -3,8 +3,8 @@ export type RemoteConfigValues = Record<string, string | number | boolean>;
 
 // Add default values for remote config here
 export const defaultRemoteConfigValues: RemoteConfigValues = {
-  enableGoogleSSO: false,
-  enableGithubSSO: false,
+  enableGoogleSSO: true,
+  enableGithubSSO: true,
 };
 
 remoteConfig.defaultConfig = defaultRemoteConfigValues;
