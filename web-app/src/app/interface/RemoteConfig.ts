e3f09b41bff0a40e06d5811ed61114a7a0d5a6e0
Jingsi Lu <jingsi@mobilitydata.org>
2024-04-04 08:11:02 -0400

    added feature flag constant to the UI contants

--- web-app/src/app/interface/RemoteConfig.ts
+++ web-app/src/app/interface/RemoteConfig.ts
@@ -4,6 +4,7 @@ export type RemoteConfigValues = Record<string, string | number | boolean>;
 // Add default values for remote config here
 export const defaultRemoteConfigValues: RemoteConfigValues = {
   enableAppleSSO: false,
+  enableMVPSearch: false,
 };
 
 remoteConfig.defaultConfig = defaultRemoteConfigValues;
