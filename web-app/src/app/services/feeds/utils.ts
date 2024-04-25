94f8ab9ddd7173b7e5b0f46eac3678963c9e7db8
Indraneel Purohit <indraneel.purohit@twosigma.com>
2024-04-25 13:56:41 -0400

    add type guards for Basic feed and GTFS-RT feeds

--- web-app/src/app/services/feeds/utils.ts
+++ web-app/src/app/services/feeds/utils.ts
@@ -6,6 +6,16 @@ export type AllFeedType =
   | paths['/v1/gtfs_rt_feeds/{id}']['get']['responses'][200]['content']['application/json']
   | undefined;
 
+export type BasicFeedType =
+  | paths['/v1/feeds/{id}']['get']['responses'][200]['content']['application/json']
+  | undefined;
+
+export const isBasicFeedType = (
+  data: AllFeedType,
+): data is paths['/v1/feeds/{id}']['get']['responses'][200]['content']['application/json'] => {
+  return data !== undefined;
+};
+
 export type GTFSFeedType =
   | paths['/v1/gtfs_feeds/{id}']['get']['responses'][200]['content']['application/json']
   | undefined;
@@ -14,4 +24,14 @@ export const isGtfsFeedType = (
   data: AllFeedType,
 ): data is paths['/v1/gtfs_feeds/{id}']['get']['responses'][200]['content']['application/json'] => {
   return data !== undefined && data.data_type === 'gtfs';
+};
+
+export type GTFSRTFeedType =
+  | paths['/v1/gtfs_rt_feeds/{id}']['get']['responses'][200]['content']['application/json']
+  | undefined;
+
+export const isGtfsRtFeedType = (
+  data: AllFeedType,
+): data is paths['/v1/gtfs_rt_feeds/{id}']['get']['responses'][200]['content']['application/json'] => {
+  return data !== undefined && data.data_type === 'gtfs_rt';
 };
