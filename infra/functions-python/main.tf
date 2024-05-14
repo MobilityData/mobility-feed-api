3843019f8b184be724afa13f72be7a4c1dc84578
cka-y <chaimaa@mobilitydata.org>
2024-05-14 16:40:33 -0400

    fix: google_cloud_tasks_queue params

--- infra/functions-python/main.tf
+++ infra/functions-python/main.tf
@@ -373,7 +373,7 @@ resource "google_cloud_run_service_iam_member" "extract_bb_cloud_run_invoker" {
 
 # Task queue to invoke update_validation_report function
 resource "google_cloud_tasks_queue" "update_validation_report" {
-  project = var.project_id
+  project  = var.project_id
   location = var.gcp_region
-  queue = "update-validation-report"
+  name     = "update-validation-report"
 }
