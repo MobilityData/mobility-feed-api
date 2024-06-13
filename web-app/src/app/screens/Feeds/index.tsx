85812ea0224ddbe55bf0f301dff6de7a9c3c289c
Indraneel Purohit <indraneel.purohit@twosigma.com>
2024-06-13 14:31:07 -0400

    Feeds search page changes
    - make data type checkboxes work
    - make search limit + load more button work
    - tie search bar to URL query param

--- web-app/src/app/screens/Feeds/index.tsx
+++ web-app/src/app/screens/Feeds/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
   Box,
@@ -27,28 +27,89 @@ import {
   type GTFSRTFeedType,
 } from '../../services/feeds/utils';
 import SearchResultItem from './SearchResultItem';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+const SEARCH_LIMIT_INCREASE = 10;
+
+const getDataTypeParamFromSelectedFeedTypes = (
+  selectedFeedTypes: Record<string, boolean>,
+): 'gtfs' | 'gtfs_rt' | undefined => {
+  let dataTypeQueryParam: 'gtfs' | 'gtfs_rt' | undefined;
+  if (selectedFeedTypes.gtfs && !selectedFeedTypes.gtfs_rt) {
+    dataTypeQueryParam = 'gtfs';
+  } else if (!selectedFeedTypes.gtfs && selectedFeedTypes.gtfs_rt) {
+    dataTypeQueryParam = 'gtfs_rt';
+  } else if (!selectedFeedTypes.gtfs && !selectedFeedTypes.gtfs_rt) {
+    dataTypeQueryParam = undefined;
+  }
+  return dataTypeQueryParam;
+};
 
 export default function Feed(): React.ReactElement {
-  //   const { feedId } = useParams();
-  const [searchText, setSearchText] = useState('');
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchLimit, setSearchLimit] = useState(10);
+  const [selectedFeedTypes, setSelectedFeedTypes] = useState({
+    gtfs: true,
+    gtfs_rt: true,
+  });
+
   const user = useSelector(selectUserProfile);
   const dispatch = useAppDispatch();
   const feedsData = useSelector(selectFeedsData);
 
   const handleSearch = (): void => {
-    if (user?.accessToken !== undefined) {
+    const searchQuery = searchParams.get('q') ?? undefined;
+    if (
+      user?.accessToken !== undefined &&
+      searchQuery !== undefined &&
+      searchQuery.trim() !== ''
+    ) {
+      setSearchLimit(10);
       dispatch(
         loadingFeeds({
           accessToken: user?.accessToken,
           params: {
             query: {
-              search_query: searchText,
+              limit: searchLimit,
+              offset: 0,
+              search_query: searchQuery,
             },
           },
         }),
       );
     }
   };
+
+  useEffect(() => {
+    const searchQuery = searchParams.get('q') ?? undefined;
+    const timer = setTimeout(() => {
+      if (
+        user?.accessToken !== undefined &&
+        searchQuery !== undefined &&
+        searchQuery.trim() !== ''
+      ) {
+        dispatch(
+          loadingFeeds({
+            accessToken: user?.accessToken,
+            params: {
+              query: {
+                limit: searchLimit,
+                search_query: searchQuery,
+                data_type:
+                  getDataTypeParamFromSelectedFeedTypes(selectedFeedTypes),
+              },
+            },
+          }),
+        );
+      }
+    }, 400);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchLimit, searchParams, selectedFeedTypes]);
 
   return (
     <Container component='main' sx={{ width: '100vw', m: 0 }}>
@@ -65,7 +126,7 @@ export default function Feed(): React.ReactElement {
       >
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography sx={{ typography: { xs: 'h4', sm: 'h3' } }}>
+            <Typography sx={{ typography: { xs: 'h3', sm: 'h3' } }}>
               Feeds
             </Typography>
           </Grid>
@@ -74,11 +135,11 @@ export default function Feed(): React.ReactElement {
               sx={{
                 width: '90vw',
               }}
-              value={searchText}
+              value={searchParams.get('q') ?? ''}
               placeholder='Transit provider, feed name, or location'
               onChange={(e) => {
-                setSearchText(e.target.value);
-                // handleSearch();
+                const searchValue = e.target.value;
+                setSearchParams({ q: searchValue });
               }}
               InputProps={{
                 startAdornment: (
@@ -116,34 +177,83 @@ export default function Feed(): React.ReactElement {
                     <div>Data Type</div>
                     <FormGroup>
                       <FormControlLabel
-                        control={<Checkbox defaultChecked />}
+                        control={
+                          <Checkbox
+                            checked={selectedFeedTypes.gtfs}
+                            onChange={(e) => {
+                              setSelectedFeedTypes({
+                                ...selectedFeedTypes,
+                                gtfs: e.target.checked,
+                              });
+                            }}
+                          />
+                        }
                         label='GTFS Schedule'
                       />
                       <FormControlLabel
-                        control={<Checkbox defaultChecked />}
+                        control={
+                          <Checkbox
+                            checked={selectedFeedTypes.gtfs_rt}
+                            onChange={(e) => {
+                              setSelectedFeedTypes({
+                                ...selectedFeedTypes,
+                                gtfs_rt: e.target.checked,
+                              });
+                            }}
+                          />
+                        }
                         label='GTFS Realtime'
                       />
                     </FormGroup>
                   </div>
                 </Grid>
-                <Grid xs={12} sm={9}>
-                  {feedsData !== undefined && (
-                    <div>1-10 of {feedsData.total} results</div>
-                  )}
-                  {feedsData?.results?.map(
-                    (result: GTFSFeedType | GTFSRTFeedType, index) => {
-                      return (
-                        <SearchResultItem
-                          key={'search-result-' + index}
-                          result={result}
-                        />
-                      );
-                    },
-                  )}
-                  <div>
-                    <Button>Load {} more feeds </Button>
-                  </div>
-                </Grid>
+                {feedsData?.total !== undefined && (
+                  <Grid container spacing={2} xs={12} sm={9}>
+                    <Grid item xs={12}>
+                      {feedsData.total > 0 && '1-'}
+                      {feedsData.total !== undefined &&
+                      feedsData.total < searchLimit
+                        ? feedsData.total
+                        : searchLimit}{' '}
+                      of {feedsData.total} results
+                    </Grid>
+                    <Grid container spacing={2} xs={12}>
+                      {feedsData?.results?.map(
+                        (result: GTFSFeedType | GTFSRTFeedType, index) => {
+                          if (result === undefined) return <></>;
+                          return (
+                            <Grid
+                              item
+                              key={'search-result-' + index}
+                              width={{ xs: '100%', sm: '80%' }}
+                              onClick={() => {
+                                navigate(`/feeds/${result.id}`);
+                              }}
+                              sx={{
+                                cursor: 'pointer',
+                              }}
+                            >
+                              <SearchResultItem result={result} />
+                            </Grid>
+                          );
+                        },
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Button
+                        disabled={
+                          feedsData?.total !== undefined &&
+                          searchLimit >= feedsData?.total
+                        }
+                        onClick={() => {
+                          setSearchLimit(searchLimit + SEARCH_LIMIT_INCREASE);
+                        }}
+                      >
+                        Load {SEARCH_LIMIT_INCREASE} more feeds
+                      </Button>
+                    </Grid>
+                  </Grid>
+                )}
               </Grid>
             </Box>
           </Grid>
