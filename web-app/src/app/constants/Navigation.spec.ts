141733e456850c0d8bff1a12805100ec2da9b4dd
Alessandro Kreslin <kalessandro14@gmail.com>
2024-06-17 15:59:45 -0400

    test fix

--- web-app/src/app/constants/Navigation.spec.ts
+++ web-app/src/app/constants/Navigation.spec.ts
@@ -4,30 +4,35 @@ import {
 } from '../interface/RemoteConfig';
 import { buildNavigationItems } from './Navigation';
 
-describe('Navigation Elements', () => {
-  describe('building navigation items', () => {
-    it('should return feed nav item if feature flag enabled', () => {
-      const featureFlags: RemoteConfigValues = {
-        ...defaultRemoteConfigValues,
-        enableFeedsPage: true,
-      };
-      const navigationItems = buildNavigationItems(featureFlags);
-      const feedsNavigation = navigationItems.find(
-        (item) => item.title === 'Feeds',
-      );
-      expect(feedsNavigation).toBeDefined();
-    });
+jest.mock('firebase/compat/app', () => ({
+  initializeApp: jest.fn(),
+  remoteConfig: jest.fn(() => ({
+    settings: { minimumFetchIntervalMillis: 3600000 },
+  })),
+}));
 
-    it('should not return feed nav item if feature flag disabled', () => {
-      const featureFlags: RemoteConfigValues = {
-        ...defaultRemoteConfigValues,
-        enableFeedsPage: false,
-      };
-      const navigationItems = buildNavigationItems(featureFlags);
-      const feedsNavigation = navigationItems.find(
-        (item) => item.title === 'Feeds',
-      );
-      expect(feedsNavigation).toBeUndefined();
-    });
+describe('Navigation Elements', () => {
+  it('should return feed nav item if feature flag enabled', () => {
+    const featureFlags: RemoteConfigValues = {
+      ...defaultRemoteConfigValues,
+      enableFeedsPage: true,
+    };
+    const navigationItems = buildNavigationItems(featureFlags);
+    const feedsNavigation = navigationItems.find(
+      (item) => item.title === 'Feeds',
+    );
+    expect(feedsNavigation).toBeDefined();
+  });
+
+  it('should not return feed nav item if feature flag disabled', () => {
+    const featureFlags: RemoteConfigValues = {
+      ...defaultRemoteConfigValues,
+      enableFeedsPage: false,
+    };
+    const navigationItems = buildNavigationItems(featureFlags);
+    const feedsNavigation = navigationItems.find(
+      (item) => item.title === 'Feeds',
+    );
+    expect(feedsNavigation).toBeUndefined();
   });
 });
