e439465217a84951ea381e2aa3552b848dd6c1b8
cka-y <chaimaa@mobilitydata.org>
2024-06-21 11:59:49 -0400

    poc: ts write to sheet

---
+++ functions/packages/write-to-sheet/src/index.ts
@@ -0,0 +1,61 @@
+import { initializeApp } from 'firebase-admin/app';
+import { onRequest } from 'firebase-functions/v2/https';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import {JWT} from "google-auth-library";
+// import creds from '../CREDENTIALS.json'; // the file saved above
+
+const SCOPES = [
+  'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive.file',
+];
+
+
+
+initializeApp();
+
+// Replace with your spreadsheet ID
+const spreadsheetId = '1fKrZqpRl7fRbSn1-0Ewx7fNmbO2G_GpLpVpuvdw3OHU';
+
+export const writeToSheet = onRequest(
+  {
+    cors: '*',
+    region: 'northamerica-northeast1',
+  },
+  async (req, res) => {
+    try {
+      // console.log('Creds', creds);
+      // console.log('Creds', creds.client_email);
+      // console.log('Creds', creds.private_key);
+      // Load the credentials from the environment variable
+      // const adcAuth = new GoogleAuth({
+      //   scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file'],
+      // });
+      const jwt = new JWT({
+        email: "",
+        key: "TODO: replace with private key",
+        scopes: SCOPES,
+      });
+      console.log('jwt', jwt);
+      // const creds = await promisify(fs.readFile)(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf8');
+
+      const doc = new GoogleSpreadsheet(spreadsheetId, jwt);
+
+      // Create a new sheet (tab)
+      const newSheetTitle = 'NewSheetTitle';
+      const newSheet = await doc.addSheet({ title: newSheetTitle, headerValues: ['Name', 'Age', 'City'] });
+
+      // Write data to the new sheet
+      const rows = [
+        { Name: 'John Doe', Age: '30', City: 'New York' },
+        { Name: 'Jane Smith', Age: '25', City: 'Los Angeles' },
+      ];
+
+      await newSheet.addRows(rows);
+
+      res.status(200).send('Data written to the new sheet successfully!');
+    } catch (error) {
+      console.error('Error writing to sheet:', error);
+      res.status(500).send('An error occurred while writing to the sheet.');
+    }
+  }
+);
