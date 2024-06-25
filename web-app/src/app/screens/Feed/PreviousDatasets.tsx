df10ea29c0824ca475f3393f33e27fe227515731
Indraneel Purohit <indraneel.purohit@twosigma.com>
2024-06-25 14:58:45 -0400

    update dataset history to match new design
    for #465

--- web-app/src/app/screens/Feed/PreviousDatasets.tsx
+++ web-app/src/app/screens/Feed/PreviousDatasets.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import { ContentBox } from '../../components/ContentBox';
 import {
+  Button,
   Chip,
   TableBody,
   TableCell,
   TableContainer,
   TableRow,
+  Typography,
   colors,
 } from '@mui/material';
 import {
   DownloadOutlined,
   ErrorOutlineOutlined,
-  OpenInNewOutlined,
   ReportOutlined,
-  ReportProblemOutlined,
+  LaunchOutlined,
+  CheckCircle,
 } from '@mui/icons-material';
 import { type paths } from '../../services/feeds/types';
 
@@ -27,85 +29,152 @@ export default function PreviousDatasets({
   datasets,
 }: PreviousDatasetsProps): React.ReactElement {
   return (
-    <ContentBox
-      width={{ xs: '100%' }}
-      title={'Previous Datasets'}
-      outlineColor={colors.indigo[500]}
-    >
-      <TableContainer>
-        <TableBody>
-          {datasets?.map((dataset) => (
-            <TableRow key={dataset.id}>
-              {dataset.downloaded_at != null && (
+    <>
+      <Typography sx={{ fontSize: { xs: 18, sm: 24 }, fontWeight: 'bold' }}>
+        Dataset History
+      </Typography>
+      <Typography>
+        The Mobility Database fetches and stores new datasets once a week, at
+        11:59 EST on Sundays.{' '}
+      </Typography>
+      <Typography>
+        {datasets !== undefined && datasets.length > 0 && (
+          <div>
+            1-{datasets.length < 10 ? datasets.length : 10} of {datasets.length}{' '}
+            {'Datasets >'}
+          </div>
+        )}
+      </Typography>
+      <ContentBox
+        width={{ xs: '100%' }}
+        title={''}
+        outlineColor={colors.indigo[500]}
+      >
+        <TableContainer>
+          <TableBody sx={{ display: 'inline-table', width: '100%' }}>
+            {datasets?.map((dataset) => (
+              <TableRow key={dataset.id}>
+                {dataset.downloaded_at != null && (
+                  <TableCell>
+                    {new Date(dataset.downloaded_at).toDateString()}
+                  </TableCell>
+                )}
                 <TableCell>
-                  {new Date(dataset.downloaded_at).toDateString()}
+                  <span style={{ display: 'flex' }}>
+                    <a href={dataset.hosted_url}>Download Dataset</a>
+                    <DownloadOutlined />
+                  </span>
                 </TableCell>
-              )}
-              <TableCell>
-                <span style={{ display: 'flex' }}>
-                  <a href={dataset.hosted_url}>Download Dataset</a>
-                  <DownloadOutlined />
-                </span>
-              </TableCell>
-              <TableCell>
-                <div>
-                  <Chip
-                    icon={<ReportOutlined />}
-                    label={`${
-                      dataset?.validation_report?.total_error ?? '0'
-                    } Error`}
-                    color='error'
-                    variant='outlined'
-                  />
-                  <Chip
-                    icon={<ReportProblemOutlined />}
-                    label={`${
-                      dataset?.validation_report?.total_warning ?? '0'
-                    } Warning`}
-                    color='warning'
-                    variant='outlined'
-                  />
-                  <Chip
-                    icon={<ErrorOutlineOutlined />}
-                    label={`${
-                      dataset?.validation_report?.total_info ?? '0'
-                    } Info Notices`}
-                    color='primary'
-                    variant='outlined'
-                  />
-                </div>
-              </TableCell>
-              {dataset.validation_report != null &&
-                dataset.validation_report !== undefined && (
-                  <TableCell>
-                    <span style={{ display: 'flex' }}>
-                      <a
-                        href={`${dataset?.validation_report?.url_html}`}
-                        target='_blank'
-                        rel='noreferrer'
+                <TableCell>
+                  {dataset.validation_report !== null &&
+                    dataset.validation_report !== undefined && (
+                      <>
+                        <Chip
+                          icon={
+                            dataset?.validation_report?.total_error !==
+                              undefined &&
+                            dataset?.validation_report?.total_error > 0 ? (
+                              <ReportOutlined />
+                            ) : (
+                              <CheckCircle />
+                            )
+                          }
+                          label={
+                            dataset?.validation_report?.total_error !==
+                              undefined &&
+                            dataset?.validation_report?.total_error > 0
+                              ? `${dataset?.validation_report?.total_error} errors`
+                              : `No errors`
+                          }
+                          color={
+                            dataset?.validation_report?.total_error !==
+                              undefined &&
+                            dataset?.validation_report?.total_error > 0
+                              ? 'error'
+                              : 'success'
+                          }
+                          variant='outlined'
+                        />
+                        <Chip
+                          icon={
+                            dataset?.validation_report?.total_warning !==
+                              undefined &&
+                            dataset?.validation_report?.total_warning > 0 ? (
+                              <ReportOutlined />
+                            ) : (
+                              <CheckCircle />
+                            )
+                          }
+                          label={
+                            dataset?.validation_report?.total_warning !==
+                              undefined &&
+                            dataset?.validation_report?.total_warning > 0
+                              ? `${dataset?.validation_report?.total_warning} warnings`
+                              : `No warnings`
+                          }
+                          color={
+                            dataset?.validation_report?.total_warning !==
+                              undefined &&
+                            dataset?.validation_report?.total_warning > 0
+                              ? 'warning'
+                              : 'success'
+                          }
+                          variant='outlined'
+                        />
+                        <Chip
+                          icon={<ErrorOutlineOutlined />}
+                          label={`${
+                            dataset?.validation_report?.total_info ?? '0'
+                          } Info Notices`}
+                          color='primary'
+                          variant='outlined'
+                        />
+                      </>
+                    )}
+                </TableCell>
+                <TableCell>
+                  {dataset.validation_report != null &&
+                    dataset.validation_report !== undefined && (
+                      <Button
+                        variant='contained'
+                        sx={{ m: 2 }}
+                        endIcon={<LaunchOutlined />}
                       >
-                        Open Full Report
-                      </a>
-                      <OpenInNewOutlined />
-                    </span>
-                  </TableCell>
-                )}
-              {dataset.validation_report != null &&
-                dataset.validation_report !== undefined && (
-                  <TableCell>
-                    <a
-                      href={`${dataset?.validation_report?.url_json}`}
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Open JSON Report <OpenInNewOutlined />
-                    </a>
-                  </TableCell>
-                )}
-            </TableRow>
-          ))}
-        </TableBody>
-      </TableContainer>
-    </ContentBox>
+                        <a
+                          href={`${dataset?.validation_report?.url_html}`}
+                          target='_blank'
+                          className='btn-link'
+                          rel='noreferrer'
+                        >
+                          View Report
+                        </a>
+                      </Button>
+                    )}
+                </TableCell>
+                <TableCell>
+                  {dataset.validation_report != null &&
+                    dataset.validation_report !== undefined && (
+                      <Button
+                        variant='contained'
+                        sx={{ m: 2 }}
+                        endIcon={<LaunchOutlined />}
+                      >
+                        <a
+                          href={`${dataset?.validation_report?.url_json}`}
+                          target='_blank'
+                          className='btn-link'
+                          rel='noreferrer'
+                        >
+                          JSON Version
+                        </a>
+                      </Button>
+                    )}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TableContainer>
+      </ContentBox>
+    </>
   );
 }
