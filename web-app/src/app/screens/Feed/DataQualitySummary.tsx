9281bd8a44e88b3de70912980467bd982317dc9a
Indraneel Purohit <indraneel.purohit@twosigma.com>
2024-06-25 14:59:05 -0400

    update dataset quality summary to match new design
    #464

--- web-app/src/app/screens/Feed/DataQualitySummary.tsx
+++ web-app/src/app/screens/Feed/DataQualitySummary.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-import { ContentBox } from '../../components/ContentBox';
-import {
-  Chip,
-  TableCell,
-  TableContainer,
-  TableRow,
-  colors,
-} from '@mui/material';
+import { Button, Chip, Grid } from '@mui/material';
 
 import {
+  CheckCircle,
   ErrorOutlineOutlined,
-  OpenInNewOutlined,
+  LaunchOutlined,
   ReportOutlined,
-  ReportProblemOutlined,
 } from '@mui/icons-material';
 import { type components } from '../../services/feeds/types';
 
@@ -23,72 +16,127 @@ export interface DataQualitySummaryProps {
 export default function DataQualitySummary({
   latestDataset,
 }: DataQualitySummaryProps): React.ReactElement {
+  if (
+    latestDataset?.validation_report === undefined ||
+    latestDataset.validation_report === null
+  )
+    return <></>;
   return (
-    <ContentBox
-      width={{ xs: '100%', md: '50%' }}
-      title={'Data Quality Summary'}
-      outlineColor={colors.indigo[500]}
-    >
-      <TableContainer>
-        <TableRow>
-          <TableCell>
-            <Chip
-              icon={<ReportOutlined />}
-              label={`${
-                latestDataset?.validation_report?.total_error ?? '0'
-              } Error`}
-              color='error'
-              variant='outlined'
-            />
-            <Chip
-              icon={<ReportProblemOutlined />}
-              label={`${
-                latestDataset?.validation_report?.total_warning ?? '0'
-              } Warning`}
-              color='warning'
-              variant='outlined'
-            />
-            <Chip
-              icon={<ErrorOutlineOutlined />}
-              label={`${
-                latestDataset?.validation_report?.total_info ?? '0'
-              } Info Notices`}
-              color='primary'
-              variant='outlined'
-            />
-          </TableCell>
-        </TableRow>
-        <TableRow>
+    <>
+      <div>Data Quality Summary</div>
+      <Grid container>
+        <Grid
+          item
+          sx={{
+            alignContent: 'center',
+          }}
+        >
+          <Chip
+            icon={
+              latestDataset?.validation_report?.total_error !== undefined &&
+              latestDataset?.validation_report?.total_error > 0 ? (
+                <ReportOutlined />
+              ) : (
+                <CheckCircle />
+              )
+            }
+            label={
+              latestDataset?.validation_report?.total_error !== undefined &&
+              latestDataset?.validation_report?.total_error > 0
+                ? `${latestDataset?.validation_report?.total_error} errors`
+                : `No errors`
+            }
+            color={
+              latestDataset?.validation_report?.total_error !== undefined &&
+              latestDataset?.validation_report?.total_error > 0
+                ? 'error'
+                : 'success'
+            }
+            variant='outlined'
+          />
+        </Grid>
+        <Grid
+          item
+          sx={{
+            alignContent: 'center',
+          }}
+        >
+          <Chip
+            icon={
+              latestDataset?.validation_report?.total_warning !== undefined &&
+              latestDataset?.validation_report?.total_warning > 0 ? (
+                <ReportOutlined />
+              ) : (
+                <CheckCircle />
+              )
+            }
+            label={
+              latestDataset?.validation_report?.total_warning !== undefined &&
+              latestDataset?.validation_report?.total_warning > 0
+                ? `${latestDataset?.validation_report?.total_warning} warnings`
+                : `No warnings`
+            }
+            color={
+              latestDataset?.validation_report?.total_warning !== undefined &&
+              latestDataset?.validation_report?.total_warning > 0
+                ? 'warning'
+                : 'success'
+            }
+            variant='outlined'
+          />
+        </Grid>
+        <Grid
+          item
+          sx={{
+            alignContent: 'center',
+          }}
+        >
+          <Chip
+            icon={<ErrorOutlineOutlined />}
+            label={`${
+              latestDataset?.validation_report?.total_info ?? '0'
+            } Info Notices`}
+            color='primary'
+            variant='outlined'
+          />
+        </Grid>
+        <Grid item>
           {latestDataset?.validation_report?.url_html !== undefined && (
-            <TableCell>
-              <span style={{ display: 'flex' }}>
-                <a
-                  href={`${latestDataset?.validation_report?.url_html}`}
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  Open Full Report
-                </a>
-                <OpenInNewOutlined />
-              </span>
-            </TableCell>
+            <Button
+              variant='contained'
+              sx={{ m: 2 }}
+              endIcon={<LaunchOutlined />}
+            >
+              <a
+                href={`${latestDataset?.validation_report?.url_html}`}
+                target='_blank'
+                className='btn-link'
+                rel='noreferrer'
+              >
+                Open Full Report
+              </a>
+            </Button>
           )}
+        </Grid>
+        <Grid item>
           {latestDataset?.validation_report?.url_json !== undefined && (
-            <TableCell>
-              <span style={{ display: 'flex' }}>
-                <a
-                  href={`${latestDataset?.validation_report?.url_json}`}
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  Open JSON Report
-                </a>
-                <OpenInNewOutlined />
-              </span>
-            </TableCell>
+            <Button
+              variant='contained'
+              sx={{ m: 2 }}
+              endIcon={<LaunchOutlined />}
+            >
+              <a
+                href={`${latestDataset?.validation_report?.url_json}`}
+                target='_blank'
+                className='btn-link'
+                rel='noreferrer'
+              >
+                Open JSON Report
+              </a>
+            </Button>
           )}
-        </TableRow>
-      </TableContainer>
-    </ContentBox>
+        </Grid>
+      </Grid>
+    </>
   );
 }
