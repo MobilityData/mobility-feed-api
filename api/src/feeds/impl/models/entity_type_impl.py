d0085e52cfb51d675ca2e54e2fc6130728f3e5c0
Jingsi Lu <jingsi@mobilitydata.org>
2024-06-25 11:28:55 -0400

    reformatted entity_type_impl.py

--- api/src/feeds/impl/models/entity_type_impl.py
+++ api/src/feeds/impl/models/entity_type_impl.py
@@ -2,17 +2,21 @@ from enum import Enum
 from typing import Optional
 from database_gen.sqlacodegen_models import Entitytype as EntitytypeOrm
 
+
 class EntitytypeEnum(Enum):
     sa = "sa"
     tu = "tu"
     vp = "vp"
 
+
 Entitytype = str
+
 
 class EntitytypeImpl(Entitytype):
     class Config:
         """Pydantic configuration.
         Enabling `from_orm` method to create a model instance from a SQLAlchemy row object."""
+
         from_attributes = True
         orm_mode = True
 
@@ -20,10 +24,10 @@ class EntitytypeImpl(Entitytype):
     def from_orm(cls, entitytype: Optional[EntitytypeOrm]) -> Optional[Entitytype]:
         if not entitytype:
             return None
-        
+
         try:
             valid_name = EntitytypeEnum(entitytype.name).value
         except ValueError:
             raise ValueError(f"Unknown name: {entitytype.name}")
-        
+
         return valid_name
