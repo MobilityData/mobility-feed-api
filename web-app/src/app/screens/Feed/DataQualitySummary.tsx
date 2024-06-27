dbbc2c5471a0ca27e95ccde927825a9e6f773490
Indraneel Purohit <indraneel.purohit@twosigma.com>
2024-06-27 13:02:30 -0400

    reformat data quality chips + buttons again

--- web-app/src/app/screens/Feed/DataQualitySummary.tsx
+++ web-app/src/app/screens/Feed/DataQualitySummary.tsx
@@ -24,115 +24,119 @@ export default function DataQualitySummary({
   return (
     <>
       <div>Data Quality Summary</div>
-      <Grid container spacing={2} padding={2}>
-        <Grid
-          item
-          sx={{
-            alignContent: 'center',
-          }}
-        >
-          <Chip
-            icon={
-              latestDataset?.validation_report?.unique_error_count !==
-                undefined &&
-              latestDataset?.validation_report?.unique_error_count > 0 ? (
-                <ReportOutlined />
-              ) : (
-                <CheckCircle />
-              )
-            }
-            label={
-              latestDataset?.validation_report?.unique_error_count !==
-                undefined &&
-              latestDataset?.validation_report?.unique_error_count > 0
-                ? `${latestDataset?.validation_report?.unique_error_count} errors`
-                : `No errors`
-            }
-            color={
-              latestDataset?.validation_report?.unique_error_count !==
-                undefined &&
-              latestDataset?.validation_report?.unique_error_count > 0
-                ? 'error'
-                : 'success'
-            }
-            variant='outlined'
-          />
+      <Grid container direction={'column'} spacing={2} padding={2}>
+        <Grid item container direction={'row'} spacing={2} padding={2}>
+          <Grid
+            item
+            sx={{
+              alignContent: 'center',
+            }}
+          >
+            <Chip
+              icon={
+                latestDataset?.validation_report?.unique_error_count !==
+                  undefined &&
+                latestDataset?.validation_report?.unique_error_count > 0 ? (
+                  <ReportOutlined />
+                ) : (
+                  <CheckCircle />
+                )
+              }
+              label={
+                latestDataset?.validation_report?.unique_error_count !==
+                  undefined &&
+                latestDataset?.validation_report?.unique_error_count > 0
+                  ? `${latestDataset?.validation_report?.unique_error_count} errors`
+                  : `No errors`
+              }
+              color={
+                latestDataset?.validation_report?.unique_error_count !==
+                  undefined &&
+                latestDataset?.validation_report?.unique_error_count > 0
+                  ? 'error'
+                  : 'success'
+              }
+              variant='outlined'
+            />
+          </Grid>
+          <Grid
+            item
+            sx={{
+              alignContent: 'center',
+            }}
+          >
+            <Chip
+              icon={
+                latestDataset?.validation_report?.unique_warning_count !==
+                  undefined &&
+                latestDataset?.validation_report?.unique_warning_count > 0 ? (
+                  <ReportOutlined />
+                ) : (
+                  <CheckCircle />
+                )
+              }
+              label={
+                latestDataset?.validation_report?.unique_warning_count !==
+                  undefined &&
+                latestDataset?.validation_report?.unique_warning_count > 0
+                  ? `${latestDataset?.validation_report?.unique_warning_count} warnings`
+                  : `No warnings`
+              }
+              color={
+                latestDataset?.validation_report?.unique_warning_count !==
+                  undefined &&
+                latestDataset?.validation_report?.unique_warning_count > 0
+                  ? 'warning'
+                  : 'success'
+              }
+              variant='outlined'
+            />
+          </Grid>
+          <Grid
+            item
+            sx={{
+              alignContent: 'center',
+            }}
+          >
+            <Chip
+              icon={<ErrorOutlineOutlined />}
+              label={`${
+                latestDataset?.validation_report?.unique_info_count ?? '0'
+              } Info Notices`}
+              color='primary'
+              variant='outlined'
+            />
+          </Grid>
         </Grid>
-        <Grid
-          item
-          sx={{
-            alignContent: 'center',
-          }}
-        >
-          <Chip
-            icon={
-              latestDataset?.validation_report?.unique_warning_count !==
-                undefined &&
-              latestDataset?.validation_report?.unique_warning_count > 0 ? (
-                <ReportOutlined />
-              ) : (
-                <CheckCircle />
-              )
-            }
-            label={
-              latestDataset?.validation_report?.unique_warning_count !==
-                undefined &&
-              latestDataset?.validation_report?.unique_warning_count > 0
-                ? `${latestDataset?.validation_report?.unique_warning_count} warnings`
-                : `No warnings`
-            }
-            color={
-              latestDataset?.validation_report?.unique_warning_count !==
-                undefined &&
-              latestDataset?.validation_report?.unique_warning_count > 0
-                ? 'warning'
-                : 'success'
-            }
-            variant='outlined'
-          />
-        </Grid>
-        <Grid
-          item
-          sx={{
-            alignContent: 'center',
-          }}
-        >
-          <Chip
-            icon={<ErrorOutlineOutlined />}
-            label={`${
-              latestDataset?.validation_report?.unique_info_count ?? '0'
-            } Info Notices`}
-            color='primary'
-            variant='outlined'
-          />
-        </Grid>
-        <Grid item>
-          {latestDataset?.validation_report?.url_html !== undefined && (
-            <Button variant='contained' endIcon={<LaunchOutlined />}>
-              <a
-                href={`${latestDataset?.validation_report?.url_html}`}
-                target='_blank'
-                className='btn-link'
-                rel='noreferrer'
-              >
-                Open Full Report
-              </a>
-            </Button>
-          )}
-        </Grid>
-        <Grid item>
-          {latestDataset?.validation_report?.url_json !== undefined && (
-            <Button variant='contained' endIcon={<LaunchOutlined />}>
-              <a
-                href={`${latestDataset?.validation_report?.url_json}`}
-                target='_blank'
-                className='btn-link'
-                rel='noreferrer'
-              >
-                Open JSON Report
-              </a>
-            </Button>
-          )}
+        <Grid item container direction={'row'} spacing={2} padding={2}>
+          <Grid item>
+            {latestDataset?.validation_report?.url_html !== undefined && (
+              <Button variant='contained' endIcon={<LaunchOutlined />}>
+                <a
+                  href={`${latestDataset?.validation_report?.url_html}`}
+                  target='_blank'
+                  className='btn-link'
+                  rel='noreferrer'
+                >
+                  Open Full Report
+                </a>
+              </Button>
+            )}
+          </Grid>
+          <Grid item>
+            {latestDataset?.validation_report?.url_json !== undefined && (
+              <Button variant='contained' endIcon={<LaunchOutlined />}>
+                <a
+                  href={`${latestDataset?.validation_report?.url_json}`}
+                  target='_blank'
+                  className='btn-link'
+                  rel='noreferrer'
+                >
+                  Open JSON Report
+                </a>
+              </Button>
+            )}
+          </Grid>
         </Grid>
       </Grid>
     </>
