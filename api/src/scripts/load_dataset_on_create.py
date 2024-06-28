95483bbb6fe4ae8dea9b7278ad5a3f1220880b73
jcpitre <jcpitre@mobilitydata.org>
2024-06-28 10:35:20 -0400

    Changed so we don't call google cloud when python files are imported.

--- api/src/scripts/load_dataset_on_create.py
+++ api/src/scripts/load_dataset_on_create.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import threading
 import uuid
 from typing import List
 
@@ -11,13 +12,25 @@ from google.cloud.pubsub_v1.futures import Future
 env = os.getenv("ENV", "dev")
 pubsub_topic_name = f"datasets-batch-topic-{env}"
 project_id = f"mobility-feeds-{env}"
-pubsub_client = pubsub_v1.PublisherClient()
+# Lazy create so we won't try to connect to google cloud when the file is imported.
+pubsub_client = None
+
+lock = threading.Lock()
+
+
+def get_pubsub_client():
+    with lock:
+        global pubsub_client
+        if pubsub_client is None:
+            pubsub_client = pubsub_v1.PublisherClient()
+    return pubsub_client
 
 
 def get_topic_path():
     if pubsub_topic_name is None or project_id is None:
         raise ValueError("PUBSUB_TOPIC_NAME and PROJECT_ID must be set in the environment")
-    return pubsub_client.topic_path(project_id, pubsub_topic_name)
+
+    return get_pubsub_client().topic_path(project_id, pubsub_topic_name)
 
 
 def publish_callback(future: Future, stable_id: str, topic_path: str):
@@ -52,7 +65,7 @@ def publish(feed: Feed, topic_path: str):
         "api_key_parameter_name": feed.api_key_parameter_name,
     }
     data_bytes = json.dumps(payload).encode("utf-8")
-    future = pubsub_client.publish(topic_path, data=data_bytes)
+    future = get_pubsub_client().publish(topic_path, data=data_bytes)
     future.add_done_callback(lambda _: publish_callback(future, feed.stable_id, topic_path))
 
 
