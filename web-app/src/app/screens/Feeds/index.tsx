ba41b0837d2d1ea06e5b687f57528625faac665c
Alessandro Kreslin <kalessandro14@gmail.com>
2024-07-03 11:16:29 -0400

    Feat: final feeds page (#514)

--- web-app/src/app/screens/Feeds/index.tsx
+++ web-app/src/app/screens/Feeds/index.tsx
@@ -11,25 +11,25 @@ import {
   FormGroup,
   Grid,
   InputAdornment,
+  Pagination,
+  TableContainer,
   TextField,
   Typography,
+  colors,
 } from '@mui/material';
 import { Search } from '@mui/icons-material';
-
 import '../../styles/SignUp.css';
 import '../../styles/FAQ.css';
 import { selectUserProfile } from '../../store/profile-selectors';
 import { useAppDispatch } from '../../hooks';
 import { loadingFeeds, resetFeeds } from '../../store/feeds-reducer';
-import { selectFeedsData } from '../../store/feeds-selectors';
 import {
-  type GTFSFeedType,
-  type GTFSRTFeedType,
-} from '../../services/feeds/utils';
-import SearchResultItem from './SearchResultItem';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-
-const SEARCH_LIMIT_INCREASE = 20;
+  selectFeedsData,
+  selectFeedsStatus,
+} from '../../store/feeds-selectors';
+import { useSearchParams } from 'react-router-dom';
+import SearchTable from './SearchTable';
+import { Trans, useTranslation } from 'react-i18next';
 
 const getDataTypeParamFromSelectedFeedTypes = (
   selectedFeedTypes: Record<string, boolean>,
@@ -46,136 +46,154 @@ const getDataTypeParamFromSelectedFeedTypes = (
 };
 
 export default function Feed(): React.ReactElement {
-  const navigate = useNavigate();
-
+  const { t } = useTranslation('feeds');
   const [searchParams, setSearchParams] = useSearchParams();
-  const [searchLimit, setSearchLimit] = useState(10);
+  const [searchLimit] = useState(20); // leaving possibility to edit in future
   const [selectedFeedTypes, setSelectedFeedTypes] = useState({
     gtfs: true,
     gtfs_rt: true,
   });
-
+  const [activeSearch, setActiveSearch] = useState('');
+  const [activePagination, setActivePagination] = useState(
+    searchParams.get('o') !== null ? Number(searchParams.get('o')) : 1,
+  );
+  const [triggerSearch, setTriggerSearch] = useState(false);
   const user = useSelector(selectUserProfile);
   const dispatch = useAppDispatch();
   const feedsData = useSelector(selectFeedsData);
+  const feedStatus = useSelector(selectFeedsStatus);
+
+  const getPaginationOffset = (activePagination?: number): number => {
+    const paginationParam =
+      searchParams.get('o') !== null ? Number(searchParams.get('o')) : 1;
+    const pagination = activePagination ?? paginationParam;
+    const paginationOffset = (pagination - 1) * searchLimit;
+    return paginationOffset;
+  };
 
   const handleSearch = (): void => {
-    const searchQuery = searchParams.get('q') ?? undefined;
-    if (
-      user?.accessToken !== undefined &&
-      searchQuery !== undefined &&
-      searchQuery.trim() !== ''
-    ) {
-      setSearchLimit(10);
+    const searchQuery = searchParams.get('q') ?? '';
+    const paginationOffset = getPaginationOffset();
+    if (user?.accessToken !== undefined) {
       dispatch(
         loadingFeeds({
           accessToken: user?.accessToken,
           params: {
             query: {
               limit: searchLimit,
-              offset: 0,
+              offset: paginationOffset,
               search_query: searchQuery,
+              data_type:
+                getDataTypeParamFromSelectedFeedTypes(selectedFeedTypes),
             },
           },
         }),
       );
+      setActiveSearch(searchQuery);
     }
   };
 
   useEffect(() => {
-    const searchQuery = searchParams.get('q') ?? undefined;
-    const timer = setTimeout(() => {
-      if (
-        user?.accessToken !== undefined &&
-        searchQuery !== undefined &&
-        searchQuery.trim() !== ''
-      ) {
-        dispatch(
-          loadingFeeds({
-            accessToken: user?.accessToken,
-            params: {
-              query: {
-                limit: searchLimit,
-                search_query: searchQuery,
-                data_type:
-                  getDataTypeParamFromSelectedFeedTypes(selectedFeedTypes),
-              },
-            },
-          }),
-        );
-      }
-    }, 400);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [searchLimit, searchParams, selectedFeedTypes]);
+    if (user?.accessToken === undefined) {
+      dispatch(resetFeeds());
+    } else {
+      handleSearch();
+    }
+  }, [user?.accessToken]);
 
   useEffect(() => {
-    const searchQuery = searchParams.get('q') ?? undefined;
-    if (
-      user?.accessToken === undefined ||
-      searchQuery === undefined ||
-      searchQuery.trim() === ''
-    ) {
-      dispatch(resetFeeds());
+    if (!triggerSearch) return;
+    handleSearch();
+    setTriggerSearch(false);
+  }, [triggerSearch]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [selectedFeedTypes]);
+
+  const getSearchResultNumbers = (): string => {
+    if (feedsData?.total !== undefined && feedsData?.total > 0) {
+      const offset = getPaginationOffset(activePagination);
+      const limit = offset + searchLimit;
+
+      const startResult = 1 + offset;
+      const endResult = limit > feedsData?.total ? feedsData.total : limit;
+      const totalResults = feedsData?.total ?? '';
+      return t('resultsFor', { startResult, endResult, totalResults });
+    } else {
+      return '';
     }
-    return () => {
-      // reset feeds state on cleanup too
-      dispatch(resetFeeds());
-    };
-  }, []);
+  };
 
   return (
-    <Container component='main' sx={{ width: '100vw', m: 0 }}>
+    <Container component='main'>
       <CssBaseline />
       <Box
         sx={{
           mt: 12,
           display: 'flex',
           flexDirection: 'column',
-          m: 0,
         }}
-        width={{ xs: '100%', sm: '90%' }}
-        margin={{ xs: '0', sm: '80px' }}
+        margin={{ xs: '80px 20px', m: '80px auto' }}
+        maxWidth={{ xs: '100%', m: '1600px' }}
       >
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography sx={{ typography: { xs: 'h3', sm: 'h3' } }}>
-              Feeds
+            <Typography variant='h4' color='primary'>
+              {t('feeds')}
             </Typography>
+            <Typography variant='subtitle1'>{t('searchFor')}</Typography>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              sx={{
-                width: '90vw',
+          <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box
+              component='form'
+              onSubmit={(event) => {
+                event.preventDefault();
+                setActivePagination(1);
+                handleSearch();
               }}
-              value={searchParams.get('q') ?? ''}
-              placeholder='Transit provider, feed name, or location'
-              onChange={(e) => {
-                const searchValue = e.target.value;
-                setSearchParams({ q: searchValue });
-              }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment
-                    style={{
-                      cursor: 'pointer',
-                    }}
-                    onClick={() => {
-                      handleSearch();
-                    }}
-                    position='start'
-                  >
-                    <Search />
-                  </InputAdornment>
-                ),
-              }}
-            />
+              sx={{ display: 'flex', width: '100%', alignItems: 'center' }}
+            >
+              <TextField
+                sx={{
+                  width: 'calc(100% - 100px)',
+                }}
+                value={searchParams.get('q') ?? ''}
+                placeholder={t('searchPlaceholder')}
+                onChange={(e) => {
+                  const searchValue = e.target.value;
+                  setSearchParams({ q: searchValue });
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment
+                      style={{
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => {
+                        setActivePagination(1);
+                        handleSearch();
+                      }}
+                      position='start'
+                    >
+                      <Search />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Button
+                variant='contained'
+                type='submit'
+                sx={{ m: 1, height: '55px' }}
+              >
+                {t('common:search')}
+              </Button>
+            </Box>
           </Grid>
+
           <Grid item xs={12}>
             <Box
-              width={{ xs: '100vw', sm: '90vw' }}
+              width={'100%'}
               sx={{
                 background: '#F8F5F5',
                 borderRadius: '6px 0px 0px 6px',
@@ -186,16 +204,18 @@ export default function Feed(): React.ReactElement {
                 mr: 0,
               }}
             >
-              <Grid container spacing={2}>
-                <Grid xs={12} sm={3}>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={2}>
                   <div>
-                    <div>Data Type</div>
+                    <div>{t('dataType')}</div>
                     <FormGroup>
                       <FormControlLabel
                         control={
                           <Checkbox
                             checked={selectedFeedTypes.gtfs}
                             onChange={(e) => {
+                              setSearchParams({ q: activeSearch });
+                              setActivePagination(1);
                               setSelectedFeedTypes({
                                 ...selectedFeedTypes,
                                 gtfs: e.target.checked,
@@ -203,13 +223,15 @@ export default function Feed(): React.ReactElement {
                             }}
                           />
                         }
-                        label='GTFS Schedule'
+                        label={t('common:gtfsSchedule')}
                       />
                       <FormControlLabel
                         control={
                           <Checkbox
                             checked={selectedFeedTypes.gtfs_rt}
                             onChange={(e) => {
+                              setSearchParams({ q: activeSearch });
+                              setActivePagination(1);
                               setSelectedFeedTypes({
                                 ...selectedFeedTypes,
                                 gtfs_rt: e.target.checked,
@@ -217,59 +239,107 @@ export default function Feed(): React.ReactElement {
                             }}
                           />
                         }
-                        label='GTFS Realtime'
+                        label={t('common:gtfsRealtime')}
                       />
                     </FormGroup>
                   </div>
                 </Grid>
-                {feedsData?.total !== undefined && (
-                  <Grid container spacing={2} xs={12} sm={9}>
+
+                {/* Content Area */}
+                <Grid item xs={12} sm={10}>
+                  {feedStatus === 'loading' && (
                     <Grid item xs={12}>
-                      {feedsData.total > 0 && '1-'}
-                      {feedsData.total !== undefined &&
-                      feedsData.total < searchLimit
-                        ? feedsData.total
-                        : searchLimit}{' '}
-                      of {feedsData.total} results
+                      <h3>{t('common:loading')}</h3>
                     </Grid>
-                    <Grid container spacing={2} xs={12}>
-                      {feedsData?.results?.map(
-                        (result: GTFSFeedType | GTFSRTFeedType, index) => {
-                          if (result === undefined) return <></>;
-                          return (
-                            <Grid
-                              item
-                              key={'search-result-' + index}
-                              width={{ xs: '100%', sm: '80%' }}
-                              onClick={() => {
-                                navigate(`/feeds/${result.id}`);
-                              }}
-                              sx={{
-                                cursor: 'pointer',
-                              }}
-                            >
-                              <SearchResultItem result={result} />
+                  )}
+
+                  {feedStatus === 'error' && (
+                    <Grid item xs={12}>
+                      <h3>{t('common:errors.generic')}</h3>
+                      <Typography>
+                        <Trans i18nKey='errorAndContact'>
+                          Please check your internet connection and try again.
+                          If the problem persists{' '}
+                          <a href='mailto:api@mobilitydata.org'>contact us</a>{' '}
+                          for for further assistance.
+                        </Trans>
+                      </Typography>
+                    </Grid>
+                  )}
+
+                  {feedsData !== undefined && feedStatus === 'loaded' && (
+                    <>
+                      {feedsData?.results?.length === 0 &&
+                        activeSearch.trim().length > 0 && (
+                          <Grid item xs={12}>
+                            <h3>{t('noResults', { activeSearch })}</h3>
+                            <Typography>{t('searchSuggestions')}</Typography>
+                            <ul>
+                              <li>
+                                <Typography>
+                                  {t('searchTips.twoDigit')}
+                                </Typography>
+                              </li>
+                              <li>
+                                <Typography>
+                                  {t('searchTips.fullName')}
+                                </Typography>
+                              </li>
+                              <li>
+                                <Typography>
+                                  {t('searchTips.checkSpelling')}
+                                </Typography>
+                              </li>
+                            </ul>
+                          </Grid>
+                        )}
+                      {feedsData?.results !== undefined &&
+                        feedsData?.results !== null &&
+                        feedsData?.results?.length > 0 && (
+                          <TableContainer>
+                            <Grid item xs={12}>
+                              <Typography
+                                variant='subtitle2'
+                                sx={{ fontWeight: 'bold' }}
+                                gutterBottom
+                              >
+                                {getSearchResultNumbers()}
+                              </Typography>
                             </Grid>
-                          );
-                        },
-                      )}
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Button
-                        variant='contained'
-                        disabled={
-                          feedsData?.total !== undefined &&
-                          searchLimit >= feedsData?.total
-                        }
-                        onClick={() => {
-                          setSearchLimit(searchLimit + SEARCH_LIMIT_INCREASE);
-                        }}
-                      >
-                        Load {SEARCH_LIMIT_INCREASE} more feeds
-                      </Button>
-                    </Grid>
-                  </Grid>
-                )}
+
+                            <SearchTable feedsData={feedsData} />
+
+                            <Pagination
+                              sx={{
+                                mt: 2,
+                                button: {
+                                  backgroundColor: 'white',
+                                  color: colors.blue[700],
+                                },
+                              }}
+                              color='primary'
+                              defaultPage={activePagination}
+                              shape='rounded'
+                              count={
+                                feedsData.total !== undefined
+                                  ? Math.ceil(feedsData.total / searchLimit)
+                                  : 1
+                              }
+                              onChange={(event, value) => {
+                                event.preventDefault();
+                                setSearchParams({
+                                  q: activeSearch,
+                                  o: String(value),
+                                });
+                                setActivePagination(value);
+                                setTriggerSearch(true);
+                              }}
+                            />
+                          </TableContainer>
+                        )}
+                    </>
+                  )}
+                </Grid>
               </Grid>
             </Box>
           </Grid>
