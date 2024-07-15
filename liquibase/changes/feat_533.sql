226536a8ff1b25d23f1d5f1fdd6d4e21876b04b6
cka-y <chaimaa@mobilitydata.org>
2024-07-15 12:46:49 -0400

    fix: added not null + default value

--- liquibase/changes/feat_533.sql
+++ liquibase/changes/feat_533.sql
@@ -1,2 +1,2 @@
--- Adding created_at column to Feed table with default value of current timestamp for new records
-ALTER TABLE Feed ADD COLUMN created_at TIMESTAMPTZ NULL;
+-- Adding created_at column to Feed table with default value and not null constraint
+ALTER TABLE Feed ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT '2024-02-08 00:00:00.000000';
