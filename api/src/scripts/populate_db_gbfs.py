e48a8ba31182b5c1f9a789d1a3f26ade5b460e7b
cka-y <chaimaa@mobilitydata.org>
2024-08-14 14:44:06 -0400

    feat: added supported versions to doc

--- api/src/scripts/populate_db_gbfs.py
+++ api/src/scripts/populate_db_gbfs.py
@@ -9,26 +9,7 @@ from scripts.gbfs_utils.comparison import generate_system_csv_from_db, compare_d
 from scripts.gbfs_utils.fetching import fetch_data, get_data_content, get_gbfs_versions
 from scripts.gbfs_utils.license import get_license_url
 from scripts.populate_db import DatabasePopulateHelper, set_up_configs
-
-OFFICIAL_VERSIONS = [
-    "1.0",
-    "1.1-RC",
-    "1.1",
-    "2.0-RC",
-    "2.0",
-    "2.1-RC",
-    "2.1-RC2",
-    "2.1",
-    "2.2-RC",
-    "2.2",
-    "2.3-RC",
-    "2.3-RC2",
-    "2.3",
-    "3.0-RC",
-    "3.0-RC2",
-    "3.0",
-    "3.1-RC",
-]
+from scripts.gbfs_utils.gbfs_versions import OFFICIAL_VERSIONS
 
 
 class GBFSDatabasePopulateHelper(DatabasePopulateHelper):
