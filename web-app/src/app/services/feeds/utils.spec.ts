995526bded775cace92c319f0d437e0e67bc4b41
Alessandro Kreslin <kalessandro14@gmail.com>
2024-10-02 12:56:39 -0400

    lint

--- web-app/src/app/services/feeds/utils.spec.ts
+++ web-app/src/app/services/feeds/utils.spec.ts
@@ -54,17 +54,22 @@ describe('Feeds Utils', () => {
     );
   });
 
-  /* eslint-disable max-len */
   it.each([
-    [true,  'https://gtfs.translink.ca/v2/gtfsposition'],
+    [true, 'https://gtfs.translink.ca/v2/gtfsposition'],
     [false, 'gtfs.translink.ca/v2/gtfsposition'],
-    [true,  'http://whistler.mapstrat.com/current/gtfrealtime_TripUpdates.bin'],
-    [true,  'http://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb'],
-    [true,  'http://api.tampa.onebusaway.org:8088/trip-updates'],
-    [true,  'https://transitfeeds.com/p/via-metropolitan-transit/62/latest/download'],
+    [true, 'http://whistler.mapstrat.com/current/gtfrealtime_TripUpdates.bin'],
+    [true, 'http://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb'],
+    [true, 'http://api.tampa.onebusaway.org:8088/trip-updates'],
+    [
+      true,
+      'https://transitfeeds.com/p/via-metropolitan-transit/62/latest/download',
+    ],
     [false, '//transitfeeds.com/p/'],
-    [true,  'https://ckan.pbh.gov.br/dataset/77764a7e-63fc-4111-ace3-fb7d3037953a/resource/f0fa78dc-74c3-49fa-8971-c310a76a07fa/download/gtfsfiles.zip'],
-    [true,  'HTTP://gtfs.translink.ca/v2/gtfsposition'],
+    [
+      true,
+      'https://ckan.pbh.gov.br/dataset/77764a7e-63fc-4111-ace3-fb7d3037953a/resource/f0fa78dc-74c3-49fa-8971-c310a76a07fa/download/gtfsfiles.zip',
+    ],
+    [true, 'HTTP://gtfs.translink.ca/v2/gtfsposition'],
     [false, 'http://.'],
     [false, ''],
   ])('should retrun %s when input is %s', (expected, input) => {
