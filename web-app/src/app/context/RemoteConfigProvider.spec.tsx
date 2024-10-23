56f2a9dee63836593dc3f109babcd4d43d0d68df
Alessandro Kreslin <kalessandro14@gmail.com>
2024-10-23 13:38:43 -0400

    error handling

--- web-app/src/app/context/RemoteConfigProvider.spec.tsx
+++ web-app/src/app/context/RemoteConfigProvider.spec.tsx
@@ -6,7 +6,12 @@ jest.mock('firebase/compat/app', () => ({
         settings: { minimumFetchIntervalMillis: 3600000 },
     })),
 }));
-describe.only('doesUserHaveBypass', () => {
+describe('doesUserHaveBypass', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should return false if userEmail is null', () => {
     const byPassConfig: ByPassConfig = { regex: ['.*@example.com'] };
     expect(doesUserHaveBypass(byPassConfig, null)).toBe(false);
@@ -45,5 +50,12 @@ describe.only('doesUserHaveBypass', () => {
   it('should return false if user is not in list', () => {
     const byPassConfig: ByPassConfig = { regex: ['alex@example.com', 'bill@example.com'] };
     expect(doesUserHaveBypass(byPassConfig, 'cris@example.com')).toBe(false);
+  });
+
+  it('should not break the function if there is an invalid regex', () => {
+    jest.spyOn(console, 'error')
+    const byPassConfig: ByPassConfig = { regex: ['alex@example.org(', 'bill@example.com'] };
+    expect(doesUserHaveBypass(byPassConfig, 'bill@example.com')).toBe(true);
+    expect(console.error).toHaveBeenCalledTimes(1);
   });
 });
