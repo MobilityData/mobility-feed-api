2c02017de73ff6f0ae4166248f7d5169fd9680c8
cka-y <chaimaa@mobilitydata.org>
2024-11-13 13:38:51 -0500

    fix: missing contact email

--- liquibase/changes/feat_741.sql
+++ liquibase/changes/feat_741.sql
@@ -1,6 +1,7 @@
 CREATE TABLE OfficialStatusHistory(
     is_official BOOLEAN NOT NULL,
     feed_id VARCHAR(255) NOT NULL,
+    contact_email VARCHAR(255) NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     notes VARCHAR(255),
     FOREIGN KEY (feed_id) REFERENCES Feed(id),
