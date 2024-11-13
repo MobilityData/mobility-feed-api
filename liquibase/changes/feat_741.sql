e0b35982eaeb1fc59313bf9bfe6b154c8ef5559c
cka-y <chaimaa@mobilitydata.org>
2024-11-13 13:35:28 -0500

    feat: added "official" status to the DB schema

---
+++ liquibase/changes/feat_741.sql
@@ -0,0 +1,8 @@
+CREATE TABLE OfficialStatusHistory(
+    is_official BOOLEAN NOT NULL,
+    feed_id VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    notes VARCHAR(255),
+    FOREIGN KEY (feed_id) REFERENCES Feed(id),
+    PRIMARY KEY (feed_id, timestamp)
+);
