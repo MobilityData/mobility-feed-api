0c173973e574778dc54b588fbcc74c9466e56204
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2024-12-05 21:18:37 -0500

    fix failing test

--- functions-python/operations_api/tests/feeds_operations/impl/test_feeds_operations_impl_gtfs.py
+++ functions-python/operations_api/tests/feeds_operations/impl/test_feeds_operations_impl_gtfs.py
@@ -150,7 +150,7 @@ async def test_update_gtfs_feed_set_wip_nochange(_, update_request_gtfs_feed):
             .filter(Gtfsfeed.stable_id == feed_mdb_40.stable_id)
             .one()
         )
-        assert db_feed.operational_status == "wip"
+        assert db_feed.operational_status is None
 
 
 @patch("helpers.logger.Logger")
