dc528ebecf91a37d5586ee1036d194bc60960557
jcpitre <jcpitre@mobilitydata.org>
2025-01-23 13:30:12 -0500

    Corrected an error in tests

--- functions-python/export_csv/tests/test_export_csv_main.py
+++ functions-python/export_csv/tests/test_export_csv_main.py
@@ -21,7 +21,7 @@ import main
 # This CSV has been created by running the tests once to extract the resulting csv, then examine the CSV to make sure
 # the data is correct.
 expected_csv = """
-mdb_source_id,data_type,entity_type,location.country_code,location.subdivision_name,location.municipality,provider,name,note,feed_contact_email,static_reference,urls.direct_download,urls.authentication_type,urls.authentication_info,urls.api_key_parameter_name,urls.latest,urls.license,location.bounding_box.minimum_latitude,location.bounding_box.maximum_latitude,location.bounding_box.minimum_longitude,location.bounding_box.maximum_longitude,location.bounding_box.extracted_on,status,features,redirect.id,redirect.comment
+id,data_type,entity_type,location.country_code,location.subdivision_name,location.municipality,provider,name,note,feed_contact_email,static_reference,urls.direct_download,urls.authentication_type,urls.authentication_info,urls.api_key_parameter_name,urls.latest,urls.license,location.bounding_box.minimum_latitude,location.bounding_box.maximum_latitude,location.bounding_box.minimum_longitude,location.bounding_box.maximum_longitude,location.bounding_box.extracted_on,status,features,redirect.id,redirect.comment
 gtfs-0,gtfs,,,,,gtfs-0 Some fake company,gtfs-0 Some fake name,gtfs-0 Some fake note,gtfs-0_some_fake_email@fake.com,,https://gtfs-0_some_fake_producer_url,0,,,https://dataset-1_some_fake_hosted_url,https://gtfs-0_some_fake_license_url,-9.0,9.0,-18.0,18.0,2025-01-12 00:00:00+00:00,active,Route Colors|Shapes,,
 gtfs-1,gtfs,,CA,Quebec,Montreal,gtfs-1 Some fake company,gtfs-1 Some fake name,gtfs-1 Some fake note,gtfs-1_some_fake_email@fake.com,,https://gtfs-1_some_fake_producer_url,0,,,,https://gtfs-1_some_fake_license_url,,,,,,active,,,
 gtfs-2,gtfs,,CA,Quebec,Montreal,gtfs-2 Some fake company,gtfs-2 Some fake name,gtfs-2 Some fake note,gtfs-2_some_fake_email@fake.com,,https://gtfs-2_some_fake_producer_url,0,,,https://dataset-3_some_fake_hosted_url,https://gtfs-2_some_fake_license_url,-9.0,9.0,-18.0,18.0,2025-01-12 00:00:00+00:00,active,Route Colors|Shapes,,
@@ -34,7 +34,6 @@ gtfs-rt-2,gtfs_rt,vp,,,,gtfs-rt-2 Some fake company,gtfs-rt-2 Some fake name,gtf
 
 
 def test_export_csv():
-    main.csv_file_path = "/Users/jcpitre/Google Drive/My Drive/output.csv"
     data_collector = main.collect_data()
     df_extracted = data_collector.get_dataframe()
 
