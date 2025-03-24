5b8200846cc7e0a4ed8ce20e5f4fd4ff8abac3cc
Alessandro Kreslin <kalessandro14@gmail.com>
2025-03-24 08:44:35 -0400

    documentation

--- liquibase/changes/feat_946.sql
+++ liquibase/changes/feat_946.sql
@@ -5,6 +5,7 @@ ADD COLUMN agency_timezone VARCHAR(255),
 ALTER COLUMN service_date_range_start SET DATA TYPE TIMESTAMP WITH TIME ZONE USING service_date_range_start::TIMESTAMP WITH TIME ZONE,
 ALTER COLUMN service_date_range_end SET DATA TYPE TIMESTAMP WITH TIME ZONE USING service_date_range_end::TIMESTAMP WITH TIME ZONE;
 
+-- Added latest_dataset_agency_timezone property to the FeedSearch view
 CREATE MATERIALIZED VIEW FeedSearch AS
 SELECT
     -- feed
