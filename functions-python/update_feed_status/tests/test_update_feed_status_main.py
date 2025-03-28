12b65114abca92f3cc42822625907a78968a4964
Simon Richard <46613478+sylvansson@users.noreply.github.com>
2025-03-28 13:17:10 -0400

    feat: update update_feed_status to return status diff counts (#992)

--- functions-python/update_feed_status/tests/test_update_feed_status_main.py
+++ functions-python/update_feed_status/tests/test_update_feed_status_main.py
@@ -1,31 +1,63 @@
 from unittest.mock import patch, MagicMock
-from test_shared.test_utils.database_utils import default_db_url
-from main import update_feed_status, update_feed_statuses_query
+from test_shared.test_utils.database_utils import default_db_url, get_testing_session
+from main import (
+    update_feed_status,
+    update_feed_statuses_query,
+)
+from shared.database_gen.sqlacodegen_models import Feed
 from datetime import date, timedelta
+from typing import Iterator, NamedTuple
 
 import os
 
+from sqlalchemy import text
+from sqlalchemy.orm import Session
 
-def test_update_feed_status_return():
-    mock_session = MagicMock()
 
-    today = date(2025, 3, 1)
+class PartialFeed(NamedTuple):
+    """
+    Subset of the Feed entity with only the fields queried in `fetch_feeds`.
+    """
 
-    mock_subquery = MagicMock()
-    mock_subquery.c.feed_id = 1
-    mock_subquery.c.service_date_range_start = today - timedelta(days=10)
-    mock_subquery.c.service_date_range_end = today + timedelta(days=10)
+    id: str
+    status: str
 
-    mock_query = mock_session.query.return_value
-    mock_query.filter.return_value.subquery.return_value = mock_subquery
 
-    mock_update_query = mock_session.query.return_value.filter.return_value
-    mock_update_query.update.return_value = 3
+def fetch_feeds(session: Session) -> Iterator[PartialFeed]:
+    # When adding or removing fields here, `PartialFeed` should be updated to
+    # match, for type safety.
+    query = session.query(Feed.id, Feed.status).filter(
+        Feed.status != text("'deprecated'::status"),
+        Feed.status != text("'development'::status"),
+    )
+    for feed in query:
+        yield PartialFeed(id=feed.id, status=feed.status)
 
-    updated_count = update_feed_statuses_query(mock_session)
 
-    assert updated_count == 3
-    mock_session.commit.assert_called_once()
+def test_update_feed_status():
+    session = get_testing_session()
+    feeds_before: dict[str, PartialFeed] = {f.id: f for f in fetch_feeds(session)}
+    result = dict(update_feed_statuses_query(session))
+    assert result == {
+        "inactive": 3,
+        "active": 2,
+        "future": 1,
+    }
+
+    feeds_after: dict[str, PartialFeed] = {f.id: f for f in fetch_feeds(session)}
+    expected_status_changes = {
+        "2": "active",
+        "7": "inactive",
+        "8": "inactive",
+        "10": "future",
+        "22": "inactive",
+        "25": "active",
+    }
+    for feed_id, feed_before in feeds_before.items():
+        feed_after = feeds_after[feed_id]
+        assert feed_after.status == expected_status_changes.get(
+            feed_id, feed_before.status
+        )
 
 
 def test_update_feed_status_failed_query():
@@ -53,13 +85,14 @@ def test_update_feed_status_failed_query():
 @patch("main.Logger", autospec=True)
 @patch("main.update_feed_statuses_query")
 def test_updated_feed_status(mock_update_query, mock_logger):
-    mock_update_query.return_value = 5
+    return_value = {"active": 5}
+    mock_update_query.return_value = return_value
 
     with patch.dict(os.environ, {"FEEDS_DATABASE_URL": default_db_url}):
         response_body, status_code = update_feed_status(None)
 
-    mock_update_query.asser_called_once()
-    assert response_body == "Script executed successfully. 5 feeds updated"
+    mock_update_query.assert_called_once()
+    assert response_body == return_value
     assert status_code == 200
 
 
@@ -71,6 +104,6 @@ def test_updated_feed_status_error_raised(mock_update_query, mock_logger):
     with patch.dict(os.environ, {"FEEDS_DATABASE_URL": default_db_url}):
         response_body, status_code = update_feed_status(None)
 
-    mock_update_query.asser_called_once()
+    mock_update_query.assert_called_once()
     assert response_body == "Error updating the feed statuses: Mocked exception"
     assert status_code == 500
