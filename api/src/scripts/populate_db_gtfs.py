db68857e801c6d686bab1c1aecded0413b58a353
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2025-04-01 09:25:37 -0400

    limiting the length for country code setting

--- api/src/scripts/populate_db_gtfs.py
+++ api/src/scripts/populate_db_gtfs.py
@@ -1,12 +1,14 @@
 import os
 import traceback
-from typing import TYPE_CHECKING
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 import pycountry
 import pytz
 from sqlalchemy import text
 
+from scripts.load_dataset_on_create import publish_all
+from scripts.populate_db import DatabasePopulateHelper, set_up_configs
 from shared.database.database import generate_unique_id, configure_polymorphic_mappers
 from shared.database_gen.sqlacodegen_models import (
     Entitytype,
@@ -16,8 +18,6 @@ from shared.database_gen.sqlacodegen_models import (
     Redirectingid,
     t_feedsearch,
 )
-from scripts.populate_db import DatabasePopulateHelper, set_up_configs
-from scripts.load_dataset_on_create import publish_all
 from utils.data_utils import set_up_defaults
 
 if TYPE_CHECKING:
@@ -90,7 +90,7 @@ class GTFSDatabasePopulateHelper(DatabasePopulateHelper):
                 if location
                 else Location(
                     id=location_id,
-                    country_code=country_code,
+                    country_code=country_code if country_code and len(country_code) <= 3 else None,
                     subdivision_name=subdivision_name,
                     municipality=municipality,
                     country=country,
