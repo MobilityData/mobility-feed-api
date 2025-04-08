c3137af688b6fcaf4bbab96bdcd85273c58a558f
cka-y <chaimaa@mobilitydata.org>
2025-04-08 12:42:43 -0400

    fix: added order on index

--- liquibase/changes/feat_951.sql
+++ liquibase/changes/feat_951.sql
@@ -1,4 +1,4 @@
-CREATE INDEX IF NOT EXISTS idx_feed_osm_location_group_feed_id_stops_count ON feedosmlocationgroup (feed_id, stops_count);
+CREATE INDEX IF NOT EXISTS idx_feed_osm_location_group_feed_id_stops_count ON feedosmlocationgroup (feed_id, stops_count DESC);
 
 CREATE INDEX IF NOT EXISTS idx_feed_osm_location_group_feed_id ON feedosmlocationgroup (feed_id);
 
