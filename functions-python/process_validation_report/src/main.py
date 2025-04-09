20f563af53a007d364b45648e00bb50cc5b8c3cc
Jingsi Lu <jingsi@mobilitydata.org>
2025-04-09 12:23:59 -0400

    removed placeholder

--- functions-python/process_validation_report/src/main.py
+++ functions-python/process_validation_report/src/main.py
@@ -315,7 +315,7 @@ def process_validation_report(request):
 
 
 @functions_framework.http
-def compute_validation_report_counters(_):
+def compute_validation_report_counters():
     """
     Compute the total number of errors, warnings, and info notices,
     as well as the number of distinct codes for each severity level
