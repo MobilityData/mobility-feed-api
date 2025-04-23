dfee1e6dd868a91370b9d67e1756ca8b0fa798c4
Alfred Nwolisa <alfred@mobilitydata.org>
2025-04-23 13:29:18 -0400

    Delete liquibase/changes/feat_1132_2.sql

--- liquibase/changes/feat_1132_2.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-BEGIN
-    REFRESH MATERIALIZED VIEW FeedSearch;
-    RAISE NOTICE 'Refreshed FeedSearch materialized view';
-EXCEPTION
-    WHEN OTHERS THEN
-        RAISE EXCEPTION 'Failed to refresh FeedSearch materialized view: %', SQLERRM;
-END $$;
