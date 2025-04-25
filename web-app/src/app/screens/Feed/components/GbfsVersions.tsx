c8f139c614b7de5a9fa01ae3dfcb48c65a801d03
Alessandro Kreslin <kalessandro14@gmail.com>
2025-04-25 11:59:30 -0400

    gbfs features filter and sort

--- web-app/src/app/screens/Feed/components/GbfsVersions.tsx
+++ web-app/src/app/screens/Feed/components/GbfsVersions.tsx
@@ -1,39 +1,16 @@
 import * as React from 'react';
 import { ContentBox } from '../../../components/ContentBox';
-import {
-  Box,
-  Button,
-  Chip,
-  IconButton,
-  Link,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  Tooltip,
-  Typography,
-  useTheme,
-} from '@mui/material';
-import {
-  DownloadOutlined,
-  ReportOutlined,
-  LaunchOutlined,
-  CheckCircle,
-} from '@mui/icons-material';
-import { type paths } from '../../../services/feeds/types';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import SummarizeIcon from '@mui/icons-material/Summarize';
-import CodeIcon from '@mui/icons-material/Code';
-import DateRangeIcon from '@mui/icons-material/DateRange';
-import { WEB_VALIDATOR_LINK } from '../../../constants/Navigation';
-import { formatServiceDateRange } from '../Feed.functions';
+import { Box, Button, Chip, Link, Typography, useTheme } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ErrorIcon from '@mui/icons-material/Error';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { type GBFSFeedType, type GBFSVersionType } from '../../../services/feeds/utils';
+import {
+  GBFSVersionEndpointType,
+  type GBFSFeedType,
+  type GBFSVersionType,
+} from '../../../services/feeds/utils';
 import { displayFormattedDate } from '../../../utils/date';
 
 export interface GbfsVersionsProps {
@@ -47,9 +24,60 @@ export default function GbfsVersions({
   const { t } = useTranslation('gbfsFeatures');
 
   const sortVersions = (a: GBFSVersionType, b: GBFSVersionType) => {
-    const na = (a.version ?? '0').replace(/[^0-9.]/g, '');
-    const nb = (b.version ?? '0').replace(/[^0-9.]/g, '');
+    const na = String(a.version ?? '0').replace(/[^0-9.]/g, '');
+    const nb = String(b.version ?? '0').replace(/[^0-9.]/g, '');
     return parseFloat(nb) - parseFloat(na);
+  };
+
+  const getGbfsVersionUrl = (version: string, feature: string) => {
+    if (
+      version === '1.1' ||
+      version === '2.0' ||
+      version === '2.1' ||
+      version === '2.2'
+    ) {
+      switch (feature) {
+        case 'gbfs_versions':
+          return `https://github.com/MobilityData/gbfs/blob/v${version}/gbfs.md#gbfs_versionsjson-added-in-v11`;
+        case 'vehicle_types':
+          return `https://github.com/MobilityData/gbfs/blob/v${version}/gbfs.md#vehicle_typesjson-added-in-v21`;
+        case 'geofencing_zones':
+          return `https://github.com/MobilityData/gbfs/blob/v${version}/gbfs.md#geofencing_zonesjson-added-in-v21`;
+      }
+    }
+    return `https://github.com/MobilityData/gbfs/blob/v${version}/gbfs.md#${feature}json`;
+  };
+
+  const getGbfsFeatures = (gbfsVersionElement: GBFSVersionType) => {
+    // group by language
+    let featuresByLanguage = new Map<string, GBFSVersionEndpointType[]>();
+    let maxLanguageElements = 0;
+    let maxLanguage = '';
+    gbfsVersionElement.endpoints?.forEach((endpoint) => {
+      if (
+        endpoint.name != null &&
+        endpoint.is_feature &&
+        endpoint.language != null
+      ) {
+        if (!featuresByLanguage.has(endpoint.language)) {
+          featuresByLanguage.set(endpoint.language, []);
+        }
+        featuresByLanguage.get(endpoint.language)?.push(endpoint);
+        if (
+          featuresByLanguage.get(endpoint.language)?.length ??
+          0 > maxLanguageElements
+        ) {
+          maxLanguageElements =
+            featuresByLanguage.get(endpoint.language)?.length ?? 0;
+          maxLanguage = endpoint.language;
+        }
+      }
+    });
+    return featuresByLanguage.get(maxLanguage) ?? [];
+  };
+
+  if (feed?.versions == null || feed?.versions?.length === 0) {
+    return <></>;
   }
 
   return (
@@ -68,142 +96,153 @@ export default function GbfsVersions({
           justifyContent: 'flex-end',
         }}
       >
-        {feed?.versions?.sort((a, b) => sortVersions(a,b)).map((item, index) => (
-          <ContentBox
-            key={index}
-            title={``}
-            outlineColor={theme.palette.secondary.main}
-            padding={2}
-            sx={{
-              my: 0,
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-between',
-            }}
-            width={{ xs: '100%' }}
-          >
-            <Box>
+        {[...(feed?.versions ?? [])]
+          .sort((a, b) => sortVersions(a, b))
+          .map((item, index) => (
+            <ContentBox
+              key={index}
+              title={``}
+              outlineColor={theme.palette.secondary.main}
+              padding={2}
+              sx={{
+                my: 0,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+              width={{ xs: '100%' }}
+            >
               <Box>
-                <Typography
-                  variant='h5'
-                  sx={{
-                    mb: 1,
-                    fontWeight: 'bold',
-                    display: 'flex',
-                    alignItems: 'center',
-                  }}
-                >
-                  v{item.version}
-                  <Chip
-                    icon={
-                      item.latest_validation_report?.total_error === 0 ? (
-                        <CheckCircleIcon />
-                      ) : (
-                        <ErrorIcon />
-                      )
-                    }
-                    label={
-                      item.latest_validation_report?.total_error != null &&
-                      item.latest_validation_report?.total_error > 0
-                        ? `${item.latest_validation_report?.total_error} errors`
-                        : 'no errors'
-                    }
-                    variant='outlined'
-                    color={
-                      item.latest_validation_report?.total_error != null &&
-                      item.latest_validation_report?.total_error > 0
-                        ? 'error'
-                        : 'success'
-                    }
-                    sx={{ ml: 2 }}
-                    clickable
-                    component={Link}
-                    href={item.latest_validation_report?.report_summary_url}
-                    target='_blank'
-                    rel='noreferrer'
-                  />
-                </Typography>
-                <Typography
-                  variant='caption'
-                  component={'div'}
-                  sx={{ mt: '-2px', mb: 2 }}
-                >
-                  Quality report updated at:{' '}
-                  {displayFormattedDate(
-                    item.latest_validation_report?.validated_at ?? '',
-                  )}
-                </Typography>
-                <Typography variant='h6' sx={{ fontSize: '1.1rem' }}>
-                  Auto-Discovery Url
-                </Typography>
-                <Box
-                  sx={{ display: 'flex', alignItems: 'center', gap: 2, mb: 2 }}
-                >
-                  <Link sx={{wordWrap: 'break-word', wordBreak: 'break-all'}} href='https://data.lime.bike/api/partners/v2/gbfs/louisville/gbfs.json'>https://data.lime.bike/api/partners/v2/gbfs/louisville/gbfs.json</Link>  
-                  {/* TODO: href={item.autoDiscovery}{item.autoDiscovery} */}
-                  <ContentCopyIcon></ContentCopyIcon>
-                </Box>
                 <Box>
-                  <Typography variant='h6' sx={{ mt: 1, fontSize: '1.1rem' }}>
-                    Features
+                  <Typography
+                    variant='h5'
+                    sx={{
+                      mb: 1,
+                      fontWeight: 'bold',
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    v{item.version}
+                    <Chip
+                      icon={
+                        item.latest_validation_report?.total_error === 0 ? (
+                          <CheckCircleIcon />
+                        ) : (
+                          <ErrorIcon />
+                        )
+                      }
+                      label={
+                        item.latest_validation_report?.total_error != null &&
+                        item.latest_validation_report?.total_error > 0
+                          ? `${item.latest_validation_report?.total_error} errors`
+                          : 'no errors'
+                      }
+                      variant='outlined'
+                      color={
+                        item.latest_validation_report?.total_error != null &&
+                        item.latest_validation_report?.total_error > 0
+                          ? 'error'
+                          : 'success'
+                      }
+                      sx={{ ml: 2 }}
+                      clickable
+                      component={Link}
+                      href={item.latest_validation_report?.report_summary_url}
+                      target='_blank'
+                      rel='noreferrer'
+                    />
                   </Typography>
-                  {item.endpoints?.map((endpoint, index) => (
-                    <>
-                    {endpoint.name != null && (
-                      <Chip
-                        component={Link}
-                        key={index}
-                        label={t(endpoint.name)}
-                        color='info'
-                        variant='filled'
-                        sx={{ margin: '4px' }}
-                        clickable
-                        target='_blank'
-                        rel='noreferrer'
-                        href={`https://github.com/MobilityData/gbfs/blob/v${item.version}/gbfs.md#${endpoint.name}json`}
-                        // TODO: ISSUE: some urls are like: https://github.com/MobilityData/gbfs/blob/v2.2/gbfs.md#vehicle_typesjson-added-in-v21
-                        // the 'json-added-in-v21' makes the url unreliable
-                        // it will still go to the page, but not the correct anchor
-                      />
+                  <Typography
+                    variant='caption'
+                    component={'div'}
+                    sx={{ mt: '-2px', mb: 2 }}
+                  >
+                    Quality report updated at:{' '}
+                    {displayFormattedDate(
+                      item.latest_validation_report?.validated_at ?? '',
                     )}
-                    </>
-                    
-                  ))}
+                  </Typography>
+                  <Typography variant='h6' sx={{ fontSize: '1.1rem' }}>
+                    Auto-Discovery Url
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: 2,
+                      mb: 2,
+                    }}
+                  >
+                    <Link
+                      sx={{ wordWrap: 'break-word', wordBreak: 'break-all' }}
+                      href='https://data.lime.bike/api/partners/v2/gbfs/louisville/gbfs.json'
+                    >
+                      https://data.lime.bike/api/partners/v2/gbfs/louisville/gbfs.json
+                    </Link>
+                    {/* TODO: href={item.autoDiscovery}{item.autoDiscovery} */}
+                    {/* <ContentCopyIcon></ContentCopyIcon> */}
+                  </Box>
+                  <Box>
+                    <Typography variant='h6' sx={{ mt: 1, fontSize: '1.1rem' }}>
+                      Features
+                    </Typography>
+                    {getGbfsFeatures(item).map((endpoint, index) => (
+                      <>
+                        {endpoint.name != null && item.version != null && (
+                          <Chip
+                            component={Link}
+                            key={index}
+                            label={t(endpoint.name)}
+                            color='info'
+                            variant='filled'
+                            sx={{ margin: '4px' }}
+                            clickable
+                            target='_blank'
+                            rel='noreferrer'
+                            href={getGbfsVersionUrl(
+                              item.version,
+                              endpoint.name,
+                            )}
+                          />
+                        )}
+                      </>
+                    ))}
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-            <Box
-              sx={{
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'flex-end',
-                gap: 2,
-                mt: 3,
-              }}
-            >
-              <Button
-                variant='text'
-                color='primary'
-                href={''} // TODO: `https://gbfs-validator.mobilitydata.org/validator?url=${item.autoDiscovery}`
-                target='_blank'
-                rel='noreferrer'
-                endIcon={<OpenInNewIcon></OpenInNewIcon>}
+              <Box
+                sx={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  gap: 2,
+                  mt: 3,
+                }}
               >
-                Run Validation Report
-              </Button>
-              <Button
-                variant='contained'
-                color='primary'
-                href='' // TODO: item.autoDiscovery
-                target='_blank'
-                rel='noreferrer'
-                endIcon={<OpenInNewIcon></OpenInNewIcon>}
-              >
-                Open Feed Url
-              </Button>
-            </Box>
-          </ContentBox>
-        ))}
+                <Button
+                  variant='text'
+                  color='primary'
+                  href={''} // TODO: `https://gbfs-validator.mobilitydata.org/validator?url=${item.autoDiscovery}`
+                  target='_blank'
+                  rel='noreferrer'
+                  endIcon={<OpenInNewIcon></OpenInNewIcon>}
+                >
+                  Run Validation Report
+                </Button>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  href='' // TODO: item.autoDiscovery
+                  target='_blank'
+                  rel='noreferrer'
+                  endIcon={<OpenInNewIcon></OpenInNewIcon>}
+                >
+                  Open Feed Url
+                </Button>
+              </Box>
+            </ContentBox>
+          ))}
       </Box>
     </>
   );
