8228751b88b6538be84ab8ff7076332e1651e409
jcpitre <jcpitre@mobilitydata.org>
2025-04-30 14:56:33 -0400

    Test4

--- scripts/duplicate-prod-db/create-dump-bucket.sh
+++ scripts/duplicate-prod-db/create-dump-bucket.sh
@@ -4,7 +4,14 @@
 # It also gives permission to the dump bucket so the SQL instances in PROD and QA can use it.
 
 # Validate required environment variables
-REQUIRED_VARS=("DEST_PROJECT_ID" "DUMP_BUCKET_NAME" "GCP_REGION" "BUCKET_PROJECT_ID" "SOURCE_SQL_SERVICE_ACCOUNT" "DB_INSTANCE_NAME")
+REQUIRED_VARS=(
+  "DEST_PROJECT_ID"
+  "DUMP_BUCKET_NAME"
+  "GCP_REGION"
+  "BUCKET_PROJECT_ID"
+  "SOURCE_SQL_SERVICE_ACCOUNT"
+  "DB_INSTANCE_NAME"
+)
 
 for VAR in "${REQUIRED_VARS[@]}"; do
   if [ -z "${!VAR}" ]; then
@@ -23,12 +30,12 @@ else
   echo "Bucket already exists."
 fi
 
-echo "Giving permission for the source sql instance to write to the bucket"
+echo "Giving permission for the source sql instance to read-write to the bucket"
 gsutil iam ch serviceAccount:$SOURCE_SQL_SERVICE_ACCOUNT:objectAdmin gs://$DUMP_BUCKET_NAME
 
 echo "Getting the service account for the QA DB to give permission to the bucket"
 DEST_SQL_SERVICE_ACCOUNT=$(gcloud sql instances describe $DB_INSTANCE_NAME --format="value(serviceAccountEmailAddress)")
 echo "Destination SQL Service Account: $DEST_SQL_SERVICE_ACCOUNT"
 
-echo "Giving permission on the bucket to the destination sql instance"
+echo "Giving permission for the dest sql instance to read-write to the bucket"
 gsutil iam ch serviceAccount:$DEST_SQL_SERVICE_ACCOUNT:objectAdmin gs://$DUMP_BUCKET_NAME
