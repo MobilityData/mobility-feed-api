9f3160f65c0361af131d1ff07606aa78fce5a6f8
Alessandro Kreslin <kalessandro14@gmail.com>
2025-05-05 11:31:00 -0400

    sketch updates

--- web-app/src/app/screens/GbfsValidator/ValidationReport.tsx
+++ web-app/src/app/screens/GbfsValidator/ValidationReport.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, Link, Typography, useTheme } from '@mui/material';
 import sampleReponse from './sampleResponse.json';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
@@ -33,7 +33,7 @@ export default function ValidationReport(): React.ReactElement {
   const theme = useTheme();
   const validationResult: ValidationResult = sampleReponse as ValidationResult;
   return (
-    <Box sx={{ mt: 5 }}>
+    <Box sx={{ mt: 8 }}>
       <Box
         sx={{
           textAlign: 'center',
@@ -51,7 +51,7 @@ export default function ValidationReport(): React.ReactElement {
         <Typography variant='body1' sx={{ fontWeight: 700, mb: 2, ml: 2 }}>
           Validator Version: {validationResult.summary.validatorVersion}
         </Typography>
-        <Typography variant='body1' sx={{ fontWeight: 700, mb: 2, ml: 2 }}>
+        <Typography variant='body1' sx={{ fontWeight: 700, mb: 2, ml: 2, color: theme.palette.error.main }}>
           Invalid GBFS Feed
         </Typography>
       </Box>
@@ -60,7 +60,7 @@ export default function ValidationReport(): React.ReactElement {
         sx={{
           display: 'flex',
           flexWrap: 'nowrap',
-          height: '700px',
+          height: '900px',
           gap: 2,
           maxWidth: 'lg',
           m: 'auto',
@@ -75,42 +75,48 @@ export default function ValidationReport(): React.ReactElement {
             width: '300px',
           }}
         >
-          <Typography variant='h5' sx={{ fontWeight: 700, mb: 2, ml: 2 }}>
-            Validation Summary
+          <Typography variant='h5' sx={{ fontWeight: 700, p: 2, mb: 2}}>
+            GBFS Feed Files Summary
           </Typography>
-          <Typography variant='body1' sx={{ fontWeight: 700, mb: 2, ml: 2 }}>
-            Total Errors: 3
+          <Typography variant='h6' sx={{  mb: 1, ml: 2 }}>
+            Total Errors: <b>3</b>
           </Typography>
 
           {validationResult.summary.files.map((file: GbfsFile) => {
             const hasErrors = file.errors.length > 0;
             return (
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <Typography>{file.name}</Typography>
-                    {hasErrors ? (
-                    <ErrorOutlineIcon
-                      fontSize='small'
-                      sx={{ color: theme.palette.error.main, mr: 1 }}
-                    />
-                  ) : (
-                    <CheckCircleOutlineIcon
-                      fontSize='small'
-                      sx={{ color: theme.palette.success.main, mr: 1 }}
-                    />
-                  )}
-                </Box>
-            )
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  p: 2,
+                  pb: 1
+                }}
+                key={file.name}
+              >
+                <Typography component={'a'} href='' sx={{color: 'black', textDecoration: 'none'}}>{file.name}</Typography>
+                {hasErrors ? (
+                  <ErrorOutlineIcon
+                    fontSize='small'
+                    sx={{ color: theme.palette.error.main, mr: 1 }}
+                  />
+                ) : (
+                  <CheckCircleOutlineIcon
+                    fontSize='small'
+                    sx={{ color: theme.palette.success.main, mr: 1 }}
+                  />
+                )}
+              </Box>
+            );
           })}
-
         </Box>
-
-
 
         <Box
           sx={{
             height: '100%',
             width: '100%',
-            overflowY: 'scroll',
+            overflowY: 'auto',
             backgroundColor: theme.palette.background.paper,
           }}
         >
@@ -121,16 +127,22 @@ export default function ValidationReport(): React.ReactElement {
                 key={file.name}
                 sx={{
                   backgroundColor: hasErrors
-                    ? theme.palette.error.light
-                    : theme.palette.success.light,
+                    ? ''//theme.palette.error.light
+                    : '#80c883',//theme.palette.success.light,
                   position: 'relative',
                   p: 2,
                 }}
               >
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                  }}
+                >
                   <Typography
                     variant='h5'
-                    sx={{ fontWeight: 700, mb: 0, ml: 2 }}
+                    sx={{ fontWeight: 700, mb: 0, ml: 2, color: hasErrors ? theme.palette.error.dark : theme.palette.success.dark }}
                   >
                     {file.name}.json
                   </Typography>
@@ -150,19 +162,44 @@ export default function ValidationReport(): React.ReactElement {
                 {hasErrors && (
                   <>
                     <Typography
-                      variant='body1'
-                      sx={{ fontWeight: 700, mb: 2, ml: 2 }}
+                      variant='h6'
+                      sx={{ m:1, ml: 2, color: theme.palette.error.dark }}
                     >
                       Errors:
                     </Typography>
-                    {file.errors.map((error) => (
+                    {file.errors.map((error, index) => (
                       <Box key={error.instancePath} sx={{ ml: 4 }}>
-                        <Typography
-                          variant='body1'
-                          sx={{ fontWeight: 700, mb: 2 }}
-                        >
-                          {error.message}
-                        </Typography>
+                        <Box>
+                          <Typography variant={'h6'}>
+                            #{index + 1} - {error.keyword}
+                          </Typography>
+                        </Box>
+                        <Box sx={{ ml: 2 }}>
+                          <Typography
+                            variant='body1'
+                            sx={{ mb: 1 }}
+                          >
+                            <b style={{marginRight: '16px'}}>Message:</b> {error.message}
+                          </Typography>
+                          <Typography
+                            variant='body1'
+                            sx={{ mb: 1 }}
+                          >
+                            <b style={{marginRight: '16px'}}>Instance Path:</b> {error.instancePath}
+                          </Typography>
+                          <Typography
+                            variant='body1'
+                            sx={{ mb: 1 }}
+                          >
+                            <b style={{marginRight: '16px'}}>Schema Path:</b> {error.schemaPath}
+                          </Typography>
+                          <Typography
+                            variant='body1'
+                            sx={{ mb: 1 }}
+                          >
+                            <b style={{marginRight: '16px'}}>Params:</b> {JSON.stringify(error.params)}
+                          </Typography>
+                        </Box>
                       </Box>
                     ))}
                   </>
