cb7e1d4c9d002db905828235427784f8ba493b97
Alessandro Kreslin <kalessandro14@gmail.com>
2025-05-05 11:31:00 -0400

    sketch updates

--- web-app/src/app/screens/GbfsValidator/index.tsx
+++ web-app/src/app/screens/GbfsValidator/index.tsx
@@ -39,7 +39,7 @@ export default function GbfsValidator(): React.ReactElement {
           <Typography variant='h3' sx={{ fontWeight: 700, mb: 2 }}>
             GBFS Validator
           </Typography>
-          <Typography sx={{maxWidth: '30em', fontSize: '1.1rem'}}>
+          <Typography sx={{ maxWidth: '30em', fontSize: '1.1rem' }}>
             The GBFS Validator is a tool that helps you validate your GBFS feeds
             against the GBFS specification.
           </Typography>
@@ -52,137 +52,151 @@ export default function GbfsValidator(): React.ReactElement {
           height: '100%',
           pb: '50px',
           position: 'relative',
+          minHeight: test ? '55vh' : 'none',
         }}
       >
-       <Box sx={{ position: 'absolute',
+        <Box
+          sx={{
+            position: 'absolute',
             top: '-110px',
             left: '50%',
-            transform: 'translateX(-50%)',            maxWidth: 'md',
-            margin: 'auto',}}>
-       <Box
-          id='input-box'
-          sx={{
-            boxShadow: '0px 3px 0px 1px rgba(0,0,0,0.2)',
-            padding: 2,
-            marginTop: 2,
-            borderRadius: 1,
-            backgroundColor: theme.palette.background.default,
-           
+            transform: 'translateX(-50%)',
+            maxWidth: 'md',
+            margin: 'auto',
           }}
         >
           <Box
-            id='input-box-header'
+            id='input-box'
             sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
+              boxShadow: '0px 3px 0px 1px rgba(0,0,0,0.2)',
+              padding: 2,
+              marginTop: 2,
+              borderRadius: 1,
+              backgroundColor: theme.palette.background.default,
             }}
           >
-            <TextField
-              id='outlined-basic'
-              variant='outlined'
-              placeholder='eg: https://example.com/gbfs.json'
-              sx={{ width: '100%', mr: 2 }}
+            <Box
+              id='input-box-header'
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <TextField
+                id='outlined-basic'
+                variant='outlined'
+                placeholder='eg: https://example.com/gbfs.json'
+                sx={{ width: '100%', mr: 2 }}
                 InputProps={{
-                    startAdornment: (
-                    <SearchIcon></SearchIcon>
-                    ),
+                  startAdornment: <SearchIcon></SearchIcon>,
                 }}
-            />
-            <Button variant='contained' color='primary' sx={{p: '12px'}}>
-              Validate
-            </Button>
-          </Box>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox />}
-              label='Requires Authentication'
-            />
-          </FormGroup>
-          <Box id='cta-buttons' sx={{ display: 'flex', gap: 2 }}>
-            <Button variant='text' color='primary'>
-              Broswe GBFS Feeds
-            </Button>
-            <Button variant='text' color='primary' endIcon={<OpenInNew />}>
-              View GBFS Validator API Docs
-            </Button>
-          </Box>
-        </Box>
-        {test ? (
-        <Box
-          id='info-container'
-          sx={{
-            mt: 4,
-            backgroundColor: theme.palette.background.default,
-            padding: 2,
-            height: '100%',
-          }}
-        >
-          <Box
-            id='row'
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-       
-              padding: 4,
-            }}
-          >
-            <Box sx={{ width: '60%' }}>
-              <Typography variant='h5' sx={{ fontWeight: 700, mb: 2 }}>
-                Official GBFS Validator
-              </Typography>
-              <Typography>
-                The GBFS Validator is based on the official GBFS JSON schema
-                and is designed to help you validate your GBFS feeds against the
-                GBFS specification. For more information about GBFS or the official
-                GBFS JSON schema, please visit
-              </Typography>
-              <Box sx={{mt: 2, display: 'flex', gap: 2}}>
-                <Button variant='outlined' endIcon={<OpenInNew />}>GBFS Schema Repository</Button>
-                <Button variant='outlined' endIcon={<OpenInNew />}>GBFS.org</Button>
-              </Box>
+              />
+              <Button variant='contained' color='primary' sx={{ p: '12px' }}>
+                Validate
+              </Button>
             </Box>
-            <Box sx={{width: '35%',textAlign: 'center'}}>
-                <Box sx={{width: '200px'}} component={'img'} src={gbfsLogo}></Box>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox />}
+                label='Requires Authentication'
+              />
+            </FormGroup>
+            <Box id='cta-buttons' sx={{ display: 'flex', gap: 2 }}>
+              <Button variant='text' color='primary'>
+                Broswe GBFS Feeds
+              </Button>
+              <Button variant='text' color='primary' endIcon={<OpenInNew />}>
+                View GBFS Validator API Docs
+              </Button>
             </Box>
           </Box>
-          <Box
-            id='row'
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              mb: 2,
-              padding: 4,
-              flexDirection: 'row-reverse',
-           
-            }}
-          >
-            <Box sx={{ width: '60%', maxWidth: '30em' }}>
-              <Typography variant='h5' sx={{ fontWeight: 700, mb: 2 }}>
-                Contribute
-              </Typography>
-              <Typography>
-                The GBFS Validator is an open-source project and we welcome
-                contributions from the community. Special thanks to <a>Tom Erik</a>
-              </Typography>
-              <Box sx={{mt: 2}}>
-                <Button variant='outlined' endIcon={<OpenInNew />}>View Github Repository</Button>
-              </Box>
-            </Box>
-            <Box sx={{width: '35%', textAlign: 'center'}}>
-                <Box sx={{width: '200px'}} component={'img'} src={githubLogo}></Box>
-            </Box>
-          </Box>
-        </Box>
-        ) : (
-        null
-        )}
-       </Box>
+          {test ? (
+            <Box
+              id='info-container'
+              sx={{
+                mt: 4,
+                backgroundColor: theme.palette.background.default,
+                padding: 2,
+                height: '100%',
+              }}
+            >
+              <Box
+                id='row'
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
 
+                  padding: 4,
+                }}
+              >
+                <Box sx={{ width: '60%' }}>
+                  <Typography variant='h5' sx={{ fontWeight: 700, mb: 2 }}>
+                    Official GBFS Validator
+                  </Typography>
+                  <Typography>
+                    The GBFS Validator is based on the official GBFS JSON schema
+                    and is designed to help you validate your GBFS feeds against
+                    the GBFS specification. For more information about GBFS or
+                    the official GBFS JSON schema, please visit
+                  </Typography>
+                  <Box sx={{ mt: 2, display: 'flex', gap: 2 }}>
+                    <Button variant='outlined' endIcon={<OpenInNew />}>
+                      GBFS Schema Repository
+                    </Button>
+                    <Button variant='outlined' endIcon={<OpenInNew />}>
+                      GBFS.org
+                    </Button>
+                  </Box>
+                </Box>
+                <Box sx={{ width: '35%', textAlign: 'center' }}>
+                  <Box
+                    sx={{ width: '200px' }}
+                    component={'img'}
+                    src={gbfsLogo}
+                  ></Box>
+                </Box>
+              </Box>
+              <Box
+                id='row'
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  mb: 2,
+                  padding: 4,
+                  flexDirection: 'row-reverse',
+                }}
+              >
+                <Box sx={{ width: '60%', maxWidth: '30em' }}>
+                  <Typography variant='h5' sx={{ fontWeight: 700, mb: 2 }}>
+                    Contribute
+                  </Typography>
+                  <Typography>
+                    The GBFS Validator is an open-source project and we welcome
+                    contributions from the community. Special thanks to{' '}
+                    <a>Tom Erik</a>
+                  </Typography>
+                  <Box sx={{ mt: 2 }}>
+                    <Button variant='outlined' endIcon={<OpenInNew />}>
+                      View Github Repository
+                    </Button>
+                  </Box>
+                </Box>
+                <Box sx={{ width: '35%', textAlign: 'center' }}>
+                  <Box
+                    sx={{ width: '200px' }}
+                    component={'img'}
+                    src={githubLogo}
+                  ></Box>
+                </Box>
+              </Box>
+            </Box>
+          ) : null}
+        </Box>
       </Box>
-      <ValidationReport></ValidationReport>
+      { !test && <ValidationReport></ValidationReport>}
     </Box>
   );
 }
