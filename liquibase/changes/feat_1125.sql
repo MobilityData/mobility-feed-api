215aa38d012ecd344702cde3dc02908a34e9c913
Jingsi Lu <jingsi@mobilitydata.org>
2025-05-08 11:58:11 -0400

    used whitelist in one-off sql update

--- liquibase/changes/feat_1125.sql
+++ liquibase/changes/feat_1125.sql
@@ -1,5 +1,16 @@
 -- Update gbfsendpoint table to set is_feature = false for specific endpoints
 UPDATE gbfsendpoint
-SET is_feature = false
-WHERE name IN ('station_information', 'system_hours', 'system_calendar')
-AND is_feature = true;
+SET is_feature = CASE
+    WHEN name IN (
+        'manifest',
+        'gbfs_versions',
+        'vehicle_types',
+        'station_status',
+        'vehicle_status',
+        'system_regions',
+        'system_pricing_plans',
+        'system_alerts',
+        'geofencing_zones'
+    ) THEN true
+    ELSE false
+END;
