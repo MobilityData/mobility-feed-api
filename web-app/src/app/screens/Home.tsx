9835becbe0e37ea4988d523fbc0d89877f168add
Alessandro Kreslin <kalessandro14@gmail.com>
2025-05-15 11:35:24 -0400

    syntax

--- web-app/src/app/screens/Home.tsx
+++ web-app/src/app/screens/Home.tsx
@@ -73,7 +73,7 @@ function Component(): React.ReactElement {
     }
   };
 
-  // Test comment to trigger a build 2
+  // Test comment to trigger a build 3
 
   return (
     <Container component='main' sx={{ px: { xs: 0, md: 3 } }}>
