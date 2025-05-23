a579f986d68443420037354ebb602740f70d3fab
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2025-05-23 09:57:37 -0400

    fix failing test

--- functions-python/big_query_ingestion/tests/test_main.py
+++ functions-python/big_query_ingestion/tests/test_main.py
@@ -9,10 +9,8 @@ from main import (
 
 class TestMain(unittest.TestCase):
     @patch("main.BigQueryDataTransferGTFS")
-    @patch("shared.helpers.logger.Logger.init_logger")
-    @patch("main.logging.info")
     def test_ingest_data_to_big_query_gtfs(
-        self, mock_logging_info, mock_init_logger, mock_big_query_transfer_gtfs
+        self, mock_big_query_transfer_gtfs
     ):
         mock_instance = mock_big_query_transfer_gtfs.return_value
         mock_instance.send_data_to_bigquery.return_value = (
@@ -22,16 +20,12 @@ class TestMain(unittest.TestCase):
 
         response = ingest_data_to_big_query_gtfs(None)
 
-        mock_init_logger.assert_called_once()
-        mock_logging_info.assert_any_call("Function triggered")
         mock_instance.send_data_to_bigquery.assert_called_once()
         self.assertEqual(response, ("Data successfully loaded to BigQuery", 200))
 
     @patch("main.BigQueryDataTransferGBFS")
-    @patch("shared.helpers.logger.Logger.init_logger")
-    @patch("main.logging.info")
     def test_ingest_data_to_big_query_gbfs(
-        self, mock_logging_info, mock_init_logger, mock_biq_query_transfer_gbfs
+        self, mock_biq_query_transfer_gbfs
     ):
         mock_instance = mock_biq_query_transfer_gbfs.return_value
         mock_instance.send_data_to_bigquery.return_value = (
@@ -41,7 +35,5 @@ class TestMain(unittest.TestCase):
 
         response = ingest_data_to_big_query_gbfs(None)
 
-        mock_init_logger.assert_called_once()
-        mock_logging_info.assert_any_call("Function triggered")
         mock_instance.send_data_to_bigquery.assert_called_once()
         self.assertEqual(response, ("Data successfully loaded to BigQuery", 200))
