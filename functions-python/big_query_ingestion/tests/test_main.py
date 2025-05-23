3ef31737151f05f50a663773eb7020600c4a5df4
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2025-05-23 10:03:33 -0400

    fix lint

--- functions-python/big_query_ingestion/tests/test_main.py
+++ functions-python/big_query_ingestion/tests/test_main.py
@@ -9,9 +9,7 @@ from main import (
 
 class TestMain(unittest.TestCase):
     @patch("main.BigQueryDataTransferGTFS")
-    def test_ingest_data_to_big_query_gtfs(
-        self, mock_big_query_transfer_gtfs
-    ):
+    def test_ingest_data_to_big_query_gtfs(self, mock_big_query_transfer_gtfs):
         mock_instance = mock_big_query_transfer_gtfs.return_value
         mock_instance.send_data_to_bigquery.return_value = (
             "Data successfully loaded to BigQuery",
@@ -24,9 +22,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(response, ("Data successfully loaded to BigQuery", 200))
 
     @patch("main.BigQueryDataTransferGBFS")
-    def test_ingest_data_to_big_query_gbfs(
-        self, mock_biq_query_transfer_gbfs
-    ):
+    def test_ingest_data_to_big_query_gbfs(self, mock_biq_query_transfer_gbfs):
         mock_instance = mock_biq_query_transfer_gbfs.return_value
         mock_instance.send_data_to_bigquery.return_value = (
             "Data successfully loaded to BigQuery",
