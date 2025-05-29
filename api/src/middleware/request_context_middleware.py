f7a12a25fc7a5f1e772e759669e8aa1b395e5a09
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2025-05-29 17:26:10 -0400

    test headers

--- api/src/middleware/request_context_middleware.py
+++ api/src/middleware/request_context_middleware.py
@@ -3,7 +3,7 @@ import time
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 from middleware.request_context import RequestContext, _request_context
-from utils.logger import HttpRequest, API_ACCESS_LOG
+from utils.logger import HttpRequest, API_ACCESS_LOG, get_logger
 
 
 class RequestContextMiddleware:
@@ -12,7 +12,7 @@ class RequestContextMiddleware:
     """
 
     def __init__(self, app: ASGIApp) -> None:
-        self.logger = logging.getLogger(API_ACCESS_LOG)
+        self.logger = get_logger(API_ACCESS_LOG)
         self.app = app
 
     @staticmethod
@@ -63,8 +63,13 @@ class RequestContextMiddleware:
         """
         latency = time.time() - start_time
         request = self.create_http_request(scope, request_context, status_code, content_length, latency)
+        headers = {
+            k.decode().lower(): v.decode()
+            for k, v in scope.get("headers", [])
+        }
         self.logger.info(
-            "API Access Log",
+            { "user_id": request_context.user_id if request_context.user_id else "NO_AUTH",
+              "headers": headers },
             extra={
                 "context": {
                     "http_request": request,
