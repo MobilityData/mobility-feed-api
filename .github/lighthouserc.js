983dce331b216b70432883fab0423c979e6c5ec2
Alessandro Kreslin <kalessandro14@gmail.com>
2025-06-03 13:23:00 -0400

    run once + seo accuracy

--- .github/lighthouserc.js
+++ .github/lighthouserc.js
@@ -1,7 +1,18 @@
-//const previewBaseUrl = process.env.LHCI_PREVIEW_URL || 'https://mobility-feeds-dev--pr-1203-nlp6ow3m.web.app';
-const previewBaseUrl = 'https://mobility-feeds-dev--pr-1203-nlp6ow3m.web.app';
+const previewBaseUrl = '';
+try {
+  const previewUrlBase64 = process.env.LHCI_PREVIEW_URL_BASE64;
+  if (previewUrlBase64) {
+    const decodedUrl = Buffer.from(previewUrlBase64, 'base64').toString('utf-8');
+    console.log("Decoded preview URL:", decodedUrl);
+    previewBaseUrl = decodedUrl;
+  } else {
+    console.error("LHCI_PREVIEW_URL_BASE64 environment variable is not set.");
+  }
+} catch (error) {
+  console.error("Error decoding LHCI_PREVIEW_URL_BASE64:", error);
+}
 
-console.log("environemtnt variable LHCI_PREVIEW_URL:", process.env.LHCI_PREVIEW_URL);
+console.log("environemtnt variable LHCI_PREVIEW_URL:", previewUrlBase64);
 
 module.exports = {
   ci: {
@@ -13,11 +24,11 @@ module.exports = {
         `${previewBaseUrl}/feeds/gtfs_rt/mdb-2585`,
         `${previewBaseUrl}/gbfs/gbfs-flamingo_porirua`
       ],
-      numberOfRuns: 2,
+      numberOfRuns: 1, // 1 to speed up the CI process but can be increased for more reliable results
       settings: {
         formFactor: 'desktop',
         throttlingMethod: 'provided',
-        skipAudits: ['robots-txt'],
+        skipAudits: ['robots-txt', 'is-crawlable'],
         screenEmulation: {
           mobile: false,
           width: 1350,
