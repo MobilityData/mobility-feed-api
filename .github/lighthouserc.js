963c7a22760545298b63cc010e9e3125e545d572
Alessandro Kreslin <kalessandro14@gmail.com>
2025-06-06 09:20:03 -0400

    feat: lighthouse report on frontend PRs (#1203)
    * lighthouse check on pr

---
+++ .github/lighthouserc.js
@@ -0,0 +1,44 @@
+let previewBaseUrl = '';
+try {
+  const previewUrlBase64 = process.env.LHCI_PREVIEW_URL_BASE64;
+  console.log("environemtnt variable LHCI_PREVIEW_URL:", previewUrlBase64);
+  if (previewUrlBase64) {
+    const decodedUrl = Buffer.from(previewUrlBase64, 'base64').toString('utf-8');
+    console.log("Decoded preview URL:", decodedUrl);
+    previewBaseUrl = decodedUrl;
+  } else {
+    console.error("LHCI_PREVIEW_URL_BASE64 environment variable is not set.");
+  }
+} catch (error) {
+  console.error("Error decoding LHCI_PREVIEW_URL_BASE64:", error);
+}
+
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        `${previewBaseUrl}/`,
+        `${previewBaseUrl}/feeds`,
+        `${previewBaseUrl}/feeds/gtfs/mdb-2126`,
+        `${previewBaseUrl}/feeds/gtfs_rt/mdb-2585`,
+        `${previewBaseUrl}/gbfs/gbfs-flamingo_porirua`
+      ],
+      numberOfRuns: 1, // 1 to speed up the CI process but can be increased for more reliable results
+      settings: {
+        formFactor: 'desktop',
+        throttlingMethod: 'provided',
+        skipAudits: ['robots-txt', 'is-crawlable'],
+        screenEmulation: {
+          mobile: false,
+          width: 1350,
+          height: 940,
+          deviceScaleRatio: 1,
+          disabled: false
+        }
+      }
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+};
