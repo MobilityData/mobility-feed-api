0b4929e614acca06c1edf305bb13a951fa25b052
Jingsi Lu <jingsi@mobilitydata.org>
2025-07-23 18:44:36 +0800

    fixed terraform script

--- infra/functions-python/main.tf
+++ infra/functions-python/main.tf
@@ -1243,6 +1243,7 @@ resource "google_cloudfunctions2_function" "tasks_executor" {
       PROJECT_ID  = var.project_id
       ENV = var.environment
       PUBSUB_TOPIC_NAME = "rebuild-bounding-boxes-topic"
+      GOOGLE_FUNCTION_SOURCE = "src/main.py"
     }
     available_memory                 = local.function_tasks_executor_config.memory
     timeout_seconds                  = local.function_tasks_executor_config.timeout
