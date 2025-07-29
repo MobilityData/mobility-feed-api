b97f5ae6560c2af3823a11aea8d7c30910be37b1
Jingsi Lu <jingsi@mobilitydata.org>
2025-07-29 09:27:24 +0800

    removed unnecessary terraform changes

--- infra/functions-python/main.tf
+++ infra/functions-python/main.tf
@@ -1223,7 +1223,6 @@ resource "google_cloudfunctions2_function" "tasks_executor" {
       PROJECT_ID  = var.project_id
       ENV = var.environment
       PUBSUB_TOPIC_NAME = "rebuild-bounding-boxes-topic"
-      GOOGLE_FUNCTION_SOURCE = "src/main.py"
     }
     available_memory                 = local.function_tasks_executor_config.memory
     timeout_seconds                  = local.function_tasks_executor_config.timeout
