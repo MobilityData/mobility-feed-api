e317124d0ff1f24ecdbfaee749fb1d9a2f182edf
Alessandro Kreslin <kalessandro14@gmail.com>
2025-11-20 09:14:27 -0500

    update variable script to allow optional variables

--- scripts/replace-variables.sh
+++ scripts/replace-variables.sh
@@ -23,17 +23,19 @@
 # For an example of a valid input file, check `../infra/vars.tfvars.rename_me`.
 # All variables need to be set to the environment previous running the script.
 # Parameters:
-#   -variables <VARIABLES_LIST>         Comma separated list of variables names.
-#   -in_file <INPUT_FULL_PATH_NAME>     Full path and file name of the input file.
-#   -out_file <OUTPUT_FULL_PATH_NAME>   Full path and file name of the output file.
-#   -no_quotes                          Option to disable enclosing variable in double quotes during substitution
+#   -variables <VARIABLES_LIST>           Comma separated list of REQUIRED variable names.
+#   -optional_variables <OPTIONAL_LIST>   Comma separated list of OPTIONAL variable names (may be unset or empty).
+#   -in_file <INPUT_FULL_PATH_NAME>       Full path and file name of the input file.
+#   -out_file <OUTPUT_FULL_PATH_NAME>     Full path and file name of the output file.
+#   -no_quotes                            Option to disable enclosing variable in double quotes during substitution
 
 display_usage() {
   printf "\nThis script replaces variables from an input file creating/overriding the content of on an output file"
   printf "\nScript Usage:\n"
   echo "Usage: $0 [options]"
   echo "Options:"
-  echo "  -variables <VARIABLES_LIST>         Comma separated list of variables names."
+  echo "  -variables <VARIABLES_LIST>         Comma separated list of REQUIRED variable names."
+  echo "  -optional_variables <OPTIONAL_LIST> Comma separated list of OPTIONAL variable names."
   echo "  -in_file <INPUT_FULL_PATH_NAME>     Full path and file name of the input file."
   echo "  -out_file <OUTPUT_FULL_PATH_NAME>   Full path and file name of the output file."
   echo "  -no_quotes                          Do not enclose variable values with quotes."
@@ -42,6 +44,7 @@ display_usage() {
 }
 
 VARIABLES=""
+OPTIONAL_VARIABLES=""
 INPUT_FILE=""
 OUT_FILE=""
 ADD_QUOTES="true"
@@ -52,6 +55,11 @@ while [[ $# -gt 0 ]]; do
   case $key in
     -variables)
       VARIABLES="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -optional_variables)
+      OPTIONAL_VARIABLES="$2"
       shift # past argument
       shift # past value
       ;;
@@ -96,30 +104,50 @@ then
 fi
 
 list=$(echo "$VARIABLES" | tr "," "\n")
+optional_list=$(echo "$OPTIONAL_VARIABLES" | tr "," "\n")
 
-#  Check if all variables are set
-for varname in $list
-do
-    if [[ -z "${!varname}" ]]; then
-      echo "Missing required variable value with name: $varname."
-      echo "Script will not execute variables replacement, bye for now."
-      exit 1
-    fi
+#  Check required variables (optional ones may be unset or empty)
+for varname in $list; do
+  if [[ -z "${!varname+x}" ]]; then
+    echo "Missing required variable (unset) with name: $varname."
+    echo "Script will not execute variables replacement, bye for now."
+    exit 1
+  fi
+  if [[ -z "${!varname}" ]]; then
+    echo "Missing required variable (empty value) with name: $varname."
+    echo "Script will not execute variables replacement, bye for now."
+    exit 1
+  fi
 done
 
 # Reads from input setting the first version of the output.
 output=$(<"$IN_FILE")
 
 # Replace variables and create output file
-for varname in $list
-do
-    # shellcheck disable=SC2001
-    # shellcheck disable=SC2016
-    if [[ "$ADD_QUOTES" == "true" ]]; then
-        output=$(echo "$output" | sed  's|{{'"$varname"'}}|'\""${!varname}"\"'|g')
-    else
-        output=$(echo "$output" | sed  's|{{'"$varname"'}}|'"${!varname}"'|g')
-    fi
+for varname in $list; do
+  # Required variables are guaranteed non-empty here
+  value="${!varname}"
+  # shellcheck disable=SC2001
+  # shellcheck disable=SC2016
+  if [[ "$ADD_QUOTES" == "true" ]]; then
+    output=$(echo "$output" | sed 's|{{'"$varname"'}}|'"\"$value\""'|g')
+  else
+    output=$(echo "$output" | sed 's|{{'"$varname"'}}|'"$value"'|g')
+  fi
+done
+
+# Substitute optional variables only when they have a non-empty value
+for varname in $optional_list; do
+  # Skip empty or unset optional vars (leave placeholder intact)
+  if [[ -z "${!varname+x}" || -z "${!varname}" ]]; then
+    continue
+  fi
+  value="${!varname}"
+  if [[ "$ADD_QUOTES" == "true" ]]; then
+    output=$(echo "$output" | sed 's|{{'"$varname"'}}|'"\"$value\""'|g')
+  else
+    output=$(echo "$output" | sed 's|{{'"$varname"'}}|'"$value"'|g')
+  fi
 done
 
 echo "$output" > "$OUT_FILE"
