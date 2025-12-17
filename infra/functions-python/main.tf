24aab7cfa32cbc8327223b7c5d7feb92e9164e43
David Gamez <1192523+davidgamez@users.noreply.github.com>
2025-12-17 14:05:14 -0500

    chore: remove tld sync (#1527)

--- infra/functions-python/main.tf
+++ infra/functions-python/main.tf
@@ -48,12 +48,6 @@ locals {
   function_reverse_geolocation_populate_config = jsondecode(file("${path.module}/../../functions-python/reverse_geolocation_populate/function_config.json"))
   function_reverse_geolocation_populate_zip = "${path.module}/../../functions-python/reverse_geolocation_populate/.dist/reverse_geolocation_populate.zip"
 
-  function_feed_sync_dispatcher_transitland_config = jsondecode(file("${path.module}/../../functions-python/feed_sync_dispatcher_transitland/function_config.json"))
-  function_feed_sync_dispatcher_transitland_zip = "${path.module}/../../functions-python/feed_sync_dispatcher_transitland/.dist/feed_sync_dispatcher_transitland.zip"
-
-  function_feed_sync_process_transitland_config = jsondecode(file("${path.module}/../../functions-python/feed_sync_process_transitland/function_config.json"))
-  function_feed_sync_process_transitland_zip = "${path.module}/../../functions-python/feed_sync_process_transitland/.dist/feed_sync_process_transitland.zip"
-
   function_operations_api_config = jsondecode(file("${path.module}/../../functions-python/operations_api/function_config.json"))
   function_operations_api_zip = "${path.module}/../../functions-python/operations_api/.dist/operations_api.zip"
 
@@ -180,19 +174,8 @@ resource "google_storage_bucket_object" "gbfs_validation_report_zip" {
   source = local.function_gbfs_validation_report_zip
 }
 
-# 6. Feed sync dispatcher transitland
-resource "google_storage_bucket_object" "feed_sync_dispatcher_transitland_zip" {
-  bucket = google_storage_bucket.functions_bucket.name
-  name   = "feed-sync-dispatcher-transitland-${substr(filebase64sha256(local.function_feed_sync_dispatcher_transitland_zip), 0, 10)}.zip"
-  source = local.function_feed_sync_dispatcher_transitland_zip
-}
-
-# 7. Feed sync process transitland
-resource "google_storage_bucket_object" "feed_sync_process_transitland_zip" {
-  bucket = google_storage_bucket.functions_bucket.name
-  name   = "feed-sync-process-transitland-${substr(filebase64sha256(local.function_feed_sync_process_transitland_zip), 0, 10)}.zip"
-  source = local.function_feed_sync_process_transitland_zip
-}
+# 6. Feed sync dispatcher transitland - Deleted
+# # 7. Feed sync process transitland - Deleted
 
 # 8. Operations API
 resource "google_storage_bucket_object" "operations_api_zip" {
@@ -596,27 +579,6 @@ resource "google_cloud_scheduler_job" "tdg_import_schedule" {
 }
 
 
-resource "google_cloud_scheduler_job" "transit_land_scraping_scheduler" {
-  name = "transitland-scraping-scheduler-${var.environment}"
-  description = "Schedule the transitland scraping function"
-  time_zone = "Etc/UTC"
-  schedule = var.transitland_scraping_schedule
-  region = var.gcp_region
-  paused = var.environment == "prod" ? false : true
-  depends_on = [google_cloudfunctions2_function.feed_sync_dispatcher_transitland, google_cloudfunctions2_function_iam_member.transitland_feeds_dispatcher_invoker]
-  http_target {
-    http_method = "POST"
-    uri = google_cloudfunctions2_function.feed_sync_dispatcher_transitland.url
-    oidc_token {
-      service_account_email = google_service_account.functions_service_account.email
-    }
-    headers = {
-      "Content-Type" = "application/json"
-    }
-  }
-  attempt_deadline = "320s"
-}
-
 # 5.3 Create function that subscribes to the Pub/Sub topic
 resource "google_cloudfunctions2_function" "gbfs_validator_pubsub" {
   name        = "${local.function_gbfs_validation_report_config.name}-pubsub"
@@ -671,59 +633,6 @@ resource "google_cloudfunctions2_function" "gbfs_validator_pubsub" {
   }
 }
 
-# 6. functions/feed_sync_dispatcher_transitland cloud function
-# 6.1 Create Pub/Sub topic
-resource "google_pubsub_topic" "transitland_feeds_dispatch" {
-  name = "transitland-feeds-dispatch"
-}
-# 6.2 Create batch function that publishes to the Pub/Sub topic
-resource "google_cloudfunctions2_function" "feed_sync_dispatcher_transitland" {
-  name        = "${local.function_feed_sync_dispatcher_transitland_config.name}-batch"
-  description = local.function_feed_sync_dispatcher_transitland_config.description
-  location    = var.gcp_region
-  depends_on = [google_project_iam_member.event-receiving, google_secret_manager_secret_iam_member.secret_iam_member]
-
-  build_config {
-    runtime     = var.python_runtime
-    entry_point = local.function_feed_sync_dispatcher_transitland_config.entry_point
-    source {
-      storage_source {
-        bucket = google_storage_bucket.functions_bucket.name
-        object = google_storage_bucket_object.feed_sync_dispatcher_transitland_zip.name
-      }
-    }
-  }
-  service_config {
-    environment_variables = {
-      PROJECT_ID = var.project_id
-      PYTHONNODEBUGRANGES = 0
-      PUBSUB_TOPIC_NAME = google_pubsub_topic.transitland_feeds_dispatch.name
-      TRANSITLAND_API_KEY=var.transitland_api_key
-      TRANSITLAND_OPERATOR_URL="https://transit.land/api/v2/rest/operators"
-      TRANSITLAND_FEED_URL="https://transit.land/api/v2/rest/feeds"
-    }
-    available_memory = local.function_feed_sync_dispatcher_transitland_config.available_memory
-    timeout_seconds = local.function_feed_sync_dispatcher_transitland_config.timeout
-    available_cpu = local.function_feed_sync_dispatcher_transitland_config.available_cpu
-    max_instance_request_concurrency = local.function_feed_sync_dispatcher_transitland_config.max_instance_request_concurrency
-    max_instance_count = local.function_feed_sync_dispatcher_transitland_config.max_instance_count
-    min_instance_count = local.function_feed_sync_dispatcher_transitland_config.min_instance_count
-    service_account_email = google_service_account.functions_service_account.email
-    ingress_settings = local.function_feed_sync_dispatcher_transitland_config.ingress_settings
-    vpc_connector = data.google_vpc_access_connector.vpc_connector.id
-    vpc_connector_egress_settings = "PRIVATE_RANGES_ONLY"
-    dynamic "secret_environment_variables" {
-      for_each = local.function_feed_sync_dispatcher_transitland_config.secret_environment_variables
-      content {
-        key        = secret_environment_variables.value["key"]
-        project_id = var.project_id
-        secret     = "${upper(var.environment)}_${secret_environment_variables.value["key"]}"
-        version    = "latest"
-      }
-    }
-  }
-}
-
 # 7. functions/operations_api cloud function
 resource "google_cloudfunctions2_function" "operations_api" {
   name        = "${local.function_operations_api_config.name}"
@@ -760,58 +669,6 @@ resource "google_cloudfunctions2_function" "operations_api" {
     vpc_connector_egress_settings = "PRIVATE_RANGES_ONLY"
     dynamic "secret_environment_variables" {
       for_each = local.function_operations_api_config.secret_environment_variables
-      content {
-        key        = secret_environment_variables.value["key"]
-        project_id = var.project_id
-        secret     = "${upper(var.environment)}_${secret_environment_variables.value["key"]}"
-        version    = "latest"
-      }
-    }
-  }
-}
-# 8. functions/feed_sync_process_transitland cloud function
-resource "google_cloudfunctions2_function" "feed_sync_process_transitland" {
-  name        = "${local.function_feed_sync_process_transitland_config.name}-pubsub"
-  description = local.function_feed_sync_process_transitland_config.description
-  location    = var.gcp_region
-  depends_on = [google_project_iam_member.event-receiving, google_secret_manager_secret_iam_member.secret_iam_member]
-  event_trigger {
-    trigger_region        = var.gcp_region
-    service_account_email = google_service_account.functions_service_account.email
-    event_type            = "google.cloud.pubsub.topic.v1.messagePublished"
-    pubsub_topic          = google_pubsub_topic.transitland_feeds_dispatch.id
-    retry_policy          = "RETRY_POLICY_RETRY"
-  }
-  build_config {
-    runtime     = var.python_runtime
-    entry_point = local.function_feed_sync_process_transitland_config.entry_point
-    source {
-      storage_source {
-        bucket = google_storage_bucket.functions_bucket.name
-        object = google_storage_bucket_object.feed_sync_process_transitland_zip.name
-      }
-    }
-  }
-  service_config {
-    available_memory = local.function_feed_sync_process_transitland_config.memory
-    timeout_seconds = local.function_feed_sync_process_transitland_config.timeout
-    available_cpu = local.function_feed_sync_process_transitland_config.available_cpu
-    max_instance_request_concurrency = local.function_feed_sync_process_transitland_config.max_instance_request_concurrency
-    max_instance_count = local.function_feed_sync_process_transitland_config.max_instance_count
-    min_instance_count = local.function_feed_sync_process_transitland_config.min_instance_count
-    service_account_email = google_service_account.functions_service_account.email
-    ingress_settings = var.environment == "dev" ? "ALLOW_ALL" : local.function_feed_sync_process_transitland_config.ingress_settings
-    vpc_connector = data.google_vpc_access_connector.vpc_connector.id
-    vpc_connector_egress_settings = "PRIVATE_RANGES_ONLY"
-    environment_variables = {
-      PYTHONNODEBUGRANGES = 0
-      DB_REUSE_SESSION = "True"
-      PROJECT_ID = var.project_id
-      PUBSUB_TOPIC_NAME = google_pubsub_topic.transitland_feeds_dispatch.name
-      DATASET_BATCH_TOPIC_NAME = data.google_pubsub_topic.datasets_batch_topic.name
-    }
-    dynamic "secret_environment_variables" {
-      for_each = local.function_feed_sync_process_transitland_config.secret_environment_variables
       content {
         key        = secret_environment_variables.value["key"]
         project_id = var.project_id
@@ -1518,19 +1375,10 @@ resource "google_cloudfunctions2_function_iam_member" "gbfs_validator_batch_invo
   member         = "serviceAccount:${google_service_account.functions_service_account.email}"
 }
 
-resource "google_cloudfunctions2_function_iam_member" "transitland_feeds_dispatcher_invoker" {
-  project        = var.project_id
-  location       = var.gcp_region
-  cloud_function = google_cloudfunctions2_function.feed_sync_dispatcher_transitland.name
-  role           = "roles/cloudfunctions.invoker"
-  member         = "serviceAccount:${google_service_account.functions_service_account.email}"
-}
-
 # Grant permissions to the service account to publish to the pubsub topic
 resource "google_pubsub_topic_iam_member" "functions_publisher" {
   for_each = {
     validate_gbfs_feed = google_pubsub_topic.validate_gbfs_feed.name
-    feed_sync_dispatcher_transitland = google_pubsub_topic.transitland_feeds_dispatch.name
     dataset_batch = data.google_pubsub_topic.datasets_batch_topic.name
   }
 
@@ -1544,7 +1392,6 @@ resource "google_pubsub_topic_iam_member" "functions_publisher" {
 resource "google_pubsub_topic_iam_member" "functions_subscriber" {
   for_each = {
     validate_gbfs_feed = google_pubsub_topic.validate_gbfs_feed.name
-    feed_sync_dispatcher_transitland = google_pubsub_topic.transitland_feeds_dispatch.name
   }
 
   project = var.project_id
