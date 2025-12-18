500a9a04e8628e0b577f1766d447ed60d74e5449
cka-y <chaimaa@mobilitydata.org>
2025-12-18 07:17:30 -0500

    fix: failing tests

--- liquibase/changes/fix_operation_status_constraint.sql
+++ liquibase/changes/fix_operation_status_constraint.sql
@@ -1,6 +1,14 @@
-update feed
-set operational_status = 'published'
-where operational_status is null;
+-- 1. Backfill existing rows
+UPDATE feed
+SET operational_status = 'published'
+WHERE operational_status IS NULL;
 
-alter table feed
-alter column operational_status set not null;
+-- 2. Set default for future inserts
+ALTER TABLE feed
+ALTER COLUMN operational_status
+SET DEFAULT 'published';
+
+-- 3. Enforce NOT NULL
+ALTER TABLE feed
+ALTER COLUMN operational_status
+SET NOT NULL;
