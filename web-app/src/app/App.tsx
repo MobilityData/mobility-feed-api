2044dcede19fbbe54ac28e9f8046844bb52c2ab4
Alessandro Kreslin <kalessandro14@gmail.com>
2026-01-09 08:13:38 -0500

    client access token on init fix

--- web-app/src/app/App.tsx
+++ web-app/src/app/App.tsx
@@ -1,29 +1,32 @@
+'use client';
+
 import './App.css';
 import AppRouter from './router/Router';
 import { BrowserRouter } from 'react-router-dom';
-import { RemoteConfigProvider } from './context/RemoteConfigProvider';
-// import { useDispatch } from 'react-redux';
-// import { anonymousLogin } from './store/profile-reducer';
-import { Suspense, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { anonymousLogin } from './store/profile-reducer';
+import { app } from '../firebase';
+import { Suspense, useEffect, useState } from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import AppContainer from './AppContainer';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App(): React.ReactElement {
-  const [isAppReady, setIsAppReady] = useState(true);
+  const dispatch = useDispatch();
+  const [isAppReady, setIsAppReady] = useState(false);
 
-  // useEffect(() => {
-  //   app.auth().onAuthStateChanged((user) => {
-  //     if (user != null) {
-  //       setIsAppReady(true);
-  //     } else {
-  //       setIsAppReady(false);
-  //       dispatch(anonymousLogin());
-  //     }
-  //   });
-  //   dispatch(anonymousLogin());
-  // }, [dispatch]);
+  useEffect(() => {
+    app.auth().onAuthStateChanged((user) => {
+      if (user != null) {
+        setIsAppReady(true);
+      } else {
+        setIsAppReady(false);
+        dispatch(anonymousLogin());
+      }
+    });
+    dispatch(anonymousLogin());
+  }, [dispatch]);
 
   return (
     <HelmetProvider>
