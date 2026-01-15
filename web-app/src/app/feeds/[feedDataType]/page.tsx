0b7f0b6b7c7002999eaf0ccba571dd99ba99c97a
Alessandro Kreslin <kalessandro14@gmail.com>
2026-01-15 15:07:01 -0500

    route hack: feeds/id for backwards compatible

---
+++ web-app/src/app/feeds/[feedDataType]/page.tsx
@@ -0,0 +1,28 @@
+/**
+ *  This route is a small hack to accomodate the route '/feeds/[feedId]/page.tsx'
+ *  Although the parameter is 'feedDataType', the actual parameter used is 'feedId'
+ *  This url is for backwards compatibility, is used more internally and is not an optimal way to access the feed detail page
+ *  IMPORTANT: This url structure will need to be reviewed in the future once our urls could contain agencies
+ */
+
+import { getFeed } from '../../services/feeds';
+import { getSSRAccessToken } from '../../utils/auth-server';
+import { notFound, redirect } from 'next/navigation';
+
+interface Props {
+  params: { feedDataType: string };
+}
+
+export default async function Page({ params }: Props): Promise<JSX.Element> {
+  const { feedDataType } = await params;
+  const accessToken = await getSSRAccessToken();
+
+  // IMPORTANT: here feedDataType is actually feedId (due to routing hack)
+  const feed = await getFeed(feedDataType, accessToken);
+
+  if (feed == undefined) {
+    notFound();
+  }
+
+  redirect(`/feeds/${feed.data_type}/${feed.id}`);
+}
