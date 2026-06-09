b216317ba52a86d2d21250fdfdba72ce7b1234dc
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2026-06-09 13:32:09 -0400

    fix lint

--- api/src/shared/database/users_database.py
+++ api/src/shared/database/users_database.py
@@ -84,6 +84,7 @@ def with_users_db_session(func=None, db_url: str | None = None):
         return lambda f: with_users_db_session(f, db_url=db_url)
 
     if inspect.iscoroutinefunction(func):
+
         async def wrapper(*args, **kwargs):
             db_session = kwargs.get("db_session")
             if db_session is None:
@@ -96,7 +97,9 @@ def with_users_db_session(func=None, db_url: str | None = None):
                     return await func(*args, **kwargs)
             else:
                 return await func(*args, **kwargs)
+
     else:
+
         def wrapper(*args, **kwargs):
             db_session = kwargs.get("db_session")
             if db_session is None:
